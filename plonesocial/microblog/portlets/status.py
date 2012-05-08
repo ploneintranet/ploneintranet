cffb71d37a498528b63f23541c74995f9c93f531
Guido A.J. Stevens <guido.stevens@cosent.nl>
2012-05-08 12:57:08 +0200

    clean up views, remove plone.app.discussion dependency

--- plonesocial/microblog/portlets/status.py
+++ plonesocial/microblog/portlets/status.py
@@ -1,45 +1,14 @@
+from zope.interface import alsoProvides
 from Products.CMFPlone.interfaces import IPloneSiteRoot
+from Acquisition import aq_inner
 from Acquisition import aq_chain
 from zope.app.component.hooks import getSite
-
-# -*- coding: utf-8 -*-
-from Acquisition import aq_inner
-
-from AccessControl import Unauthorized
-from AccessControl import getSecurityManager
-
-from datetime import datetime
-from DateTime import DateTime
-
-from urllib import quote as url_quote
-
-from zope.i18n import translate
-from zope.i18nmessageid import Message
-
-from zope.component import createObject, queryUtility
-
-from zope.interface import alsoProvides
-
-from z3c.form import form, field, button, interfaces
-from z3c.form.interfaces import IFormLayer
-from z3c.form.browser.checkbox import SingleCheckBoxFieldWidget
-
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
-from Products.statusmessages.interfaces import IStatusMessage
-
-from plone.registry.interfaces import IRegistry
-
 from plone.app.layout.viewlets.common import ViewletBase
 
-from plone.app.discussion.interfaces import IConversation
-from plone.app.discussion.interfaces import IComment
-from plone.app.discussion.interfaces import IReplies
-from plone.app.discussion.interfaces import IDiscussionSettings
-from plone.app.discussion.interfaces import ICaptcha
-
-from plone.app.discussion.browser.validator import CaptchaValidator
-
+from z3c.form import form, field, button
+from z3c.form.interfaces import IFormLayer
 from plone.z3cform import z2
 from plone.z3cform.fieldsets import extensible
 from plone.z3cform.interfaces import IWrappedForm
@@ -50,27 +19,6 @@ from plonesocial.microblog.statusupdate import StatusUpdate
 
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('plonesocial.microblog')
-
-COMMENT_DESCRIPTION_PLAIN_TEXT = _(
-    u"comment_description_plain_text",
-    default=u"You can add a comment by filling out the form below. " +
-             "Plain text formatting.")
-
-COMMENT_DESCRIPTION_MARKDOWN = _(
-    u"comment_description_markdown",
-    default=u"You can add a comment by filling out the form below. " +
-             "Plain text formatting. You can use the Markdown syntax for " +
-             "links and images.")
-
-COMMENT_DESCRIPTION_INTELLIGENT_TEXT = _(
-    u"comment_description_intelligent_text",
-    default=u"You can add a comment by filling out the form below. " +
-             "Plain text formatting. Web and email addresses are " +
-             "transformed into clickable links.")
-
-COMMENT_DESCRIPTION_MODERATION_ENABLED = _(
-    u"comment_description_moderation_enabled",
-    default=u"Comments are moderated.")
 
 
 class StatusForm(extensible.ExtensibleForm, form.Form):
@@ -128,187 +76,7 @@ class StatusForm(extensible.ExtensibleForm, form.Form):
         pass  # pragma: no cover
 
 
-
-class CommentsViewlet(ViewletBase):
-
-    form = StatusForm
-    index = ViewPageTemplateFile('status.pt')
-
-    def update(self):
-        super(CommentsViewlet, self).update()
-        if self.is_discussion_allowed() and \
-           (self.is_anonymous() and self.anonymous_discussion_allowed() \
-            or self.can_reply()):
-            z2.switch_on(self, request_layer=IFormLayer)
-            self.form = self.form(aq_inner(self.context), self.request)
-            alsoProvides(self.form, IWrappedForm)
-            self.form.update()
-
-    # view methods
-
-    def can_reply(self):
-        """Returns true if current user has the 'Reply to item' permission.
-        """
-        return getSecurityManager().checkPermission('Reply to item',
-                                                    aq_inner(self.context))
-
-    def can_manage(self):
-        """We keep this method for <= 1.0b9 backward compatibility. Since we do
-           not want any API changes in beta releases.
-        """
-        return self.can_review()
-
-    def can_review(self):
-        """Returns true if current user has the 'Review comments' permission.
-        """
-        return getSecurityManager().checkPermission('Review comments',
-                                                    aq_inner(self.context))
-
-    def is_discussion_allowed(self):
-        context = aq_inner(self.context)
-        return context.restrictedTraverse('@@conversation_view').enabled()
-
-    def comment_transform_message(self):
-        """Returns the description that shows up above the comment text,
-           dependent on the text_transform setting and the comment moderation
-           workflow in the discussion control panel.
-        """
-        context = aq_inner(self.context)
-        registry = queryUtility(IRegistry)
-        settings = registry.forInterface(IDiscussionSettings, check=False)
-
-        # text transform setting
-        if settings.text_transform == "text/x-web-intelligent":
-            message = translate(Message(COMMENT_DESCRIPTION_INTELLIGENT_TEXT),
-                                context=self.request)
-        elif settings.text_transform == "text/x-web-markdown":
-            message = translate(Message(COMMENT_DESCRIPTION_MARKDOWN),
-                                context=self.request)
-        else:
-            message = translate(Message(COMMENT_DESCRIPTION_PLAIN_TEXT),
-                                context=self.request)
-
-        # comment workflow
-        wftool = getToolByName(context, "portal_workflow", None)
-        workflow_chain = wftool.getChainForPortalType('Discussion Item')
-        if workflow_chain:
-            comment_workflow = workflow_chain[0]
-            comment_workflow = wftool[comment_workflow]
-            # check if the current workflow implements a pending state. If this
-            # is true comments are moderated
-            if 'pending' in comment_workflow.states:
-                message = message + " " + \
-                    translate(Message(COMMENT_DESCRIPTION_MODERATION_ENABLED),
-                              context=self.request)
-
-        return message
-
-    def has_replies(self, workflow_actions=False):
-        """Returns true if there are replies.
-        """
-        if self.get_replies(workflow_actions) is not None:
-            try:
-                self.get_replies(workflow_actions).next()
-                return True
-            except StopIteration:  # pragma: no cover
-                pass
-        return False
-
-    def get_replies(self, workflow_actions=False):
-        """Returns all replies to a content object.
-
-        If workflow_actions is false, only published
-        comments are returned.
-
-        If workflow actions is true, comments are
-        returned with workflow actions.
-        """
-        context = aq_inner(self.context)
-        conversation = IConversation(context)
-
-        wf = getToolByName(context, 'portal_workflow')
-
-        # workflow_actions is only true when user
-        # has 'Manage portal' permission
-
-        def replies_with_workflow_actions():
-            # Generator that returns replies dict with workflow actions
-            for r in conversation.getThreads():
-                comment_obj = r['comment']
-                # list all possible workflow actions
-                actions = [a for a in wf.listActionInfos(object=comment_obj)
-                               if a['category'] == 'workflow' and a['allowed']]
-                r = r.copy()
-                r['actions'] = actions
-                yield r
-
-        def published_replies():
-            # Generator that returns replies dict with workflow status.
-            for r in conversation.getThreads():
-                comment_obj = r['comment']
-                workflow_status = wf.getInfoFor(comment_obj, 'review_state')
-                if workflow_status == 'published':
-                    r = r.copy()
-                    r['workflow_status'] = workflow_status
-                    yield r
-
-        # Return all direct replies
-        if conversation.total_comments > 0:
-            if workflow_actions:
-                return replies_with_workflow_actions()
-            else:
-                return published_replies()
-
-    def get_commenter_home_url(self, username=None):
-        if username is None:
-            return None
-        else:
-            return "%s/author/%s" % (self.context.portal_url(), username)
-
-    def get_commenter_portrait(self, username=None):
-
-        if username is None:
-            # return the default user image if no username is given
-            return 'defaultUser.gif'
-        else:
-            portal_membership = getToolByName(self.context,
-                                              'portal_membership',
-                                              None)
-            return portal_membership.getPersonalPortrait(username)\
-                   .absolute_url()
-
-    def anonymous_discussion_allowed(self):
-        # Check if anonymous comments are allowed in the registry
-        registry = queryUtility(IRegistry)
-        settings = registry.forInterface(IDiscussionSettings, check=False)
-        return settings.anonymous_comments
-
-    def show_commenter_image(self):
-        # Check if showing commenter image is enabled in the registry
-        registry = queryUtility(IRegistry)
-        settings = registry.forInterface(IDiscussionSettings, check=False)
-        return settings.show_commenter_image
-
-    def is_anonymous(self):
-        portal_membership = getToolByName(self.context,
-                                          'portal_membership',
-                                          None)
-        return portal_membership.isAnonymousUser()
-
-    def login_action(self):
-        return '%s/login_form?came_from=%s' % \
-            (self.navigation_root_url,
-             url_quote(self.request.get('URL', '')),)
-
-    def format_time(self, time):
-        # We have to transform Python datetime into Zope DateTime
-        # before we can call toLocalizedTime.
-        util = getToolByName(self.context, 'translation_service')
-        zope_time = DateTime(time.isoformat())
-        return util.toLocalizedTime(zope_time, long_format=True)
-
-
-class StatusViewlet(CommentsViewlet):
+class StatusViewlet(ViewletBase):
     """Subclass the p.a.discussion comments viewlet
     and change it's behaviour to suit our purposes."""
 
@@ -318,10 +86,24 @@ class StatusViewlet(CommentsViewlet):
     comment_transform_message = "What's on your mind?"
 
     def __init__(self, compact, *args, **kwargs):
-        CommentsViewlet.__init__(self, *args, **kwargs)
+        super(StatusViewlet, self).__init__(*args, **kwargs)
         self.compact = compact
         # force microblog context to SiteRoot singleton
         for obj in aq_chain(self.context):
             if IPloneSiteRoot.providedBy(obj):
                 self.context = obj
                 return
+
+    def update(self):
+        super(StatusViewlet, self).update()
+        if not self.is_anonymous():
+            z2.switch_on(self, request_layer=IFormLayer)
+            self.form = self.form(aq_inner(self.context), self.request)
+            alsoProvides(self.form, IWrappedForm)
+            self.form.update()
+
+    def is_anonymous(self):
+        portal_membership = getToolByName(self.context,
+                                          'portal_membership',
+                                          None)
+        return portal_membership.isAnonymousUser()
--- plonesocial/microblog/tests/test_comments.py
+++ plonesocial/microblog/tests/test_comments.py
@@ -1,16 +1,12 @@
 import unittest2 as unittest
 
-from zope.component import createObject
-from Acquisition import aq_base, aq_parent
-
-from Products.CMFCore.utils import getToolByName
+#from zope.component import createObject
+#from Acquisition import aq_base, aq_parent
+#from Products.CMFCore.utils import getToolByName
 from plone.app.testing import TEST_USER_ID, setRoles
 
 from plonesocial.microblog.testing import\
     PLONESOCIAL_MICROBLOG_INTEGRATION_TESTING
-
-from plone.app.discussion.interfaces import IConversation
-from plone.app.discussion.interfaces import IComment
 
 
 class TestInstall(unittest.TestCase):
@@ -21,38 +17,3 @@ class TestInstall(unittest.TestCase):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        typetool = self.portal.portal_types
-        typetool.constructContent('Document', self.portal, 'doc1')
-        self.typetool = typetool
-        self.portal_discussion = getToolByName(self.portal,
-                                               'portal_discussion',
-                                               None)
-
-    def test_add_statusupdate(self):
-        microblog = IConversation(self.portal)
-        comment = createObject('plone.Comment')
-        comment.text = 'Comment text'
-        new_id = microblog.addComment(comment)
-        self.assertTrue(IComment.providedBy(microblog[new_id]))
-        self.assertEqual(aq_base(microblog[new_id].__parent__),
-                          aq_base(microblog))
-        self.assertEqual(microblog.total_comments, 1)
-
-    def test_parent_microblog(self):
-        microblog = IConversation(self.portal)
-        self.assertEqual(aq_parent(microblog),
-                         aq_base(self.portal))
-        self.assertEqual(microblog.__parent__,
-                         self.portal)
-
-    def test_parent_statusupdate(self):
-        microblog = IConversation(self.portal)
-        comment = createObject('plone.Comment')
-        comment.text = 'Comment text'
-        microblog.addComment(comment)
-        self.assertEqual(aq_parent(comment),
-                         aq_base(microblog))
-        self.assertEqual(aq_parent(aq_parent(comment)),
-                         aq_base(self.portal))
-        self.assertEqual(comment.__parent__.__parent__,
-                         self.portal)
