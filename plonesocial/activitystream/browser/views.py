48542e727485e1fbe71b4647192dd89f44865b54
Guido A.J. Stevens <guido.stevens@cosent.nl>
2012-05-08 13:44:18 +0200

    move separate ZODB shard documentation to plonesocial.microblog

--- plonesocial/activitystream/browser/views.py
+++ plonesocial/activitystream/browser/views.py
@@ -11,30 +11,6 @@ class PortalView(BrowserView):
         return self.index()
 
     def __call__(self):
-        """Call.
-
-        app.quickstorage should be a mount point for a separate
-        database, where the instance in buildout should have code like
-        this:
-
-        zope-conf-additional =
-            <zodb_db quick>
-                mount-point /quickstorage:/
-                cache-size 20000
-                <filestorage catalog>
-                    path ${buildout:directory}/var/filestorage/quick.fs
-                </filestorage>
-            </zodb_db>
-
-        Then add the ZODB Mount Point manually in the ZMI.
-
-        Then in a 'bin/instance debug' session do this:
-
-        from BTrees.OOBTree import OOBTree
-        app.quickstorage.tree = OOBTree()
-
-        """
-        #tree = self.context.quickstorage
         return self.render()
 
     def update(self):
