94634e542df614bd58ac5cf9f86e0b97b0a5073a
Guido A.J. Stevens <guido.stevens@cosent.nl>
2012-05-09 15:24:50 +0000

    fix test setup

--- plonesocial/suite/testing.py
+++ plonesocial/suite/testing.py
@@ -1,10 +1,11 @@
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import FunctionalTesting
+#from plone.app.testing import FunctionalTesting
 from plone.app.testing import applyProfile
 
 from zope.configuration import xmlconfig
+
 
 class PlonesocialSuite(PloneSandboxLayer):
 
@@ -13,10 +14,17 @@ class PlonesocialSuite(PloneSandboxLayer):
     def setUpZope(self, app, configurationContext):
         # Load ZCML for this package
         import plonesocial.suite
+        import plonesocial.microblog
+        import plonesocial.activitystream
         xmlconfig.file('configure.zcml',
                        plonesocial.suite,
                        context=configurationContext)
-
+        xmlconfig.file('configure.zcml',
+                       plonesocial.microblog,
+                       context=configurationContext)
+        xmlconfig.file('configure.zcml',
+                       plonesocial.activitystream,
+                       context=configurationContext)
 
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'plonesocial.suite:default')
