cc0151e573989b8989e2bafb937ab8a5b70c62e9
Guido A.J. Stevens <guido.stevens@cosent.nl>
2012-09-24 09:59:39 +0000

    base profile

--- plonesocial/network/monkey.py
+++ plonesocial/network/monkey.py
@@ -7,5 +7,5 @@ logger = logging.getLogger(__name__)
 
 
 logger.info('Setting PlonePAS portrait scale')
-plonepas_config.MEMBER_IMAGE_SCALE = portrait_scale = (225, 300)
+plonepas_config.MEMBER_IMAGE_SCALE = portrait_scale = (300, 300)
 plonepas_config.IMAGE_SCALE_PARAMS['scale'] = portrait_scale
