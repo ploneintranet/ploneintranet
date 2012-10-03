a2a6278e5d25013a8f37e202e4698c9f497bb9c5
Guido A.J. Stevens <guido.stevens@cosent.nl>
2012-10-03 17:17:44 +0200

    provide integration menu

---
+++ plonesocial/suite/browser/navigation.py
@@ -0,0 +1,29 @@
+from zope.interface import implements
+from zope.viewlet.interfaces import IViewlet
+from zope.publisher.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
+
+
+class PloneSocialNavigation(BrowserView):
+    """Provide toplevel navigation that spans plonesocial.activitystream
+    and plonesocial.network.
+    """
+    implements(IViewlet)
+
+    def __init__(self, context, request, view, manager):
+        self.context = context
+        self.request = request
+        self.view = self.__parent__ = view
+        self.manager = manager
+
+    def update(self):
+        pass
+
+    render = ViewPageTemplateFile("navigation.pt")
+
+    def portal_url(self):
+        portal_state = getMultiAdapter(
+            (self.context, self.request),
+            name=u'plone_portal_state')
+        return portal_state.portal_url()
