ecabaa3775b6ace5002f7807078a5268832418a0
Guido Stevens <guido.stevens@cosent.nl>
2012-10-15 21:32:45 +0200

    fix pyflakes by moving monkey into __init__

--- plonesocial/network/__init__.py
+++ plonesocial/network/__init__.py
@@ -1,4 +1,13 @@
-import monkey
+# monkey Products.PlonePAS image scales
+# until PLIP 12870 is merged
+
+from Products.PlonePAS import config as plonepas_config
+import logging
+logger = logging.getLogger(__name__)
+
+logger.info('Setting PlonePAS portrait scale')
+plonepas_config.MEMBER_IMAGE_SCALE = portrait_scale = (300, 300)
+plonepas_config.IMAGE_SCALE_PARAMS['scale'] = portrait_scale
 
 
 def initialize(context):
