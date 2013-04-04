8fd416883278f755601a5fb91e5081550e3c4280
Guido Stevens <guido.stevens@cosent.nl>
2013-04-04 19:27:09 +0200

    implement and integrate context permission checks for local microblog spaces

--- plonesocial/microblog/tests/test_tool.py
+++ plonesocial/microblog/tests/test_tool.py
@@ -1,13 +1,18 @@
 import unittest2 as unittest
 from zope.component import queryUtility
+from AccessControl import getSecurityManager
+from Products.CMFCore.permissions import View
+from Products.CMFCore.utils import getToolByName
 
 from plone.app.testing import TEST_USER_ID, setRoles
+from plone.app.testing import login, logout
 
 from plonesocial.microblog.testing import \
     PLONESOCIAL_MICROBLOG_INTEGRATION_TESTING
 
 from plonesocial.microblog.interfaces import IStatusContainer
 from plonesocial.microblog.interfaces import IMicroblogTool
+from plonesocial.microblog.statusupdate import StatusUpdate
 
 
 class TestMicroblogTool(unittest.TestCase):
@@ -21,3 +26,54 @@ class TestMicroblogTool(unittest.TestCase):
     def test_tool_available(self):
         tool = queryUtility(IMicroblogTool)
         self.assertTrue(IStatusContainer.providedBy(tool))
+
+
+class TestMicroblogToolContextBlacklisting(unittest.TestCase):
+
+    layer = PLONESOCIAL_MICROBLOG_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        workflowTool = getToolByName(self.portal, 'portal_workflow')
+        workflowTool.setDefaultChain('simple_publication_workflow')
+        workflowTool.updateRoleMappings()
+        self.portal.invokeFactory('Folder', 'f1', title=u"Folder 1")
+        f1 = self.portal['f1']
+        self.portal.invokeFactory('Folder', 'f2', title=u"Folder 2")
+        f2 = self.portal['f2']
+
+        workflowTool.doActionFor(f2, 'publish')
+        self.assertEqual(workflowTool.getInfoFor(f1, 'review_state'),
+                         'private')
+        self.assertEqual(workflowTool.getInfoFor(f2, 'review_state'),
+                         'published')
+
+        tool = queryUtility(IMicroblogTool)
+        su1 = StatusUpdate('test #foo', f1)
+        tool.add(su1)
+        self.su2 = su2 = StatusUpdate('test #foo', f2)
+        tool.add(su2)
+        # the tool is queued
+        tool.flush_queue()
+
+        # set up new user and switch to that user
+        acl_users = getToolByName(self.portal, 'acl_users')
+        acl_users.userFolderAddUser('user1', 'secret', ['Member'], [])
+        logout()
+
+    def test_allowed_status(self):
+        """The base implementation does not test access controls.
+        The tool should provide those.
+        """
+        # verify setup: only f2 is accessible for Member
+        login(self.portal, 'user1')
+        sm = getSecurityManager()
+        user = getSecurityManager().getUser()
+        self.assertEqual(user.getId(), 'user1')
+        self.assertFalse(sm.checkPermission(View, self.portal['f1']))
+        self.assertTrue(sm.checkPermission(View, self.portal['f2']))
+
+        # and now finally the actual test
+        tool = queryUtility(IMicroblogTool)
+        self.assertEqual(list(tool.values()), [self.su2])
--- plonesocial/microblog/tool.py
+++ plonesocial/microblog/tool.py
@@ -1,6 +1,8 @@
 from zope.interface import implements
 from Products.CMFCore.utils import UniqueObject
+from Products.CMFCore.utils import getToolByName
 from OFS.SimpleItem import SimpleItem
+from plone.uuid.interfaces import IUUID
 
 from interfaces import IMicroblogTool
 from statuscontainer import QueuedStatusContainer
@@ -13,3 +15,19 @@ class MicroblogTool(UniqueObject, SimpleItem, QueuedStatusContainer):
 
     meta_type = 'plonesocial.microblog tool'
     id = 'plonesocial_microblog'
+
+    def allowed_status_keys(self):
+        """Return the subset of IStatusUpdate keys
+        that are related to UUIDs of accessible contexts.
+        I.e. blacklist all IStatusUpdate that has a context
+        which we don't have permission to access.
+
+        This method is implemented on the tool,
+        overriding a noop implementation on BaseStatusContainer.
+        """
+        catalog = getToolByName(self, 'portal_catalog')
+        results = catalog.searchResults({'portal_type': 'Folder'})
+        whitelist = [IUUID(x.getObject()) for x in results]
+        blacklist = [x for x in self._context_mapping.keys()
+                     if x not in whitelist]
+        return self._allowed_status_keys(blacklist)
