707ffb947e8fd33431ae0f27c6a1645fbd99027d
tdesvenain <thomas.desvenain@gmail.com>
2013-04-04 11:59:46 +0200

    Fix import errors under Plone 4.3

--- plonesocial/activitystream/browser/activity_provider.py
+++ plonesocial/activitystream/browser/activity_provider.py
@@ -3,7 +3,10 @@ from zope.interface import Interface
 from zope.interface import implements
 from zope.component import adapts
 from zope.component import getMultiAdapter
-from zope.app.component.hooks import getSite
+try:
+    from zope.component.hooks import getSite
+except ImportError:
+    from zope.app.component.hooks import getSite
 
 from DateTime import DateTime
 from Products.CMFCore.utils import getToolByName
