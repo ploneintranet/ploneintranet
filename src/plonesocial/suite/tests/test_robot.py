fc58a40d2eef9f2cd4f3eca97ced5785cc00ca20
Guido A.J. Stevens <guido.stevens@cosent.nl>
2013-04-09 14:47:52 +0000

    factor demo into a separate test level

--- src/plonesocial/suite/tests/test_robot.py
+++ src/plonesocial/suite/tests/test_robot.py
@@ -6,6 +6,11 @@ from plone.testing import layered
 
 def test_suite():
     suite = unittest.TestSuite()
+    demo_suite = robotsuite.RobotTestSuite("demo")
+    # disabled by default, to run use: bin/test -a 2
+    demo_suite.level = 2
+    suite.addTests([layered(demo_suite,
+                            layer=PLONESOCIAL_ROBOT_TESTING), ])
     suite.addTests([layered(robotsuite.RobotTestSuite('robot'),
                             layer=PLONESOCIAL_ROBOT_TESTING), ])
     return suite
