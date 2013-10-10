aab834136f3f4d6ea897d451d4bfcf6a2480a598
Gil Forcada <gil.forcada@freitag.de>
2013-10-10 14:51:39 +0200

    Add a setUp method on test_messaging to group repeated steps

--- src/plonesocial/messaging/tests/test_messaging.py
+++ src/plonesocial/messaging/tests/test_messaging.py
@@ -12,6 +12,10 @@ class TestMessagingLocator(unittest.TestCase):
 
     layer = PLONESOCIAL_MESSAGING_INTEGRATION_TESTING
 
+    def setUp(self):
+        self.site = self.layer['portal']
+        self.tool = getToolByName(site, 'plonesocial_messaging')
+
     def test_interface(self):
         from plonesocial.messaging.interfaces import IMessagingLocator
         from plonesocial.messaging.messaging import MessagingLocator
@@ -19,17 +23,13 @@ class TestMessagingLocator(unittest.TestCase):
 
     def test_tool_installed(self):
         from plonesocial.messaging.tool import MessagingTool
-        site = self.layer['portal']
-        self.assertTrue('plonesocial_messaging' in site)
+        self.assertTrue('plonesocial_messaging' in self.site)
 
-        tool = getToolByName(site, 'plonesocial_messaging')
-        self.assertTrue(isinstance(tool, MessagingTool))
+        self.assertTrue(isinstance(self.tool, MessagingTool))
 
     def test_tool_provides_inboxes(self):
         from plonesocial.messaging.interfaces import IInboxes
-        site = self.layer['portal']
-        tool = getToolByName(site, 'plonesocial_messaging')
-        self.assertTrue(IInboxes.providedBy(tool))
+        self.assertTrue(IInboxes.providedBy(self.tool))
 
     def test_messaginglocator(self):
         from plonesocial.messaging.interfaces import IMessagingLocator
