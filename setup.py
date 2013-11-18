4234807db97e1fd0d7f677efb50d73d71e9352b2
Matthew Sital-Singh <matthew.sitalsingh@gmail.com>
2013-11-18 12:16:22 +0000

    fix reference to readme file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 version = '0.1'
 
 long_description = (
-    open('README').read()
+    open('README.md').read()
     + '\n' +
     'Contributors\n'
     '============\n'
