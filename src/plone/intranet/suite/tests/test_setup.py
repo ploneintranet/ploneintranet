f0a6fa53c4daf4d7e1c8c1847e6254a916e33494
Matthew Sital-Singh <matthew.sitalsingh@gmail.com>
2013-11-18 12:55:45 +0000

    pep8

--- src/plone/intranet/suite/tests/test_setup.py
+++ src/plone/intranet/suite/tests/test_setup.py
@@ -9,14 +9,14 @@ from plone.intranet.suite.testing import\
 class TestExample(unittest.TestCase):
 
     layer = PLONE_INTRANET_SUITE_INTEGRATION
-    
+
     def setUp(self):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
         self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
-    
+
     def test_product_is_installed(self):
-        """ Validate that our products GS profile has been run and the product 
+        """ Validate that our products GS profile has been run and the product
             installed
         """
         pid = 'plone.intranet.suite'
