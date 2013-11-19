956b73e1d2c7e266d1499fb331129eac93919cdf
Matt Hamilton <matth@netsight.co.uk>
2013-11-19 16:25:18 +0000

    initial egg creation

---
+++ src/ploneintranet/workspace/testing.py
@@ -0,0 +1,43 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+
+
+class PloneintranetworkspaceLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import ploneintranet.workspace
+        xmlconfig.file(
+            'configure.zcml',
+            ploneintranet.workspace,
+            context=configurationContext
+        )
+
+        # Install products that use an old-style initialize() function
+        #z2.installProduct(app, 'Products.PloneFormGen')
+
+#    def tearDownZope(self, app):
+#        # Uninstall products installed above
+#        z2.uninstallProduct(app, 'Products.PloneFormGen')
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'ploneintranet.workspace:default')
+
+PLONEINTRANET_WORKSPACE_FIXTURE = PloneintranetworkspaceLayer()
+PLONEINTRANET_WORKSPACE_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONEINTRANET_WORKSPACE_FIXTURE,),
+    name="PloneintranetworkspaceLayer:Integration"
+)
+PLONEINTRANET_WORKSPACE_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(PLONEINTRANET_WORKSPACE_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="PloneintranetworkspaceLayer:Functional"
+)
---
+++ src/ploneintranet/workspace/tests/test_robot.py
@@ -0,0 +1,13 @@
+from  ploneintranet.workspace.testing import PLONEINTRANET_WORKSPACE_FUNCTIONAL_TESTING
+from plone.testing import layered
+import robotsuite
+import unittest
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite("robot_test.txt"),
+                layer=PLONEINTRANET_WORKSPACE_FUNCTIONAL_TESTING)
+    ])
+    return suite
