03a68134f9e06620d656b7c71334f023bae6d93d
Matthew Sital-Singh <matthew.sitalsingh@gmail.com>
2013-11-20 16:41:46 +0000

    pep8

--- src/ploneintranet/workspace/workspacefolder.py
+++ src/ploneintranet/workspace/workspacefolder.py
@@ -1,20 +1,21 @@
 from five import grok
 
-from z3c.form import group, field
-from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+#from z3c.form import group, field
+#from zope import schema
+#from zope.interface import invariant, Invalid
+#from zope.schema.interfaces import IContextSourceBinder
+#from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from plone.dexterity.content import Container
-from plone.directives import dexterity, form
-from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+#from plone.directives import dexterity
+from plone.directives import form
+#from plone.app.textfield import RichText
+#from plone.namedfile.field import NamedImage, NamedFile
+#from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 from plone.namedfile.interfaces import IImageScaleTraversable
 
 
-from ploneintranet.workspace import MessageFactory as _
+#from ploneintranet.workspace import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
