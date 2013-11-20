2982ad0d5b162bc7b6cea5b0a7b202493b499691
Matthew Sital-Singh <matthew.sitalsingh@gmail.com>
2013-11-20 11:56:15 +0000

    basic test for workspace view

--- src/ploneintranet/workspace/tests/test_content_type.py
+++ src/ploneintranet/workspace/tests/test_content_type.py
@@ -6,10 +6,14 @@ from collective.workspace.interfaces import IWorkspace
 class TestContentTypes(BaseTestCase):
 
     def test_add_workspace(self):
+        """ check that we can create our workspace type,
+            and that it provides the collective.workspace behaviour
+        """
         self.login_as_portal_owner()
         workspace = api.content.create(self.portal,
                                        'ploneintranet.workspace.workspace',
-                                       'example-workspace')
+                                       'example-workspace',
+                                       title='Welcome to my workspace')
 
         ws = IWorkspace(workspace, None)
         self.assertIsNotNone(
@@ -17,3 +21,9 @@ class TestContentTypes(BaseTestCase):
             'Workspace type does not provide the'
             'collective.workspace behaviour',
         )
+
+        # does the view work?
+        view = workspace.restrictedTraverse('@@view')
+        html = view()
+        self.assertIn(workspace.title, html,
+                      'Workspace title not found on view page')
