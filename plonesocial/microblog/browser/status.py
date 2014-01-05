514716340bf726d06d9f6c2e55c86927227e16b6
maartenkling <maarten@fourdigits.nl>
2014-01-05 18:22:22 +0100

    add threaded info to status provided

--- plonesocial/microblog/browser/status.py
+++ plonesocial/microblog/browser/status.py
@@ -39,7 +39,6 @@ class StatusForm(extensible.ExtensibleForm, form.Form):
     fields = field.Fields(IStatusUpdate).omit('portal_type',
                                               '__parent__',
                                               '__name__',
-                                              'id',
                                               'mime_type',
                                               'creator',
                                               'userid',
@@ -50,11 +49,40 @@ class StatusForm(extensible.ExtensibleForm, form.Form):
 
     def updateWidgets(self):
         super(StatusForm, self).updateWidgets()
+        self.widgets['thread'].addClass("hide")
 
     def updateActions(self):
         super(StatusForm, self).updateActions()
         self.actions['cancel'].addClass("hide")
         self.actions['statusupdate'].addClass("standalone")
+        self.actions['statusreply'].addClass("standalone")
+        self.actions['statusreply'].addClass("reply")
+
+    @button.buttonAndHandler(_(u"label_statusreply",
+                               default=u"Reply"),
+                             name='statusreply')
+    def handleReply(self, action):
+
+        # Validation form
+        data, errors = self.extractData()
+        if errors:
+            return
+
+        container = queryUtility(IMicroblogTool)
+        microblog_context = get_microblog_context(self.context)
+        thread = data.get('thread', None)
+        status = StatusUpdate(data['text'],
+                              context=microblog_context,
+                              thread=thread)
+
+        # debugging only
+#        container.clear()
+
+        # save the status update
+        container.add(status)
+
+        # Redirect to portal home
+        self.request.response.redirect(self.action)
 
     @button.buttonAndHandler(_(u"label_statusupdate",
                                default=u"Status Update"),
@@ -68,7 +96,10 @@ class StatusForm(extensible.ExtensibleForm, form.Form):
 
         container = queryUtility(IMicroblogTool)
         microblog_context = get_microblog_context(self.context)
-        status = StatusUpdate(data['text'], context=microblog_context)
+        thread = data.get('thread', None)
+        status = StatusUpdate(data['text'],
+                              context=microblog_context,
+                              thread=thread)
 
         # debugging only
 #        container.clear()
@@ -100,6 +131,8 @@ class StatusProvider(object):
         self.context = context
         self.request = request
         self.view = view
+        # optional give thread to provider
+        self.thread = None
         self.portlet_data = None  # used by microblog portlet
         # force microblog context to IMicroblogContext or SiteRoot
         for obj in aq_chain(self.context):
@@ -114,7 +147,7 @@ class StatusProvider(object):
     def _update(self):
         if self.available:
             z2.switch_on(self, request_layer=IFormLayer)
-            self.form = self.form(aq_inner(self.context), self.request)
+            self.form = self.form(self, self.request)
             alsoProvides(self.form, IWrappedForm)
             self.form.update()
 
