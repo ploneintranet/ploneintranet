864595d02fb4233ba9f4290b692cc36c7e885a5d
Guido A.J. Stevens <guido.stevens@cosent.nl>
2014-01-17 14:22:05 +0000

    add upgrade step to support conversations on existing installations

--- plonesocial/microblog/migration.py
+++ plonesocial/microblog/migration.py
@@ -14,3 +14,20 @@ def setup_uuid_mapping(context):
         logger.info("Adding missing UUID mapping to %s" % repr(tool))
         tool._uuid_mapping = OOBTree.OOBTree()
         commit()
+
+
+def setup_threadids(context):
+    """0.6 adds a new statuscontainerindex and statusupdate attribute"""
+    tool = queryUtility(IMicroblogTool)
+    if not hasattr(tool, '_threadid_mapping'):
+        logger.info("Adding missing threadid mapping to %s" % repr(tool))
+        tool._threadid_mapping = OOBTree.OOBTree()
+        commit()
+    i = 0
+    for status in tool.values(limit=None):
+        i += 1
+        if getattr(status, 'thread_id', False) is False:
+            status.thread_id = None
+        if i % 100 == 0:
+            commit()
+    commit()
