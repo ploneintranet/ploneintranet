9b819af74f14f29f858ab45f2bc1e17a2ea2fee1
maartenkling <maarten@fourdigits.nl>
2014-02-02 07:50:16 +0100

    ajax reply

--- plonesocial/activitystream/browser/javascripts/main.js
+++ plonesocial/activitystream/browser/javascripts/main.js
@@ -3,10 +3,37 @@
         $('.reply a').click(function(event) {
             var url = "@@status/" + $(this).attr('data-reply') + '/';
             // replace a > div.reply > div.activityItem
-            $(this).parent().parent().load(url + ' .conversation');
-            $(this).parent().parent().removeClass('activityItem');
+            $(this).parent().parent().load(url + ' .conversation', function() {
+                $(this).closest('.activityItem').removeClass('activityItem');
+            });
+
             event.preventDefault();
             return false;
+        });
+
+        // ajaxify submitting reply
+        $('.activityItem').on('submit', '.conversation form', function(event) {
+
+            event.preventDefault();
+
+            var postData = $(this).serializeArray();
+            // Add submit button
+            postData.push({ name: $(this).find('#form-buttons-statusupdate').attr('name'),
+                            value: $(this).find('#form-buttons-statusupdate').attr('value') });
+            var url = $(this).attr( "action" );
+
+            // Send the data using post
+            var posting = $.post( url, postData );
+
+            posting.done(function( data ) {
+                var content = $( data ).find( ".conversation" ).html();
+                $('#content .conversation').empty();
+                $('#content .conversation').append( content );
+                $('#ajax-spinner').hide();
+            });
+
+            return false;
+
         });
     });
 }(jQuery));
