2c13ea44c5b9f826533583312cba9ca09789076c
maartenkling <maarten@fourdigits.nl>
2014-02-02 11:54:29 +0100

    change title for text when reply

--- plonesocial/microblog/browser/status.py
+++ plonesocial/microblog/browser/status.py
@@ -159,6 +159,7 @@ class StatusReplyProvider(StatusProvider):
 
     form = StatusForm
     label = _(u"Add a reply")
+
     index = ViewPageTemplateFile('status.pt')
     available = True  # fixme security check
 
@@ -171,6 +172,7 @@ class StatusReplyProvider(StatusProvider):
         self.thread_id = context.thread_id or context.id  # catch first
         self.portlet_data = None  # used by microblog portlet
 
+        self.form.fields['text'].field.title = self.label
 
 class StatusViewlet(StatusProvider, ViewletBase):
 
