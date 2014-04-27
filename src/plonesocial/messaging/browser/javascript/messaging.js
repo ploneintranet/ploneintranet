0ae2fee7b4f3bc86b608e47ecb60802bb19c9807
Adam Castle <acastle@ripe.net>
2014-04-27 19:45:21 +0200

    js working with convo json feed

--- src/plonesocial/messaging/browser/javascript/messaging.js
+++ src/plonesocial/messaging/browser/javascript/messaging.js
@@ -9,13 +9,31 @@ function messaging_ajax (url, replaceid, datatype) {
                $(replaceid).replaceWith(data);
                message_click();
            }
+           if (datatype == 'json') {
+               show_convos(data, replaceid);
+           }
        }
     });
 }
 
+
+function show_convos(data, replaceid) {
+    var conversations = data['conversations'];
+    for (var c = 0; c < conversations.length; ++ c) {
+        var new_message_count = ''
+        if (conversations[c].new_messages_count > 0) {
+            new_message_count = '<span class="new-message-name"> ' + conversations[c].new_messages_count + '</span>';
+        }
+        $(replaceid).append('<div class="message"><input type="hidden" class="username" value="'+conversations[c].username+'"/>'
+                               + new_message_count + ' ' +
+                               conversations[c].fullname +'</div>');
+    }
+
+}
+
 function message_click() {
     $('#your-messages-icon').click(function(){
-        messaging_ajax('@@messaging-conversations', '#show-your-messages', 'json');
+        messaging_ajax('@@messaging-conversations', '#your-messages', 'json');
     });
 }
 
