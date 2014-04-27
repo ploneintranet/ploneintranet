df0b067f86a12c7e258c4c862839a2e21e4a8af5
Adam Castle <acastle@ripe.net>
2014-04-27 17:09:28 +0200

    ajax call for the browser view

--- src/plonesocial/messaging/browser/javascript/messaging.js
+++ src/plonesocial/messaging/browser/javascript/messaging.js
@@ -1,1 +1,11 @@
-#
+$(document).ready(function(){
+    if ($('#show-your-messages').length > 0) {
+        $.ajax({
+           type: "GET",
+           url: "@@your-messages"
+           success: function(html) {
+               $('#show-your-messages').replaceWith(html);
+           }
+        });
+    }
+});
