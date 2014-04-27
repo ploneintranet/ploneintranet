e0f17e325151c1749547d412f8610ed5442ed3f3
Adam Castle <acastle@ripe.net>
2014-04-27 17:15:33 +0200

    background image for your messages

--- src/plonesocial/messaging/browser/javascript/messaging.js
+++ src/plonesocial/messaging/browser/javascript/messaging.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     if ($('#show-your-messages').length > 0) {
         $.ajax({
            type: "GET",
-           url: "@@your-messages"
+           url: "@@your-messages",
            success: function(html) {
                $('#show-your-messages').replaceWith(html);
            }
