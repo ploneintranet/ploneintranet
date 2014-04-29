4c622ed148bea3aa902df7c381f4f95eadc1655c
Rapolas K <rapolas@netsight.co.uk>
2014-04-29 17:01:15 +0100

    added test to check if user can be added to workspace

--- src/ploneintranet/workspace/tests/base.py
+++ src/ploneintranet/workspace/tests/base.py
@@ -15,6 +15,7 @@ class BaseTestCase(unittest.TestCase):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
         self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
+        self.request = self.portal.REQUEST
 
     def login_as_portal_owner(self):
         z2.login(self.app['acl_users'], SITE_OWNER_NAME)
