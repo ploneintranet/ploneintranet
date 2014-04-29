9f9145bc1b08148d7db8a306ff40d1a2d5bfcae0
Matthew Sital-Singh <mattss@netsight.co.uk>
2014-04-29 17:28:19 +0100

    add tests for open and secret workspaces

--- src/ploneintranet/workspace/tests/test_workspace_workflow.py
+++ src/ploneintranet/workspace/tests/test_workspace_workflow.py
@@ -26,10 +26,8 @@ class TestWorkSpaceWorkflow(BaseTestCase):
             username='testuser1',
             obj=workspace_folder,
         )
-        # Normal users can view
         self.assertIn('View', permissions,
                       'Non-member cannot view private workspace')
-        # ... but not get access to
         self.assertNotIn('Access contents information', permissions,
                          'Non-member can access contents of private workspace')
 
@@ -46,4 +44,84 @@ class TestWorkSpaceWorkflow(BaseTestCase):
                       'Non-member cannot view private workspace')
         # ... and get access to
         self.assertIn('Access contents information', member_permissions,
-                      'Non-member can access contents of private workspace')
+                      'Non-member cannot access contents of private workspace')
+
+    def test_secret_workspace(self):
+        """
+        Secret workspaces should be invisible to all but members
+        """
+        self.login_as_portal_owner()
+        workspace_folder = api.content.create(
+            self.portal,
+            'ploneintranet.workspace.workspacefolder',
+            'example-workspace',
+            title='A secret workspace')
+
+        # Ssssh...
+        api.content.transition(workspace_folder,
+                               'make_secret')
+
+        api.user.create(username='testuser1', email="test@test.com")
+        permissions = api.user.get_permissions(
+            username='testuser1',
+            obj=workspace_folder,
+        )
+        self.assertNotIn('View', permissions,
+                         'Non-member can view secret workspace')
+        self.assertNotIn('Access contents information', permissions,
+                         'Non-member can access contents of secret workspace')
+
+        IWorkspace(workspace_folder).add_to_team(
+            user=api.user.get('testuser1')
+        )
+
+        member_permissions = api.user.get_permissions(
+            username='testuser1',
+            obj=workspace_folder,
+        )
+        # Normal users can view
+        self.assertIn('View', member_permissions,
+                      'Non-member cannot view secret workspace')
+        # ... and get access to
+        self.assertIn('Access contents information', member_permissions,
+                      'Non-member cannot access contents of secret workspace')
+
+    def test_open_workspace(self):
+        """
+        Open workspaces should be visible
+        and accessible to all users
+        """
+        self.login_as_portal_owner()
+        workspace_folder = api.content.create(
+            self.portal,
+            'ploneintranet.workspace.workspacefolder',
+            'example-workspace',
+            title='A secret workspace')
+
+        api.content.transition(workspace_folder,
+                               'make_open')
+
+        api.user.create(username='testuser1', email="test@test.com")
+        permissions = api.user.get_permissions(
+            username='testuser1',
+            obj=workspace_folder,
+        )
+        self.assertIn('View', permissions,
+                      'Non-member cannot view open workspace')
+        self.assertIn('Access contents information', permissions,
+                      'Non-member cannot access contents of open workspace')
+
+        IWorkspace(workspace_folder).add_to_team(
+            user=api.user.get('testuser1')
+        )
+
+        member_permissions = api.user.get_permissions(
+            username='testuser1',
+            obj=workspace_folder,
+        )
+        # Normal users can view
+        self.assertIn('View', member_permissions,
+                      'Member cannot view open workspace')
+        # ... and get access to
+        self.assertIn('Access contents information', member_permissions,
+                      'Member cannot access contents of open workspace')
