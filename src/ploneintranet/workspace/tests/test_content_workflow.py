6e5a65ae9ccab21278031f061a61ab409069703b
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2014-05-02 12:06:47 +0100

    remove unused imports

--- src/ploneintranet/workspace/tests/test_content_workflow.py
+++ src/ploneintranet/workspace/tests/test_content_workflow.py
@@ -1,7 +1,5 @@
-from collective.workspace.interfaces import IWorkspace
 from plone import api
 from ploneintranet.workspace.tests.base import BaseTestCase
-from zope.annotation.interfaces import IAnnotations
 
 
 class TestWorkflow(BaseTestCase):
