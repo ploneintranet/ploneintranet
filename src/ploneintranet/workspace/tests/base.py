a78ef7bd7eef408d82a037f4a3c5f0e2802f0fa7
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2014-05-02 15:50:11 +0100

    remove unused methods

--- src/ploneintranet/workspace/tests/base.py
+++ src/ploneintranet/workspace/tests/base.py
@@ -20,19 +20,18 @@ class BaseTestCase(unittest.TestCase):
         self.request = self.portal.REQUEST
 
     def login(self, username):
+        """
+        helper method to login as specific user
+
+        :param username: the username of the user to add to the group
+        :type username: str
+        :rtype: None
+
+        """
         login(self.portal, username)
 
     def login_as_portal_owner(self):
         z2.login(self.app['acl_users'], SITE_OWNER_NAME)
-
-    def login_as(self, username):
-        """
-        helper method to login as specific user
-        """
-        z2.login(self.portal['acl_users'], username)
-
-    def logout(self):
-        z2.logout()
 
     def add_user_to_workspace(self, username, workspace, groups=None):
         """
