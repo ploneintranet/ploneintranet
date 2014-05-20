211ed119f354cf6ebb006b8939599efd431549fa
Ben Cole <benc@netsight.co.uk>
2014-05-20 14:55:21 +0100

    Minor changes to token utility

--- src/ploneintranet/invitations/token_utility.py
+++ src/ploneintranet/invitations/token_utility.py
@@ -1,8 +1,13 @@
+from zope.interface import implements
+from .interfaces import ITokenUtility
+
 
 class TokenUtility(object):
     """
     Utility to generate and consume tokens
     """
+    implements(ITokenUtility)
+
     def get_new_token(self, uses=1, expiry=300):
         """
         Get a new unique token
@@ -17,46 +22,49 @@ class TokenUtility(object):
         """
         pass
 
-    def get_uses(self, token):
+    def get_uses(self, hash_str):
         """
         Get the number of uses remaining for a given token
 
-        :param token: The token hash
-        :type token: str
+        :param hash_str: The token hash
+        :type hash_str: str
         :return: The number of uses remaining or None if this token no longer
                  exists
         :rtype: int
         """
+        token = self._get_token(hash_str)
         pass
 
-    def get_expiry(self, token):
+    def get_expiry(self, hash_str):
         """
         Get the datetime of the expiry of a given token
 
-        :param token: The token hash
+        :param hash_str: The token hash
         :return: The datetime of expiry
         :rtype: datetime
         """
+        token = self._get_token(hash_str)
         pass
 
-    def _consume_token(self, token):
+    def _consume_token(self, hash_str):
         """
         Consume the given token and fire the event
 
-        :param token: The token hash
-        :type token: str
+        :param hash_str: The token hash
+        :type hash_str: str
         :return: Whether consumption succeeded or not. It will fail if the
                  token has expired
         :rtype: bool
         """
+        token = self._get_token(hash_str)
         pass
 
-    def _get_token(self, token):
+    def _get_token(self, hash_str):
         """
         Fetch token from storage
 
-        :param token: A token hash
-        :type token: str
+        :param hash_str: A token hash
+        :type hash_str: str
         :return: Token object
         """
         pass
