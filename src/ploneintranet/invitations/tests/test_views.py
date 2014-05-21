8dd2eb27cc80147a02b5751cb9a8c6d8166c6b7d
Matthew Sital-Singh <mattss@netsight.co.uk>
2014-05-21 16:53:00 +0100

    Added a resolveuid-style @@invitations view for accepting invitations

---
+++ src/ploneintranet/invitations/tests/test_views.py
@@ -0,0 +1,27 @@
+import unittest
+from zope.component import getUtility
+from zope.component import getMultiAdapter
+from ploneintranet.invitations.interfaces import ITokenUtility
+
+from ploneintranet.invitations.testing import \
+    PLONEINTRANET_INVITATIONS_INTEGRATION_TESTING
+
+
+class TestViews(unittest.TestCase):
+
+    layer = PLONEINTRANET_INVITATIONS_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.util = getUtility(ITokenUtility)
+
+    def test_invitation_view(self):
+        token_id = self.util.generate_new_token(usage_limit=1)
+        self.assertTrue(self.util.valid(token_id))
+
+        view = getMultiAdapter((self.portal, self.request),
+                               name=u'invitations')
+        view.publishTraverse(self.request, token_id)()
+        self.assertFalse(self.util.valid(token_id))
--- src/ploneintranet/invitations/views.py
+++ src/ploneintranet/invitations/views.py
@@ -1,8 +1,26 @@
+from zope.component import getUtility
+from zope.interface import implements
 from zope.publisher.browser import BrowserView
+from zope.publisher.interfaces import IPublishTraverse
+from ploneintranet.invitations.interfaces import ITokenUtility
 
 
-class ConsumeToken(BrowserView):
+class Invitation(BrowserView):
     """
     View that will be called to consume a token
+
+    Usage: /invitations/<token_id>
     """
-    pass
+    implements(IPublishTraverse)
+
+    token_id = None
+
+    def publishTraverse(self, request, name):
+        self.token_id = name
+        return self
+
+    def __call__(self):
+        if self.token_id is None:
+            raise KeyError("No token id given in sub-path. Use .../@@invitations/tokenid")
+        util = getUtility(ITokenUtility)
+        util._consume_token(self.token_id)
