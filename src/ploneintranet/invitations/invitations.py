446643dab47d7641df444d2ae5383d0db53c53f3
Ben Cole <benc@netsight.co.uk>
2014-05-22 15:09:36 +0100

    Document invite user view

--- src/ploneintranet/invitations/invitations.py
+++ src/ploneintranet/invitations/invitations.py
@@ -6,8 +6,17 @@ from plone import api
 
 
 class InviteUser(BrowserView):
+    """
+    View for sending invitation emails to potential new users of the Plone site
+    """
+    def invite_user(self, email):
+        """
+        Get new token, build email and send it to the given email address
 
-    def invite_user(self, email_address):
+        :param email: Email address to send invitation to, and for new user
+        :type email: str
+        :return:
+        """
         token_util = getUtility(ITokenUtility)
         token_id, token_url = token_util.generate_new_token()
         # TODO - send an email
@@ -15,6 +24,13 @@ class InviteUser(BrowserView):
 
 
 def accept_invitation(event):
+    """
+    Event handler for :class:`AcceptToken` event fired by invitation framework
+
+    :param event: The event object
+    :type event: :class:`AcceptToken`
+    :return:
+    """
     request = getRequest()
     acl_users = api.portal.get_tool('acl_users')
     username = 'bob@test.com'
