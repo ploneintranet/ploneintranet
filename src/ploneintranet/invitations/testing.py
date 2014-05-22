b58bf32df0093db84e1e8233be1cba3bddb9c85d
Ben Cole <benc@netsight.co.uk>
2014-05-22 15:46:13 +0100

    Added - collective.MockMailHost

--- src/ploneintranet/invitations/testing.py
+++ src/ploneintranet/invitations/testing.py
@@ -2,6 +2,7 @@ from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
+from plone.app.testing import helpers
 
 from plone.testing import z2
 
@@ -20,16 +21,27 @@ class PloneintranetinvitationsLayer(PloneSandboxLayer):
             ploneintranet.invitations,
             context=configurationContext
         )
+        import collective.MockMailHost
+
+        self.loadZCML(package=collective.MockMailHost)
+
+        # Install product and call its initialize() function
+        z2.installProduct(app, 'collective.MockMailHost')
 
     def tearDownZope(self, app):
-        pass
+        z2.uninstallProduct(app, 'collective.MockMailHost')
+
+    def setUpPloneSite(self, portal):
+        helpers.quickInstallProduct(portal, 'collective.MockMailHost')
+
+        helpers.applyProfile(portal, 'collective.MockMailHost:default')
 
 PLONEINTRANET_INVITATIONS_FIXTURE = PloneintranetinvitationsLayer()
 PLONEINTRANET_INVITATIONS_INTEGRATION_TESTING = IntegrationTesting(
     bases=(PLONEINTRANET_INVITATIONS_FIXTURE,),
-    name="PloneintranetinvitationsLayer:Integration"
+    name='PloneintranetinvitationsLayer:Integration'
 )
 PLONEINTRANET_INVITATIONS_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(PLONEINTRANET_INVITATIONS_FIXTURE, z2.ZSERVER_FIXTURE),
-    name="PloneintranetinvitationsLayer:Functional"
+    name='PloneintranetinvitationsLayer:Functional'
 )
