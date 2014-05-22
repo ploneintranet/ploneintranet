03ac7ba892706f0be2ecf307efd31c38456f300a
Ben Cole <benc@netsight.co.uk>
2014-05-22 16:15:10 +0100

    Removed - collective.MockMailHost (funtimes)

--- src/ploneintranet/invitations/testing.py
+++ src/ploneintranet/invitations/testing.py
@@ -2,7 +2,6 @@ from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
-from plone.app.testing import helpers
 
 from plone.testing import z2
 
@@ -21,20 +20,10 @@ class PloneintranetinvitationsLayer(PloneSandboxLayer):
             ploneintranet.invitations,
             context=configurationContext
         )
-        import collective.MockMailHost
-
-        self.loadZCML(package=collective.MockMailHost)
-
-        # Install product and call its initialize() function
-        z2.installProduct(app, 'collective.MockMailHost')
 
     def tearDownZope(self, app):
-        z2.uninstallProduct(app, 'collective.MockMailHost')
+        pass
 
-    def setUpPloneSite(self, portal):
-        helpers.quickInstallProduct(portal, 'collective.MockMailHost')
-
-        helpers.applyProfile(portal, 'collective.MockMailHost:default')
 
 PLONEINTRANET_INVITATIONS_FIXTURE = PloneintranetinvitationsLayer()
 PLONEINTRANET_INVITATIONS_INTEGRATION_TESTING = IntegrationTesting(
