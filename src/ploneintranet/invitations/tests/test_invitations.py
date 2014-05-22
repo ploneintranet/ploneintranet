1ef7f01e81239ba51f4b82dc95a7eeb12b1de17d
Matthew Sital-Singh <mattss@netsight.co.uk>
2014-05-22 15:19:43 +0100

    Fix test for invitation view so we don't use the magic user

--- src/ploneintranet/invitations/tests/test_invitations.py
+++ src/ploneintranet/invitations/tests/test_invitations.py
@@ -15,9 +15,10 @@ class TestInviteUser(unittest.TestCase):
         self.request = self.layer['request']
 
     def test_invite_user(self):
+        email = 'test@test.com'
         view = getMultiAdapter((self.portal, self.request),
                                name=u'invite-user')
-        token_id, token_url = view.invite_user('bob@test.com')
+        token_id, token_url = view.invite_user(email)
         # Commit so that the test browser sees these changes
         import transaction
         transaction.commit()
@@ -26,8 +27,12 @@ class TestInviteUser(unittest.TestCase):
         browser.open(token_url)
 
         # We should now be authenticated
-        self.assertIn('userrole-authenticated', browser.contents)
-        self.assertIn('bob@test.com', browser.contents)
+        self.assertIn('userrole-authenticated', browser.contents,
+                      'User was not authenticated after accepting token')
+        self.assertIn(
+            email, browser.contents,
+            'Incorrect user authenticated after accepting token',
+        )
 
         browser.open('%s/logout' % (
             self.portal.absolute_url(),
@@ -35,4 +40,5 @@ class TestInviteUser(unittest.TestCase):
         browser.open('%s/@@accept-token/thisisnotatoken' % (
             self.portal.absolute_url(),
         ))
-        self.assertNotIn('userrole-authenticated', browser.contents)
+        self.assertNotIn('userrole-authenticated', browser.contents,
+                         'Invalid token should not allow access')
