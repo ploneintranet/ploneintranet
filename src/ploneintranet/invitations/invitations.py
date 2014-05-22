a771ff3b36173f0a0a8b22dcb6cd39e36e6f2b37
Ben Cole <benc@netsight.co.uk>
2014-05-22 16:05:31 +0100

    More implementation of invitations
    Added - annotation storage for storing invitation
    Added - email the token URL to the invitee

--- src/ploneintranet/invitations/invitations.py
+++ src/ploneintranet/invitations/invitations.py
@@ -1,8 +1,15 @@
+from Products.CMFCore.interfaces import ISiteRoot
+from zope.annotation import IAnnotations
 from zope.component import getUtility
-from ploneintranet.invitations.interfaces import ITokenUtility
 from Products.Five import BrowserView
 from zope.globalrequest import getRequest
 from plone import api
+from BTrees.OOBTree import OOBTree
+
+from ploneintranet.invitations.interfaces import ITokenUtility
+
+
+ANNOTATION_KEY = 'ploneintranet.invitations.invitation_storage'
 
 
 class InviteUser(BrowserView):
@@ -19,7 +26,12 @@ class InviteUser(BrowserView):
         """
         token_util = getUtility(ITokenUtility)
         token_id, token_url = token_util.generate_new_token()
-        # TODO - send an email
+        _store_invite(token_id, email)
+        api.portal.send_email(
+            recipient=email,
+            subject='Please join my Plone site',
+            body=token_url
+        )
         return token_id, token_url
 
 
@@ -31,17 +43,32 @@ def accept_invitation(event):
     :type event: :class:`AcceptToken`
     :return:
     """
+    email = _get_storage().get(event.token_id)
+    if email is None:
+        return
     request = getRequest()
     acl_users = api.portal.get_tool('acl_users')
-    username = 'bob@test.com'
-    if api.user.get(username=username) is None:
+    if api.user.get(username=email) is None:
         api.user.create(
-            email=username,
-            username=username,
+            email=email,
+            username=email,
         )
     acl_users.updateCredentials(
         request,
         request.response,
-        username,
+        email,
         None
     )
+
+
+def _get_storage(clear=False):
+    portal = getUtility(ISiteRoot)
+    annotations = IAnnotations(portal)
+    if ANNOTATION_KEY not in annotations or clear:
+        annotations[ANNOTATION_KEY] = OOBTree()
+    return annotations[ANNOTATION_KEY]
+
+
+def _store_invite(token_id, email):
+    storage = _get_storage()
+    storage[token_id] = email
