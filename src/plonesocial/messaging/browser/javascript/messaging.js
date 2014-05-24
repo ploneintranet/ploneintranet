6c86a63538c965030bf9d0c5bfc0d837a548bce0
Adam Castle <acastle@ripe.net>
2014-05-24 19:09:04 +0200

    tiyding up the bindings of the ajax query

--- src/plonesocial/messaging/browser/javascript/messaging.js
+++ src/plonesocial/messaging/browser/javascript/messaging.js
@@ -1,5 +1,6 @@
 
-function messaging_ajax (url, replaceid, datatype) {
+function messaging_ajax (url, replaceid, datatype, contentid) {
+    // default ajax query
     $.ajax({
        type: "GET",
        dataType: datatype,
@@ -9,14 +10,12 @@ function messaging_ajax (url, replaceid, datatype) {
                if (replaceid == '#personaltools-plone_social_menu') {
                    add_inbox_count(data);
                }
-               else if (replaceid == '#your-messages .messages') {
-                   $(replaceid).replaceWith($(data).find('#content').html());
+               else if (contentid) {
+                   $(replaceid).append($(data).find('#content').html());
                }
                else {
                    $(replaceid).replaceWith(data);
                }
-               send_new_message();
-               message_click();
            }
        }
     });
@@ -24,33 +23,49 @@ function messaging_ajax (url, replaceid, datatype) {
 
 
 function message_click() {
-    $('#your-messages-icon').one('click', function(){
-        $('#your-messages .messages').empty();
-        messaging_ajax('@@social-inbox?view=small', '#your-messages .messages', 'html');
-        $('#your-messages').toggle();
-        send_new_message();
-    });
+    // show mini view of the inbox
+    $('#your-messages .messages').empty();
+    messaging_ajax('@@social-inbox?view=small', '#your-messages .messages', 'html', true);
+    $('#your-messages').toggle();
 }
 
 
 function add_inbox_count(data) {
+    // simple inbox number look up
     var msg_count = data.trim();
     $('#personaltools-plone_social_menu a').append(" (" + msg_count + ")");
 }
 
+
 function send_new_message(){
+    // create overlay of send messages
     $('#inbox-new-message a').prepOverlay({
         subtype: 'ajax',
         filter: '#content > *',
-        });
+    });
+}
+
+
+function messages_full() {
+    // show all messages
+    $('#conversation-content').empty();
+    messaging_ajax('@@messaging-send', '#conversation-content', 'html', true);
+    return false;
 }
 
 
 $(document).ready(function(){
+
+    $('#inbox-new-message-full').live('click', messages_full);
+
     if ($('#personaltools-plone_social_menu').length > 0) {
+        // if there is a social menu link, then add number of unread messages
         messaging_ajax('@@your-messages?count=true', '#personaltools-plone_social_menu', 'html');
+
     }
     if ($('#show-your-messages').length > 0) {
+        // if there is a inbox icon, then load the messages inbox details
         messaging_ajax('@@your-messages', '#show-your-messages', 'html');
+        $('#your-messages-icon').live('click', message_click);
     }
 });
