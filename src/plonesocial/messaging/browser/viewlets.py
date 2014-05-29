07308d633d67eb731ce3c36071c9b34facc066bf
hvelarde <hector.velarde@gmail.com>
2014-05-29 19:30:52 -0300

    Show notification viewlet only when the user has new messages (fixes #2)

--- src/plonesocial/messaging/browser/viewlets.py
+++ src/plonesocial/messaging/browser/viewlets.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-
+from plone import api
 from plone.app.layout.viewlets.common import ViewletBase
+from plonesocial.messaging.interfaces import IMessagingLocator
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getUtility
 
 
 class NotificationsViewlet(ViewletBase):
@@ -10,4 +12,11 @@ class NotificationsViewlet(ViewletBase):
 
     def available(self):
         """Return True if the viewlet is available."""
-        return True
+        locator = getUtility(IMessagingLocator)
+        inboxes = locator.get_inboxes()
+        username = api.user.get_current().getUserName()
+        if username in inboxes:
+            conversation = inboxes[username]
+            return conversation.new_messages_count > 0
+        else:
+            return False
