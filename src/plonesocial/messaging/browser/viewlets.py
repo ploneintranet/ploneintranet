b8f10f4ecd7125f07374bfb561c5258ef8095213
hvelarde <hector.velarde@gmail.com>
2014-06-05 19:27:32 -0300

    Show notifications viewlet always (refs. #4)

--- src/plonesocial/messaging/browser/viewlets.py
+++ src/plonesocial/messaging/browser/viewlets.py
@@ -1,22 +1,8 @@
 # -*- coding: utf-8 -*-
-from plone import api
 from plone.app.layout.viewlets.common import ViewletBase
-from plonesocial.messaging.interfaces import IMessagingLocator
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.component import getUtility
 
 
 class NotificationsViewlet(ViewletBase):
 
     index = ViewPageTemplateFile('notifications.pt')
-
-    def available(self):
-        """Return True if the user has new messages on her inbox."""
-        locator = getUtility(IMessagingLocator)
-        inboxes = locator.get_inboxes()
-        username = api.user.get_current().getUserName()
-        if username in inboxes:
-            conversation = inboxes[username]
-            return conversation.new_messages_count > 0
-        else:
-            return False
