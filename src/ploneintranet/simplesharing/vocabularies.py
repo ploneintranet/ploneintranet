ba20370ab31a9dc928e9deae9449c711fa6dca6a
Ben Cole <benc@netsight.co.uk>
2014-06-10 16:06:00 +0100

    Added - Vocab for visibility selection

--- src/ploneintranet/simplesharing/vocabularies.py
+++ src/ploneintranet/simplesharing/vocabularies.py
@@ -1,8 +1,18 @@
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 
+WORKFLOW_MAPPING = {
+    'private': 'draft',
+    'limited': 'draft',
+    'published': 'published',
+    'public': 'public'
+}
+
+
 def workflow_states_vocab(context):
     return SimpleVocabulary([
-        SimpleTerm(u'thingy'),
-        SimpleTerm(u'mabob'),
+        SimpleTerm(u'private', u'private', u'Only visible to me'),
+        SimpleTerm(u'limited', u'limited', u'Share with selected users'),
+        SimpleTerm(u'published', u'published', u'Shared with my team'),
+        SimpleTerm(u'public', u'public', u'Shared with all Intranet users')
     ])
