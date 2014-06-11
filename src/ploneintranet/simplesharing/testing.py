0cac54460243682dfd5c9e510dbcbb1370d22079
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2014-06-11 11:32:16 +0100

    remove unused import

--- src/ploneintranet/simplesharing/testing.py
+++ src/ploneintranet/simplesharing/testing.py
@@ -1,5 +1,4 @@
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import applyProfile
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
