2cada500edc8e180ef2f915ed2d07d8f370ea451
Ben Cole <benc@netsight.co.uk>
2014-06-12 17:02:52 +0100

    Added - More tests to increase coverage

--- src/ploneintranet/simplesharing/tests/test_simplesharing.py
+++ src/ploneintranet/simplesharing/tests/test_simplesharing.py
@@ -48,18 +48,43 @@ class TestBehaviors(BaseTestCase):
         alsoProvides(request, IAttributeAnnotatable)
         return request
 
-    def test_visibility(self):
+    def simple_share(self, users, visibility='private'):
         request = self.make_request(
-            visibility='published',
-            share_with=[]
+            visibility=visibility,
+            share_with=users
         )
 
-        shareform = api.content.get_view('simple-sharing',
-                                         context=self.doc,
-                                         request=request)
+        shareform = api.content.get_view(
+            'simple-sharing',
+            context=self.doc,
+            request=request
+        )
         shareform.update()
+        return shareform.extractData()
 
-        data, errors = shareform.extractData()
+    def test_visibility(self):
+        # Test empty form
+        data, errors = self.simple_share([], '')
+        self.assertEqual(len(errors), 0)
+        self.assertEqual(api.content.get_state(self.doc), 'private')
+        self.assertEqual(self.doc.users_with_local_role('Reader'), [])
+
+        # Test validation error
+        data, errors = self.simple_share(['foobar'], 'published')
+        self.assertEqual(len(errors), 1)
+        # State and shared with should remain unchanged
+        self.assertEqual(api.content.get_state(self.doc), 'private')
+        self.assertEqual(self.doc.users_with_local_role('Reader'), [])
+
+        # Test invalid workflow state
+        data, errors = self.simple_share(['user1'], 'foobar')
+        self.assertEqual(len(errors), 1)
+        # State and shared with should remain unchanged
+        self.assertEqual(api.content.get_state(self.doc), 'private')
+        self.assertEqual(self.doc.users_with_local_role('Reader'), [])
+
+        # Test "Share with all" (publish)
+        data, errors = self.simple_share([], 'published')
         self.assertEqual(len(errors), 0)
         self.assertEqual(api.content.get_state(self.doc), 'published')
         self.assertEqual(self.doc.users_with_local_role('Reader'), [])
@@ -70,19 +95,53 @@ class TestBehaviors(BaseTestCase):
             username='user1',
             password='12345'
         )
-
-        request = self.make_request(
-            visibility='private',
-            share_with='user1'
+        api.user.create(
+            email='test2@user.co',
+            username='user2',
+            password='12345'
+        )
+        api.user.create(
+            email='test3@user.co',
+            username='user3',
+            password='12345'
         )
 
-        shareform = api.content.get_view('simple-sharing',
-                                         context=self.doc,
-                                         request=request)
-        shareform.update()
-        data, errors = shareform.extractData()
+        # Test sharing with one user
+        data, errors = self.simple_share(['user1'])
         self.assertEqual(len(errors), 0)
         self.assertEqual(api.content.get_state(self.doc), 'private')
         self.assertEqual(self.doc.users_with_local_role('Reader'),
                          ['user1'],
                          'User1 has not been given the Reader role')
+
+        # Test sharing with 2 users
+        self.simple_share(['user1', 'user2'])
+        self.assertEqual(
+            self.doc.users_with_local_role('Reader'),
+            ['user2', 'user1'],
+            'User1 and User2 have not both been given the Reader role'
+        )
+
+        # Test replacing existing shared users with a new one
+        self.simple_share(['user3'])
+        self.assertEqual(
+            self.doc.users_with_local_role('Reader'),
+            ['user3'],
+            'User3 has not replaced the previously shared users'
+        )
+
+        # Test remove all shares
+        self.simple_share([])
+        self.assertEqual(
+            len(self.doc.users_with_local_role('Reader')),
+            0,
+            'Users have not been removed from sharing'
+        )
+
+        # Test share with None
+        self.simple_share(None)
+        self.assertEqual(
+            len(self.doc.users_with_local_role('Reader')),
+            0,
+            'Some users have magically been added'
+        )
