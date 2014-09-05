93ff3a79ba250696c99d0a9be799a43f0937de1d
Alexander Pilz <pilz@syslab.com>
2014-09-05 10:47:04 +0200

    reworking theme to integrate more plone elements. Starting to override viewlets

---
+++ src/ploneintranet/theme/interfaces.py
@@ -0,0 +1,5 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+
+class IThemeSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer and a plone skin marker.
+    """
