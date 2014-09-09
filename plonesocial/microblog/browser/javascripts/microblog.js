2d5924ef032e48dbf9efec798e6e60bd2ec3eb06
Adam Castle <acastle@ripe.net>
2014-09-09 15:25:58 +0200

    changes JS to work with current selected-user id

--- plonesocial/microblog/browser/javascripts/microblog.js
+++ plonesocial/microblog/browser/javascripts/microblog.js
@@ -1,8 +1,10 @@
 // microblog js file
 
 
-function add_to_post(input_el) {
+function add_to_post(input_el, tag_type) {
     // append comments textarea from a given input name
+    // function expects input element (name) and appendable element
+    // i.e. '#selected-tags' or ''#selected-user'
     var inp = 'input[name="'+input_el+'"]';
     if ($(inp).length > 0) {
         $(inp).keypress(function(ev) {
@@ -10,14 +12,8 @@ function add_to_post(input_el) {
                 // only do this on the return key
                 var tag = $(this).val();
                 if (tag) {
-                    var comments = $('.pat-comment-box').val();
-                    var val = '#' + tag;
-                    if (comments) {
-                        // if a comment already exists then append
-                        // add a space
-                        val = comments + ' ' + val;
-                    }
-                    $('.pat-comment-box').val(val);
+                    var link = $('<a></a>');
+                    $(tag_type).append($(link).text('#'+tag));
                 }
             }
         });
@@ -29,6 +25,6 @@ $(document).ready(function() {
 
     $('.icon-tags').click(function() {
         // have to use settimeout for the time being to wait for ajax return!
-        setTimeout(function(){add_to_post('tagsearch')}, 500);
+        setTimeout(function(){add_to_post('tagsearch', '#selected-tags')}, 500);
     });
 });
