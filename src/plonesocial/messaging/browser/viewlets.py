3b0220696c5c566bd7b6ce42758b202924bed006
Adam Castle <acastle@ripe.net>
2014-09-09 18:26:49 +0200

    import messaging count to viewlet

--- src/plonesocial/messaging/browser/viewlets.py
+++ src/plonesocial/messaging/browser/viewlets.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+
+from zope.component import getUtility
+
+from plonesocial.messaging.interfaces import IMessagingLocator
 
 
 class NotificationsViewlet(ViewletBase):
 
     index = ViewPageTemplateFile('notifications.pt')
+
+    def number_of_messages(self):
+        # return number of messages
+        user = api.user.get_current()
+        if not user:
+            return None
+
+        locator = getUtility(IMessagingLocator)
+        inboxes = locator.get_inboxes()
+
+        if user.id not in inboxes:
+            return None
+
+        messages = inboxes[user.id]
+        return messages.new_messages_count
