1f357322dcfca6f52da3779d17f9568c50ac3d19
Guido A.J. Stevens <guido.stevens@cosent.nl>
2014-09-12 11:37:24 +0200

    initialize setup profiles with browser layer

---
+++ src/plonesocial/core/browser/interfaces.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+
+# from zope.i18nmessageid import MessageFactory
+# _ = MessageFactory('plonesocial.core')
+
+
+class IPlonesocialCoreLayer(Interface):
+    """Marker interface to define ZTK browser layer"""
