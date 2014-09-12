38790b403c9239c296fa342b8d7ac75d33e268f7
Bogdan Girman <bogdan.girman@gmail.com>
2014-09-12 13:37:25 +0200

    Move links utils from plonesocial.activitystream

---
+++ src/plonesocial/core/browser/utils.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from plone import api
+
+import re
+
+
+TAGRE = re.compile('(#(\S+))')
+USERRE = re.compile('(@\S+)')
+
+
+def link_tags(text, url=''):
+    tmpl = '<a href="%s/@@stream/tag/\\2" class="tag tag-\\2">\\1</a>'
+    return TAGRE.sub(tmpl % url, text)
+
+
+def link_users(text, url=''):
+    user_tmpl = '<a href="{0}/@@profile/{1}" class="user user-{1}">@{2}</a>'
+    user_marks = USERRE.findall(text)
+    for user_mark in user_marks:
+        user_id = user_mark[1:]
+        user = api.user.get(username=user_id)
+        if user:
+            user_fullname = user.getProperty('fullname', '') or user_id
+            text = re.sub(
+                user_mark,
+                user_tmpl.format(
+                    url,
+                    user_id,
+                    user_fullname),
+                text
+            )
+    return text
