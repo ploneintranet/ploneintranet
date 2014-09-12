2d36bcbded56a6895a6ce18ea75334d1a119a953
Guido A.J. Stevens <guido.stevens@cosent.nl>
2014-09-12 17:25:43 +0000

    merge berlin

--- src/ploneintranet/theme/interfaces.py
+++ src/ploneintranet/theme/interfaces.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 from plone.theme.interfaces import IDefaultPloneLayer
 
+
 class IThemeSpecific(IDefaultPloneLayer):
-    """Marker interface that defines a Zope 3 browser layer and a plone skin marker.
+    """Marker interface that defines a Zope 3 browser layer and a plone skin
+    marker.
     """
---
+++ src/ploneintranet/theme/static/README.md
@@ -0,0 +1,5 @@
+# Plone white label intranet
+
+To edit the diazo template "home.html" DO NOT EDIT THE generated/home.html.
+Instead, edit ploneintranet.theme/prototype/_layouts/home.html and run 'make release'.
+
