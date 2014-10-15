7bbd031eb5478ca16f1b0071293166a22fce67de
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-10-15 16:31:20 +0200

    default preview

--- src/ploneintranet/documentviewer/tests/test_views.py
+++ src/ploneintranet/documentviewer/tests/test_views.py
@@ -3,6 +3,7 @@ import unittest
 from plone import api
 from ploneintranet.documentviewer.testing import \
     PLONEINTRANET_documentviewer_INTEGRATION_TESTING
+import os
 
 
 class TestViews(unittest.TestCase):
@@ -13,24 +14,39 @@ class TestViews(unittest.TestCase):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
         self.request = self.layer['request']
-        import pdb;pdb.set_trace()
 
-    def test_storage(self):
-        '''
-        '''
-        view = api.content.get_view(
+        with api.env.adopt_roles(['Manager']):
+            self.image = api.content.create(
+                self.portal,
+                'Image',
+                'test_image',
+                image=open(os.path.join(os.path.dirname(__file__), 'test.png')),  # noqa
+            )
+        self.image_view = api.content.get_view(
+            'document_preview',
+            self.image,
+            self.request,
+        )
+        self.portal_view = api.content.get_view(
             'document_preview',
             self.portal,
             self.request,
         )
-        view.thumbnail_storage
 
     def test_storage(self):
         '''
         '''
-        view = api.content.get_view(
-            'document_preview',
-            self.portal,
-            self.request,
+        self.assertEqual(self.image_view.thumbnail_storage, {})
+
+    def test_get_default_preview(self):
+        '''
+        '''
+
+        self.assertEqual(
+            self.image_view.get_default_preview(),
+            'http://nohost/plone/png.png'
         )
-        view.thumbnail_storage
+        self.assertEqual(
+            self.portal_view.get_default_preview(),
+            'http://nohost/plone/document.png'
+        )
--- src/ploneintranet/documentviewer/views.py
+++ src/ploneintranet/documentviewer/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser import BrowserView
 from persistent.dict import PersistentDict
+from plone import api
 from plone.memoize.view import memoize
 from zope.annotation.interfaces import IAnnotations
 
@@ -22,3 +23,17 @@ class Documentpreview(BrowserView):
             # BBB this is bad because will write data in the annotations
             annotations[self.thumbnail_storage_key] = PersistentDict()
         return annotations[self.thumbnail_storage_key]
+
+    def get_default_preview(self):
+        ''' Return
+        '''
+        navigation_root_url = api.content.get_view(
+            'plone_portal_state',
+            self.context,
+            self.request
+        ).navigation_root_url()
+        mt = api.portal.get_tool('mimetypes_registry')
+        ct = getattr(self.context, 'content_type')
+        for mtobj in mt.lookup(ct):
+            return '%s/%s' % (navigation_root_url, mtobj.icon_path)
+        return '%s/document.png' % navigation_root_url
