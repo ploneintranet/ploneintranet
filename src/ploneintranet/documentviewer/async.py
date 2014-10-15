8a5ff4d99f66489e655d1d7d15cc24c3b1fadb19
Simone Deponti <simone.deponti@gmail.com>
2014-10-15 19:33:18 +0200

    Refactored a bit of the async code.
    WARNING: the async part is not yet tested at all.
    Will work on a RabbitMQ test case looking at c.zamqp tests for hints.
    
    For now async is disabled as it is activated only when the
    [async] extra is pulled in.

--- src/ploneintranet/documentviewer/async.py
+++ src/ploneintranet/documentviewer/async.py
@@ -1,36 +1,74 @@
-import importlib
 from zope.interface import implementer
-from zope.interface import Interface
+from zope.component import getUtility
 from zope.component import queryUtility
+from zope.dottedname.resolve import resolve
 from five import grok  # It pains me so, but collective.zamqp uses it
 from collective.zamqp.interfaces import IProducer
 from collective.zamqp.interfaces import IMessageArrivedEvent
 from collective.zamqp.producer import Producer
 from collective.zamqp.consumer import Consumer
 from .interfaces import IExecutor
+from .interfaces import IAsyncTask
+from .interfaces import IMarshaller
 
 
-def load(dotted_name):
-    module_name, func_name = dotted_name.rsplit('.', 1)
-    module = importlib.import_module(module_name)
-    return getattr(module, func_name)
+DEFAULT_KEY = 'ploneintranet.documentviewer.async.default'
 
 
 def get_name(func):
+    # Refactor this so it could work with static and class methods too.
     return func.__module__+func.__name__
 
 
-class IAsyncTask(Interface):
-    """Marker interface for async class
+class DefaultProducer(Producer):
+    """Produces requests for async tasks
     """
+    # Note: if we want to provide dynamic queue names we must see
+    # that the consumer is registered as an utility for that queue name.
+    # Basically, once message is received from queue 'x.y', c.zamqp
+    # does a queryUtility(IConsumer, 'x.y')
+    grok.name(DEFAULT_KEY)
+    queue = DEFAULT_KEY
+    connection_id = "async"
+    serializer = "application/x-python-serialize"
+    durable = False
+
+
+class DefaultConsumer(Consumer):
+    """Consumes requests for async tasks
+    """
+    grok.name(DEFAULT_KEY)
+    connection_id = "async"
+    marker = IAsyncTask
+    durable = False
+
+
+@implementer(IMarshaller)
+class BaseMarshaller(object):
+    """Serializes and deserializes objects.
+
+    It is also the default marshaller for function (a no-op).
+    """
+
+    @property
+    def mkey(self):
+        self.mkey = self.__class__.__module__+'.'+self.__class__.__name__
+
+    def marshal(self, *args, **kwargs):
+        # No-op
+        return (args, kwargs)
+
+    def unmarshal(self, *marshalled_args, **marshalled_kwargs):
+        # No-op
+        return (marshalled_args, marshalled_kwargs)
 
 
 @grok.subscribe(IAsyncTask, IMessageArrivedEvent)
 def consumeMessage(message, event):
-    """Consume message"""
+    """Consume message
+    """
     executor = queryUtility(IExecutor)
-    args, kwargs = executor.unmarshal(message.header_frame.__dict__)
-    func = load(message.body)
+    func, args, kwargs = executor.unmarshal(*message.body)
     func(*args, **kwargs)
     message.ack()
 
@@ -40,26 +78,44 @@ class AsyncExecutor(object):
     """This is an utility
     """
 
-    def marshal(self, *args, **kwargs):
+    def marshal(self, func, args, kwargs):
         """Makes sure we do serialize items in a way that can pass through amqp.
-        This means strings.
+        This means pickleable objects.
         """
-        dict_ = {}
-        # Do magic stuff that turns things into strings: could be IUUID(object) or pickling or crazy stuff.
-        # It's probably going to be crazy stuff.
-        # It must return a dictionary that is then passed to unmarshal()
-        return dict_
+        func_name = get_name(func)
+        marshaller = queryUtility(IMarshaller, name=func_name)
+        if marshaller is None:
+            # Default marshaller
+            marshaller = queryUtility(IMarshaller)
+        marshalled_args, marshalled_kwargs = marshaller.marshal(
+            *args,
+            **kwargs
+        )
+        return (
+            marshaller.mkey,
+            func_name,
+            marshalled_args,
+            marshalled_kwargs
+        )
 
-    def unmarshal(self, dict_):
-        # More magic stuff
-        return (args, kwargs)
+    def unmarshal(self, mkey, func_name, marshalled_args, marshalled_kwargs):
+        """Does the reverse operation of ``marshal``
+        """
+        Marshaller = resolve(mkey)
+        marshaller = Marshaller()
+        func = resolve(func_name)
+        args, kwargs = marshaller.unmarshal(
+            *marshalled_args,
+            **marshalled_kwargs
+        )
+        return (func, args, kwargs)
 
     def run(self, func, *args, **kwargs):
         # We do check if there is a custom producer here
         producer = queryUtility(IProducer, name=get_name(func))
         if producer is None:
-            producer = Producer(
-                # Creates an anonymous producer on-the-fly
-            )
-        producer._register()  # publish only after the transaction has succeeded
-        producer.publish(get_name(func), **self.marshal(args, kwargs))
+            producer = getUtility(IProducer, name=DEFAULT_KEY)
+        # publish only after the transaction
+        # has succeeded
+        producer._register()
+        producer.publish(self.marshal(func, args, kwargs))
