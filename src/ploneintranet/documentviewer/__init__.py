2e542138e112d8c721fe187c495228deb5ea979d
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-10-15 14:40:15 +0200

    first commit

---
+++ src/ploneintranet/documentviewer/__init__.py
@@ -0,0 +1,7 @@
+from zope.i18nmessageid import MessageFactory
+
+MessageFactory = MessageFactory('ploneintranet')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
---
+++ src/ploneintranet/documentviewer/testing.py
@@ -0,0 +1,39 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import helpers
+
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+
+
+class PloneintranetdocumentviewerLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import ploneintranet.documentviewer
+        xmlconfig.file(
+            'configure.zcml',
+            ploneintranet.documentviewer,
+            context=configurationContext
+        )
+
+    def tearDownZope(self, app):
+        pass
+
+    def setUpPloneSite(self, portal):
+        helpers.applyProfile(portal, 'ploneintranet.documentviewer:default')
+
+PLONEINTRANET_documentviewer_FIXTURE = PloneintranetdocumentviewerLayer()
+PLONEINTRANET_documentviewer_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONEINTRANET_documentviewer_FIXTURE,),
+    name='PloneintranetdocumentviewerLayer:Integration'
+)
+PLONEINTRANET_documentviewer_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(PLONEINTRANET_documentviewer_FIXTURE, z2.ZSERVER_FIXTURE),
+    name='PloneintranetdocumentviewerLayer:Functional'
+)
