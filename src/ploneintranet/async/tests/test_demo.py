295a69d6e9b809fb2615d1b8a1fe077574616fec
Simone Deponti <simone.deponti@gmail.com>
2014-11-05 11:56:54 +0100

    Added example test.

---
+++ src/ploneintranet/async/tests/test_demo.py
@@ -0,0 +1,60 @@
+import unittest
+import transaction
+from plone.uuid.interfaces import IUUID
+from plone.app.testing import login
+from plone.app.testing import logout
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from ..testing import PLONEINTRANET_async_FUNCTIONAL_TESTING
+from ..demo.tasks import create_content
+
+
+class TestContentCreation(unittest.TestCase):
+
+    layer = PLONEINTRANET_async_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def tearDown(self):
+        setRoles(self.portal, TEST_USER_ID, ['Member'])
+
+    def test_content_creation(self):
+        login(self.portal, TEST_USER_ID)
+        self.assertNotIn('foo', self.portal)
+        result = create_content.delay(
+            self.portal,
+            'Document',
+            'foo',
+            setTitle=u"Foo",
+            setDescription=u"Just a test page",
+            setText=u"<p>Hello <em>world</em>!</p>"
+        )
+        self.assertNotIn('foo', self.portal)
+        transaction.commit()
+        result = result.get()
+        self.assertIn('foo', self.portal)
+        self.assertEqual(result, IUUID(self.portal['foo']))
+        logout()
+
+    def test_content_creation_in_dir(self):
+        login(self.portal, TEST_USER_ID)
+        self.portal.invokeFactory('Folder', 'test-f')
+        folder = self.portal['test-f']
+        folder.setTitle('Test folder')
+        self.assertNotIn('foo', folder)
+        result = create_content.delay(
+            folder,
+            'Document',
+            'foo',
+            setTitle=u"Foo",
+            setDescription=u"Just a test page",
+            setText=u"<p>Hello <em>world</em>!</p>"
+        )
+        self.assertNotIn('foo', folder)
+        transaction.commit()
+        result = result.get()
+        self.assertIn('foo', folder)
+        self.assertEqual(result, IUUID(folder['foo']))
+        logout()
