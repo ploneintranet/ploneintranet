f840dc02a0f83a956f3ac799e794a1683e207561
JC Brand <jc@opkode.com>
2014-11-06 13:36:38 +0100

    Enable the custom template. refs #243

--- src/ploneintranet/theme/browser/document.py
+++ src/ploneintranet/theme/browser/document.py
@@ -3,15 +3,30 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class AddForm(add.DefaultAddForm):
-    xtemplate = ViewPageTemplateFile('templates/add_document.pt')
+    template = ViewPageTemplateFile('templates/add_document.pt')
 
     def render(self):
+        """ The "contents" attribute of the AddView gets populated with the
+            return results of this method.
+        """
         return super(AddForm, self).render()
 
+    def updat(self):
+        return super(AddForm, self).update()
+
+    def updateWidgets(self):
+        return super(AddForm, self).updateWidgets()
+
     def extractData(self, setErrors=True):
-        """See interfaces.IForm"""
         return super(AddForm, self).extractData()
 
 
 class AddView(add.DefaultAddView):
     form = AddForm
+
+    def __init__(self, context, request, ti):
+        return super(AddView, self).__init__(context, request, ti)
+
+    def update(self):
+        return super(AddView, self).update()
+
