9bf82531c8596a5c276ba2af4c0fd09ac7d1d996
JC Brand <jc@opkode.com>
2014-11-06 13:51:42 +0100

    Move example views to examples.py module.

---
+++ src/ploneintranet/theme/browser/examples.py
@@ -0,0 +1,43 @@
+from Acquisition import aq_inner
+from datetime import datetime
+from datetime import timedelta
+from plone import api
+from zExceptions import NotFound
+from zope.publisher.browser import BrowserPage
+from zope.publisher.browser import BrowserView
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class ExampleDragAndDropUpload(BrowserView):
+    """ A view to serve as an example of drag & drop uploading.
+    """
+
+
+class ExampleResourcePolling(BrowserView):
+    """ A view to serve as an example of how to use pat-resourcepolling
+    """
+
+    def get_images(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        return catalog(portal_type='Image', path=self.context.getPhysicalPath())
+
+
+class ExampleFilePreview(BrowserPage):
+    """ A view to serve as an example of file previews (e.g. via docconv)
+    """
+
+    def __call__(self, *args, **kw):
+        seconds = 10;
+        context = aq_inner(self.context)
+        if not hasattr(context, '_v_wait'):
+            context._v_wait = datetime.now() + timedelta(seconds=seconds)
+            log.info("Set waiting time to %s seconds" % seconds)
+        if context._v_wait <= datetime.now():
+            log.info("Waiting time exceeded, resetting.")
+            del context._v_wait
+            return self.request.RESPONSE.redirect(context.absolute_url())
+        else:
+            log.info("Waiting time not yet exceeded.")
+            raise NotFound
--- src/ploneintranet/theme/browser/views.py
+++ src/ploneintranet/theme/browser/views.py
@@ -1,14 +1,5 @@
-from datetime import datetime
-from datetime import timedelta
-from plone import api
-from zExceptions import NotFound
-from zope.publisher.browser import BrowserPage
 from zope.publisher.browser import BrowserView
 from ploneintranet.theme.interfaces import IThemeSpecific
-from Acquisition import aq_inner
-import logging
-
-log = logging.getLogger(__name__)
 
 
 class IsThemeEnabled(BrowserView):
@@ -17,36 +8,4 @@ class IsThemeEnabled(BrowserView):
         """ """
         return IThemeSpecific.providedBy(self.request)
 
-
-class ExampleDragAndDropUpload(BrowserView):
-    """ A view to serve as an example of drag & drop uploading.
-    """
-
-
-class ExampleResourcePolling(BrowserView):
-    """ A view to serve as an example of how to use pat-resourcepolling
-    """
-
-    def get_images(self):
-        catalog = api.portal.get_tool('portal_catalog')
-        return catalog(portal_type='Image', path=self.context.getPhysicalPath())
-
-
-class ExampleFilePreview(BrowserPage):
-    """ A view to serve as an example of file previews (e.g. via docconv)
-    """
-
-    def __call__(self, *args, **kw):
-        seconds = 10;
-        context = aq_inner(self.context)
-        if not hasattr(context, '_v_wait'):
-            context._v_wait = datetime.now() + timedelta(seconds=seconds)
-            log.info("Set waiting time to %s seconds" % seconds)
-        if context._v_wait <= datetime.now():
-            log.info("Waiting time exceeded, resetting.")
-            del context._v_wait
-            return self.request.RESPONSE.redirect(context.absolute_url())
-        else:
-            log.info("Waiting time not yet exceeded.")
-            raise NotFound
 
