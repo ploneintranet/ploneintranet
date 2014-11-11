75953c9b5c1bb5bdf990e9b56acca02846df0a4d
Fulvio Casali <fulviocasali@gmail.com>
2014-11-11 21:56:32 +0100

    Handle other uninstall GS steps.

--- plonesocial/network/Extensions/Install.py
+++ plonesocial/network/Extensions/Install.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
-
+from plonesocial.network.interfaces import INetworkTool
 PROJECTNAME = 'plonesocial.network'
+
+
+def _removeTool(portal):
+    portal.manage_delObjects(['plonesocial_network'])
+
+
+def _removePersistentUtility(portal):
+    sm = portal.getSiteManager()
+    sm.unregisterUtility(provided=INetworkTool)
+    sm.utilities.unsubscribe((), INetworkTool)
 
 
 def uninstall(portal, reinstall=False):
@@ -9,4 +19,6 @@ def uninstall(portal, reinstall=False):
         profile = 'profile-%s:uninstall' % PROJECTNAME
         setup_tool = getToolByName(portal, 'portal_setup')
         setup_tool.runAllImportStepsFromProfile(profile)
+        _removeTool(portal)
+        _removePersistentUtility(portal)
         return 'Ran all uninstall steps.'
