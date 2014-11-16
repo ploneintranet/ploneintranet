acb33b38cff5c5923480e5f6bb4efbaf93e1cb97
Fulvio Casali <fulviocasali@gmail.com>
2014-11-16 22:28:53 +0100

    refs #245 When uninstalling plonesocial.network, manually remove bundle reference from registry.  Also get rid of uselsess stuff in registry.xml.

--- plonesocial/network/Extensions/Install.py
+++ plonesocial/network/Extensions/Install.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
+from plone import api
 from plonesocial.network.interfaces import INetworkTool
+
+import logging
+
 PROJECTNAME = 'plonesocial.network'
 
+logger = logging.getLogger(PROJECTNAME)
 
 def _removeTool(portal):
     portal.manage_delObjects(['plonesocial_network'])
@@ -13,6 +18,14 @@ def _removePersistentUtility(portal):
     sm.unregisterUtility(provided=INetworkTool)
     sm.utilities.unsubscribe((), INetworkTool)
 
+def _removeBundleFromRegistry():
+    logger.info('Removing bundle reference from registry')
+
+    record = 'plone.bundles/plone-legacy.resources'
+    resources = api.portal.get_registry_record(record)
+    if u'resource-plonesocial-network-stylesheets' in resources:
+        resources.remove(u'resource-plonesocial-network-stylesheets')
+
 
 def uninstall(portal, reinstall=False):
     if not reinstall:
@@ -21,4 +34,5 @@ def uninstall(portal, reinstall=False):
         setup_tool.runAllImportStepsFromProfile(profile)
         _removeTool(portal)
         _removePersistentUtility(portal)
+        _removeBundleFromRegistry()
         return 'Ran all uninstall steps.'
