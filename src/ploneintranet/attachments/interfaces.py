62ef545f124aa7cff8c3caf8d96f3376e4526fb9
Manuel Reinhardt <reinhardt@syslab.com>
2014-11-20 13:03:23 +0000

    added attachment-info helper view

--- src/ploneintranet/attachments/interfaces.py
+++ src/ploneintranet/attachments/interfaces.py
@@ -2,7 +2,16 @@
 """Module where all interfaces, events and exceptions live."""
 
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.interface import Interface
 
 
 class IPloneintranetAttachmentsLayer(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer."""
+
+
+class IAttachmentInfo(Interface):
+    """Interface for helper view to get attachment information"""
+
+    def get_attachment_ids():
+        """Get the list of ids of all attachments of the context"""
+        pass
