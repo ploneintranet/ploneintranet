b4acb828cbe6db12320f382189ad9c98902ea9e7
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-11-26 14:34:07 +0100

    spreading the message to all users

--- src/ploneintranet/notifications/tests/test_events.py
+++ src/ploneintranet/notifications/tests/test_events.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from ..testing import PLONEINTRANET_NOTIFICATIONS_INTEGRATION_TESTING
 from plone import api
+from plone.app.testing.interfaces import TEST_USER_PASSWORD
+from plone.app.testing.interfaces import TEST_USER_ROLES
 from plonesocial.microblog.statusupdate import StatusUpdate
 from unittest import TestCase
 
@@ -12,22 +14,43 @@ class TestEvents(TestCase):
     def setUp(self):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
+        # add another user
+        self.pas = self.layer['portal']['acl_users']
+        # We need another user
+        self.pas.source_users.addUser(
+            'test_user_2_',
+            'test_user_2_',
+            TEST_USER_PASSWORD
+        )
+        for role in TEST_USER_ROLES:
+            self.pas.portal_role_manager.doAssignRoleToPrincipal('test_user_2_', role)  # noqa
+        with api.env.adopt_user('test_user_2_'):
+            api.user.get('test_user_2_').setProperties({
+                'email': 'marielle@sjogren.se',
+                'fullname': 'Marielle Sjögren',
+            })  # randomly generated
 
     def test_on_status_update(self):
-        self.assertTrue(True)
+        ''' A user makes a status update and we get a notification message
+        '''
         # When we add a status update
-        pm = api.portal.get_tool('plonesocial_microblog')
-        su = StatusUpdate(u'Test à')
-        pm.add(su)
+        with api.env.adopt_user('test_user_2_'):
+            su = StatusUpdate(u'Test à')
+            pm = api.portal.get_tool('plonesocial_microblog')
+            pm.add(su)
         # an events creates a notification for that user
         pin = api.portal.get_tool('ploneintranet_notifications')
-        user = api.user.get_current()
-        queue = pin.get_user_queue(user)
-        self.assertTrue(len(queue), 1)
-        message = queue[0]
-        self.assertEqual(message.predicate, 'StatusUpdate')
-        self.assertEqual(message.obj['creator'], 'test-user')
-        self.assertDictEqual(
-            message.actors,
-            {'email': '', 'fullname': '', 'userid': 'test_user_1_'}
-        )
+        for user in api.user.get_users():
+            queue = pin.get_user_queue(user)
+            self.assertTrue(len(queue), 1)
+            message = queue[0]
+            self.assertEqual(message.predicate, 'StatusUpdate')
+            self.assertEqual(message.obj['creator'], 'test_user_2_')
+            self.assertDictEqual(
+                message.actors,
+                {
+                    'email': 'marielle@sjogren.se',
+                    'fullname': 'Marielle Sjögren',
+                    'userid': 'test_user_2_',
+                }
+            )
