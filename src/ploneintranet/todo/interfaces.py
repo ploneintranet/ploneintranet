ebc761f7eed0feb930378ba3252c9df926ed90b4
Ben Cole <benc@netsight.co.uk>
2014-11-26 15:43:51 +0000

    Added - Interfaces
    ContentActions interface specifies the object that will be stored in annotation storage
    TodoUtility interface specifies the API for the TodoUtility

--- src/ploneintranet/todo/interfaces.py
+++ src/ploneintranet/todo/interfaces.py
@@ -1,2 +1,69 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
+from zope.interface import Interface, Attribute
+from ploneintranet.readstatus import MessageFactory as _
+
+
+class IContentAction(Interface):
+    """
+    Interface for ContentAction class
+    """
+    userid = Attribute(_(u'The userid this action is for'))
+    content_uid = Attribute(_(u'The UID of the content that needs actioning'))
+    verb = Attribute(_(u'The action to be taken on the content'))
+    created = Attribute(_(u'The datetime this ContentAction was created'))
+    completed = Attribute(_(u'The datetime this ContentAction was completed'))
+
+
+class ITodoUtility(Interface):
+    """
+    Interface for the TodoUtility
+    """
+    def add_action(content_uid, verb, userids=None, all_users=False):
+        """
+        Add the given action for the given content to the given users, or all
+        users
+
+        :param content_uid: The UID of the content
+        :type content_uid: str
+        :param verb: The action to take
+        :type verb: str
+        :param userids: The userids to add the action to
+        :type userids: list
+        :param all_users: Whether to add action to all users
+        :type all_users: bool
+        """
+
+    def complete_action(content_uid, verb, userids=None):
+        """
+        Mark the given action for the given content as compelte for the given
+        users
+
+        :param content_uid: The UID of the content
+        :type content_uid: str
+        :param verb: The action to complete
+        :type verb: str
+        :param userids: The userids to complete the action from or None for all
+                        users
+        :type userids: list or None
+        """
+
+    def query(userids=None, verbs=None, content_uids=None, sort_on=None,
+              sort_order=None):
+        """
+        Query the action storage for ContentActions matching the given query.
+        Results are AND'd together
+
+        :param userids: UserIDs to lookup
+        :type userids: list or str or None
+        :param verbs: Action verb to lookup
+        :type verbs: list or str or None
+        :param content_uids: Content UIDs to lookup
+        :type content_uids: list or str or None
+        :param sort_on: Field to sort on (created, completed, verb)
+        :type sort_on: str
+        :param sort_order: Whether to reverse the sort order ('reverse')
+        :type sort_order: str or None
+        :return: List of ContentActions
+        :rtype: :class: `ContentAction`
+        """
