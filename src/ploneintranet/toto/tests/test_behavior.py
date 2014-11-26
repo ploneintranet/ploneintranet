d09b81402a6f3fef1c03b74414c9bdd9c6f2ceac
Eric BREHAULT <ebrehault@gmail.com>
2014-11-26 14:15:24 +0100

    init

---
+++ src/ploneintranet/toto/tests/test_behavior.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from ploneintranet.todo.testing import IntegrationTestCase
+from plone import api
+
+from ploneintranet.todo.behaviors import IMustRead
+
+class TestBehavior(IntegrationTestCase):
+    """Test MustRead behavior."""
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_set_get(self):
+        news = api.content.create(
+            type='NewsItem',
+            title='My news',
+            container=self.portal)
+        behavior = IMustRead(news)
+        behavior.mustread = True
+        self.assertTrue(behavior.mustread)
+
---
+++ src/ploneintranet/toto/tests/test_setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""Setup/installation tests for this package."""
+
+from ploneintranet.todo.testing import IntegrationTestCase
+from plone import api
+
+
+class TestInstall(IntegrationTestCase):
+    """Test installation of ploneintranet.todo into Plone."""
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_product_installed(self):
+        """Test if ploneintranet.todo is installed with portal_quickinstaller."""
+        self.assertTrue(self.installer.isProductInstalled('ploneintranet.todo'))
+
+    def test_uninstall(self):
+        """Test if ploneintranet.todo is cleanly uninstalled."""
+        self.installer.uninstallProducts(['ploneintranet.todo'])
+        self.assertFalse(self.installer.isProductInstalled('ploneintranet.todo'))
+
+    # browserlayer.xml
+    def test_browserlayer(self):
+        """Test that IPloneintranetReadstatusLayer is registered."""
+        from ploneintranet.todo.interfaces import IPloneintranetReadstatusLayer
+        from plone.browserlayer import utils
+        self.failUnless(IPloneintranetReadstatusLayer in utils.registered_layers())
