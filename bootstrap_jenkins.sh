b75428124a717ce0be96da6300dd2d732a0cd981
Patrick Gerken <do3cc@patrick-gerken.de>
2014-11-26 11:29:29 +0100

    Cleanup

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -1,7 +1,7 @@
 virtualenv --no-setuptools .
 mkdir -p buildout-cache/downloads
-./bin/python bootstrap.py -v 1.6.3
+./bin/python bootstrap.py
 ./bin/buildout -c travis.cfg buildout:develop= install download install
-./bin/buildout -N -t 3 -c travis.cfg
+./bin/buildout -N -t 3 -c jenkins.cfg
 ./bin/develop up -f
 # Xvfb :99 -a &
