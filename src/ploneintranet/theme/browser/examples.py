4c6405f2dc756deb756ebd194850fdb83b11f500
JC Brand <jc@opkode.com>
2014-11-27 14:18:15 +0100

    Show an image in the equaliser example view.

--- src/ploneintranet/theme/browser/examples.py
+++ src/ploneintranet/theme/browser/examples.py
@@ -10,26 +10,30 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class ExampleDragAndDropUpload(BrowserView):
-    """ A view to serve as an example of drag & drop uploading.
-    """
-
-
-class ExampleResourcePolling(BrowserView):
-    """ A view to serve as an example of how to use pat-resourcepolling
-    """
+class Mixin(object):
 
     def get_images(self):
         catalog = api.portal.get_tool('portal_catalog')
         return catalog(portal_type='Image', path=self.context.getPhysicalPath())
 
 
-class ExampleEqualiser(BrowserView):
+class ExampleDragAndDropUpload(BrowserView):
+    """ A view to serve as an example of drag & drop uploading.
+    """
+
+
+class ExampleResourcePolling(BrowserView, Mixin):
+    """ A view to serve as an example of how to use pat-resourcepolling
+    """
+
+
+class ExampleEqualiser(BrowserView, Mixin):
     """ A view to illustrate the usage of pat-equaliser, particularly the
         slow loading of images, in order to fix the following bug:
 
         https://github.com/Patternslib/Patterns/issues/383
     """
+
 
 class ExampleFilePreview(BrowserPage):
     """ A view to serve as an example of file previews (e.g. via docconv)
