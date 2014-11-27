fa06b0b80425c5f3f11bbb0bdf82192907af4d44
Patrick Gerken <do3cc@patrick-gerken.de>
2014-11-27 16:31:58 +0100

    Actually fail if something does not work as expected

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -1,7 +1,6 @@
 virtualenv --no-setuptools .
-mkdir -p buildout-cache/downloads
-./bin/python bootstrap.py
-./bin/buildout -c travis.cfg buildout:develop= install download install
-./bin/buildout -N -t 3 -c jenkins.cfg
-./bin/develop up -f
-# Xvfb :99 -a &
+mkdir -p buildout-cache/downloads || exit 1
+./bin/python bootstrap.py || exit 1
+./bin/buildout -c travis.cfg buildout:develop= install download install || exit 1
+./bin/buildout -N -t 3 -c jenkins.cfg || exit 1
+./bin/develop up -f || exit 1
