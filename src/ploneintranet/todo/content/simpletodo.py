fa54f4374ca2a7b74a12de00917d3feab935a851
Eric BREHAULT <ebrehault@gmail.com>
2014-12-03 17:17:12 +0100

    fix todo content-type definition

--- src/ploneintranet/todo/content/simpletodo.py
+++ src/ploneintranet/todo/content/simpletodo.py
@@ -1,20 +1,28 @@
 from plone.dexterity.content import Item
+from plone.supermodel import model
 from zope import schema
-from zope.interface import implements, Interface
+from zope.interface import implements
 
 from ploneintranet.todo import _
 
 
-class ISimpleTodo(Interface):
+class ISimpleTodo(model.Schema):
     """A simple todo content type
     """
 
-    title = schema.TextLine(title=_("Task"))
+    taskname = schema.TextLine(title=_("Task"))
 
     assignee = schema.Choice(
         title=_(u"Assignee"),
         description=_("A user (or a group) assigned to this task"),
+        required=False,
         vocabulary="plone.principalsource.Principals"
+        )
+
+    workspace = schema.TextLine(
+        title=_(u"Workspace"),
+        description=_(u"The workspace assigned to this task"),
+        required=False,
         )
 
 
