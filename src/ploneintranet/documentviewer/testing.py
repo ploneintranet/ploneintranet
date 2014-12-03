05a7258007116df88ca5eb8c3af08da06d0905f9
Patrick Gerken <do3cc@patrick-gerken.de>
2014-12-03 15:39:08 +0100

    move to plone5
    So far I updated the test code so that real problems can be handled
    In this case content is created with the assumption that it will be
    archetypes objects. This fails here because images seem to need some
    attribute. plone.app.contenttypes can be used to check for examples how
    to create images.
    I asked on irc if we need AT compatibility.
    2 Answered with screw it, so go for it

--- src/ploneintranet/documentviewer/testing.py
+++ src/ploneintranet/documentviewer/testing.py
@@ -1,3 +1,4 @@
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
@@ -11,7 +12,7 @@ from zope.configuration import xmlconfig
 
 class PloneintranetdocumentviewerLayer(PloneSandboxLayer):
 
-    defaultBases = (PLONE_FIXTURE,)
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
         # Load ZCML
