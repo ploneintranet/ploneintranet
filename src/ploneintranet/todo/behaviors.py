f3b5972eaa2fdc68c88f5c427f06690374419328
Eric BREHAULT <ebrehault@gmail.com>
2014-12-03 17:48:50 +0100

    turn todo fields into a behavior

--- src/ploneintranet/todo/behaviors.py
+++ src/ploneintranet/todo/behaviors.py
@@ -1,8 +1,9 @@
 from plone.directives import form
 from zope.interface import alsoProvides, Interface
-from zope.schema import Bool
+from zope.schema import Bool, Choice, TextLine
 
 from . import _
+from .vocabularies import todo_status, todo_priority
 
 
 class IMustRead(form.Schema):
@@ -28,4 +29,44 @@ alsoProvides(IMustRead, form.IFormFieldProvider)
 class IMustReadMarker(Interface):
     """Marker interface that will be provided by instances using the
     IMustRead behavior.
+    """
+
+
+class ITodo(form.Schema):
+    """Todo schema
+    """
+
+    assignee = Choice(
+        title=_(u"Assignee"),
+        description=_("A user (or a group) assigned to this task"),
+        required=False,
+        vocabulary="plone.principalsource.Principals"
+        )
+
+    workspace = TextLine(
+        title=_(u"Workspace"),
+        description=_(u"The workspace assigned to this task"),
+        required=False,
+        )
+
+    status = Choice(
+        title=_(u"Status"),
+        required=True,
+        default=u'tbd', 
+        vocabulary=todo_status,
+        )
+
+    priority = Choice(
+        title=_(u"Priority"),
+        required=True,
+        default=1, 
+        vocabulary=todo_priority,
+        )
+
+alsoProvides(ITodo, form.IFormFieldProvider)
+
+
+class ITodoMarker(Interface):
+    """Marker interface that will be provided by instances using the
+    ITodo behavior.
     """
--- src/ploneintranet/todo/content/simpletodo.py
+++ src/ploneintranet/todo/content/simpletodo.py
@@ -4,7 +4,6 @@ from zope import schema
 from zope.interface import implements
 
 from ploneintranet.todo import _
-from ploneintranet.todo.vocabularies import todo_status, todo_priority
 
 
 class ISimpleTodo(model.Schema):
@@ -12,33 +11,6 @@ class ISimpleTodo(model.Schema):
     """
 
     taskname = schema.TextLine(title=_("Task"))
-
-    assignee = schema.Choice(
-        title=_(u"Assignee"),
-        description=_("A user (or a group) assigned to this task"),
-        required=False,
-        vocabulary="plone.principalsource.Principals"
-        )
-
-    workspace = schema.TextLine(
-        title=_(u"Workspace"),
-        description=_(u"The workspace assigned to this task"),
-        required=False,
-        )
-
-    status = schema.Choice(
-        title=_(u"Status"),
-        required=True,
-        default=u'tbd', 
-        vocabulary=todo_status,
-        )
-
-    priority = schema.Choice(
-        title=_(u"Priority"),
-        required=True,
-        default=1, 
-        vocabulary=todo_priority,
-        )
 
 
 class SimpleTodo(Item):
