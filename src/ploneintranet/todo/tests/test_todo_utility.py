421932d4b8b5ebc50634a71f89584acb1e77381b
Ben Cole <benc@netsight.co.uk>
2014-12-10 11:35:49 +0000

    WiP todo_utility TDD

---
+++ src/ploneintranet/todo/tests/test_todo_utility.py
@@ -0,0 +1,122 @@
+from datetime import datetime
+from faker import Factory
+from plone import api
+import transaction
+from zope.component import getUtility
+
+from ..interfaces import ITodoUtility
+from ..content.content_action import ContentAction
+from ..testing import IntegrationTestCase
+
+
+class TestTodoUtility(IntegrationTestCase):
+
+    def setUp(self):
+        faker = Factory.create()
+        self.portal = self.layer['portal']
+        self.util = getUtility(ITodoUtility)
+        self.user1 = api.user.create(
+            email=faker.company_email(),
+            username='user1'
+        )
+        self.user2 = api.user.create(
+            email=faker.company_email(),
+            username='user2'
+        )
+        self.all_userids = {
+            self.user1.getId(),
+            self.user2.getId(),
+            'admin',
+            'test_user_1_',
+        }
+        self.doc1 = api.content.create(
+            container=self.portal,
+            type='Document',
+            id='doc1',
+            title='Doc 1'
+        )
+        self.doc2 = api.content.create(
+            container=self.portal,
+            type='Document',
+            id='doc2',
+            title='Doc 2'
+        )
+
+    def test__get_storage(self):
+        storage = self.util._get_storage()
+        key = 'foo'
+        value = 'bar'
+        storage[key] = value
+        self.assertEqual(
+            self.util._get_storage().get(key),
+            value
+        )
+
+    def test__all_users(self):
+        self.assertEqual(
+            set(self.util._all_users()),
+            self.all_userids,
+        )
+
+    def test__user_in_storage(self):
+        user1_id = self.user1.getId()
+        storage = self.util._get_storage()
+        storage[user1_id] = 'foo'
+        transaction.commit()
+        self.assertTrue(self.util._user_in_storage(user1_id))
+
+    def test_add_action_for_one_user(self):
+        user1_id = self.user1.getId()
+        doc1_uid = self.doc1.UID()
+        self.util.add_action(
+            doc1_uid,
+            'todo',
+            userids=user1_id
+        )
+        storage = self.util._get_storage()
+        self.assertEqual(len(storage.keys()), 1)
+        actions = storage[user1_id]
+        self.assertEqual(len(actions), 1)
+        action = actions[0]
+        self.assertIsInstance(action, ContentAction)
+        self.assertEqual(action.userid, user1_id)
+        self.assertEqual(action.content_uid, doc1_uid)
+        self.assertEqual(action.action_verb, 'todo')
+        self.assertLess(action.created, datetime.now())
+        self.assertIsNone(action.completed)
+        self.assertIsNone(action.modified)
+
+    def test_add_action_for_some_users(self):
+        user1_id = self.user1.getId()
+        user2_id = self.user2.getId()
+        doc1_uid = self.doc1.UID()
+        self.util.add_action(
+            doc1_uid,
+            'todo',
+            userids=[user1_id, user2_id]
+        )
+        storage = self.util._get_storage()
+        self.assertEqual(len(storage.keys()), 2)
+        self.assertIn(user1_id, storage.keys())
+        self.assertIn(user2_id, storage.keys())
+
+    def test_add_action_for_all_users(self):
+        doc1_uid = self.doc1.UID()
+        self.util.add_action(
+            doc1_uid,
+            'todo'
+        )
+        storage = self.util._get_storage()
+        self.assertEqual(
+            len(storage.keys()),
+            len(self.all_userids)
+        )
+
+    def add_test_actions(self):
+        user1_id = self.user1.getId()
+        user2_id = self.user2.getId()
+        doc1_uid = self.doc1.UID()
+        doc2_uid = self.doc2.UID()
+
+    def test_query_no_params(self):
+        self.fail()
--- src/ploneintranet/todo/todo_utility.py
+++ src/ploneintranet/todo/todo_utility.py
@@ -1,12 +1,10 @@
 from BTrees.OOBTree import OOBTree
-from Products.PlonePAS.tools.memberdata import MemberData
-
 from plone import api
 from zope.annotation import IAnnotations
 from zope.interface import implements
 
 from .interfaces import ITodoUtility
-from ploneintranet.todo.content_action import ContentAction
+from .content.content_action import ContentAction
 
 ANNOTATION_KEY = 'ploneintranet.todo.action_storage'
 
@@ -28,50 +26,13 @@ class TodoUtility(object):
             annotations[ANNOTATION_KEY] = OOBTree()
         return annotations[ANNOTATION_KEY]
 
-    def add_action(self, content_uid, verb, userids=None):
-        """
-        Add the given action for the given content to the given users, or all
-        users
+    @staticmethod
+    def _all_users():
+        return [x.getId() for x in api.user.get_users()]
 
-        :param content_uid: The UID of the content
-        :type content_uid: str
-        :param verb: The action to take
-        :type verb: str
-        :param userids: The userids to add the action to
-        :type userids: list
-        """
+    def _user_in_storage(self, userid):
         storage = self._get_storage()
-        if userids is None:
-            userids = [x.getId() for x in api.user.get_users()]
-        for userid in userids:
-            user_actions = self.query(
-                userids=userid,
-                verbs=verb,
-                content_uids=content_uid
-            )
-            if not user_actions:
-                user_action = ContentAction(
-                    userid,
-                    content_uid,
-                    verb
-                )
-            else:
-                user_action = user_actions[0]
-
-
-    def complete_action(self, content_uid, verb, userids=None):
-        """
-        Mark the given action for the given content as compelte for the given
-        users
-
-        :param content_uid: The UID of the content
-        :type content_uid: str
-        :param verb: The action to complete
-        :type verb: str
-        :param userids: The userids to complete the action from or None for all
-                        users
-        :type userids: list or None
-        """
+        return userid in storage
 
     def query(self, userids=None, verbs=None, content_uids=None, sort_on=None,
               sort_order=None):
@@ -99,3 +60,52 @@ class TodoUtility(object):
         if isinstance(content_uids, basestring):
             content_uids = [content_uids]
         return []
+
+    def add_action(self, content_uid, verb, userids=None):
+        """
+        Add the given action for the given content to the given users, or all
+        users
+
+        :param content_uid: The UID of the content
+        :type content_uid: str
+        :param verb: The action to take
+        :type verb: str
+        :param userids: The userids to add the action to
+        :type userids: list
+        """
+        if isinstance(userids, basestring):
+            userids = [userids]
+        storage = self._get_storage()
+        if userids is None:
+            userids = self._all_users()
+        for userid in userids:
+            if not self._user_in_storage(userid):
+                storage[userid] = []
+            user_actions = self.query(
+                userids=userid,
+                verbs=verb,
+                content_uids=content_uid
+            )
+            if not user_actions:
+                user_action = ContentAction(
+                    userid,
+                    content_uid,
+                    verb
+                )
+                storage[userid].append(user_action)
+
+    def complete_action(self, content_uid, verb, userids=None):
+        """
+        Mark the given action for the given content as complete for the given
+        users
+
+        :param content_uid: The UID of the content
+        :type content_uid: str
+        :param verb: The action to complete
+        :type verb: str
+        :param userids: The userids to complete the action from or None for all
+                        users
+        :type userids: list or None
+        """
+
+
