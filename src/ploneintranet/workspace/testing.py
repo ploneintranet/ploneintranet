61bb3e33e551a6bdb49ece5a824860a590c1b1be
Simone Deponti <simone.deponti@gmail.com>
2014-12-17 13:23:22 +0100

    Using the correct base layer (a change that was introduced with the switch to Plone 5)

--- src/ploneintranet/workspace/testing.py
+++ src/ploneintranet/workspace/testing.py
@@ -2,9 +2,9 @@ from plone.testing import z2
 
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
-from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.robotframework.testing import AUTOLOGIN_LIBRARY_FIXTURE
 
 from zope.configuration import xmlconfig
@@ -12,7 +12,7 @@ from zope.configuration import xmlconfig
 
 class PloneintranetworkspaceLayer(PloneSandboxLayer):
 
-    defaultBases = (PLONE_FIXTURE,)
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
         # Load ZCML
