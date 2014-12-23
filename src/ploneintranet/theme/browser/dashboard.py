2991f8180ac77502c83ebad2a89ca2bcd8a05af3
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-12-23 17:31:43 +0100

    Fill the markup with some dynamic data

--- src/ploneintranet/theme/browser/dashboard.py
+++ src/ploneintranet/theme/browser/dashboard.py
@@ -1,10 +1,12 @@
 from plone import api
+from plone.memoize.view import memoize
 from plone.tiles import Tile
 from zope.publisher.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class Dashboard(BrowserView):
+
     """ A view to serve as a dashboard for homepage and/or users
     """
 
@@ -24,15 +26,47 @@ class WorkspacesTile(Tile):
 
     index = ViewPageTemplateFile("templates/workspace-tile.pt")
 
+    def get_workspace_activities(self, brain, limit=1):
+        ''' Return the workspace activities sorted by reverse chronological
+        order
+
+        Regarding the time value:
+         - the datetime value contains the time in international format
+           (machine readable)
+         - the title value contains the absolute date and time of the post
+        '''
+        # BBB: this is a mock!!!!
+        return [
+            {
+                'subject': 'Charlotte Holzer',
+                'verb': 'published',
+                'object': 'Proposal draft V1.0 # This is a mock!!!',
+                'time': {
+                    'datetime': '2008-02-14',
+                    'title': '5 October 2015, 18:43',
+                }
+            }
+        ][:limit]
+
+    @memoize
     def workspaces(self):
         ''' The list of my workspaces
         '''
         pc = api.portal.get_tool('portal_catalog')
-        return pc(
+        brains = pc(
             portal_type="ploneintranet.workspace.workspacefolder",
             sort_on="modified",
             sort_order="reversed",
         )
+        workspaces = [
+            {
+                'title': brain.Title,
+                'description': brain.Description,
+                'url': brain.getURL(),
+                'activities': self.get_workspace_activities(brain),
+            } for brain in brains
+        ]
+        return workspaces
 
     def render(self):
         return self.index()
