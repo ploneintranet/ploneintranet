5d91904feeccdbdf2d4b2d45af7605f65bf5f7a8
Alexander Pilz <pilz@syslab.com>
2015-01-08 11:46:17 +0100

    added workspaces overview

--- src/ploneintranet/theme/browser/workspace.py
+++ src/ploneintranet/theme/browser/workspace.py
@@ -1,7 +1,24 @@
 from plone import api
+from plone.memoize.view import memoize
 from plone.tiles import Tile
 from zope.publisher.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ploneintranet.workspace.utils import my_workspaces
+
+class Workspaces(BrowserView):
+
+    """ A view to serve as overview over workspaces
+    """
+
+    """ The tiles below are dummy tiles.
+         Please do NOT implement "real" tiles here, put them in another package.
+         We want to keep the theme simple and devoid of business logic
+    """
+    @memoize
+    def workspaces(self):
+        ''' The list of my workspaces
+        '''
+        return my_workspaces(self.context)
 
 
 class WorkspaceTabsTile(Tile):
