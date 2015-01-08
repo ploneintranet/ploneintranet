428c6237ce295f19ae45eca93a7f46a612ce054e
maartenkling <maarten@fourdigits.nl>
2015-01-08 09:50:56 +0100

    create news items

--- src/ploneintranet/suite/setuphandlers.py
+++ src/ploneintranet/suite/setuphandlers.py
@@ -6,11 +6,13 @@ def create_as(userid, *args, **kwargs):
     current = api.user.get_current()
     user = api.user.get(username=userid)
     newSecurityManager(None, user)
+    obj = None
     try:
-        api.content.create(*args, **kwargs)
+        obj = api.content.create(*args, **kwargs)
     finally:
         # we always restore the previous security context, no matter what
         newSecurityManager(None, current)
+    return obj
 
 
 def testing(context):
@@ -18,31 +20,51 @@ def testing(context):
     if context.readDataFile('ploneintranet.suite_testing.txt') is None:
         return
 
-    portal = api.portal.get()
-
     # see: https://github.com/cosent/plonesocial.suite/
     #       blob/master/src/plonesocial/suite/setuphandlers.py
-
     # for creating users and network
 
+    # We use following fixed tags
+    tags = ['Rain', 'Sun', 'Planes', 'ICT', ]
+
+    # We use fixed dates, we need thes to be relative
+    #publication_date = ['just now', 'next week', 'next year', ]
+
+    # make newsitems
+    news_content = [
+        {'title': 'Second Indian Airline to join Global Airline Alliance',
+         'description': 'Weak network in growing Indian aviation market',
+         'tags': [tags[0]],
+         'publication_date': ''},
+
+        {'title': 'BNB and Randomize to codeshare',
+         'description': 'Starting September 10, BNB passengers will be' +
+         'able to book connecting flights on Ethiopian Airlines.',
+         'tags': [tags[1]],
+         'publication_date': ''},
+
+        {'title': 'Alliance Officially Opens New Lounge',
+         'description': '',
+         'tags': [tags[0], tags[1]],
+         'publication_date': ''},
+    ]
+    create_news_items(news_content)
+
+
+def create_news_items(context, newscontent):
     # news item
+    portal = api.portal.get()
+
+    if not 'news' in portal.keys():
+        # do we creat it?
+        pass
+
     if 'news' in portal.keys():
-
-        newscontent = [
-            {'title': 'Second Indian Airline to join Global Airline Alliance',
-             'description': 'Weak network in growing Indian aviation market'},
-
-            {'title': 'BNB and Randomize to codeshare',
-             'description': 'Starting September 10, BNB passengers will be' +
-             'able to book connecting flights on Ethiopian Airlines.'},
-
-            {'title': 'Alliance Officially Opens New Lounge',
-             'description': ''},
-        ]
         for newsitem in newscontent:
-            create_as(
-                "admin",
-                type='News Item',
-                title=newsitem['title'],
-                description=newsitem['description'],
-                container=portal['news'])
+            obj = create_as("admin",
+                            type='News Item',
+                            title=newsitem['title'],
+                            description=newsitem['description'],
+                            container=portal['news'])
+            obj.publication_date = newsitem['publication_date']
+            obj.Subject = newsitem['tags']
