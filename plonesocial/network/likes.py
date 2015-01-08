ae72f6b1ad1181723fe1d5cf2e75619830973320
Manuel Reinhardt <reinhardt@syslab.com>
2015-01-08 17:39:58 +0000

    continued implementation of LikesContainer

--- plonesocial/network/likes.py
+++ plonesocial/network/likes.py
@@ -17,8 +17,43 @@ class LikesContainer(Persistent, Explicit):
         # maps status id to user ids
         #self._statusid_userids_mapping = OOBTree.OOBTree()
 
-    def add_like(self, userid, object_id, context=None):
-        self._user_uuids_mapping[userid] = [object_id]
+    def add(self, user_id, item_id):
+        self._user_uuids_mapping[user_id] = [item_id]
+        self._uuid_userids_mapping[item_id] = [user_id]
+
+    def like(self, user_id, item_id):
+        self.add(user_id, item_id)
+
+    def remove(self, user_id, item_id):
+        self._user_uuids_mapping[user_id].remove(item_id)
+        self._uuid_userids_mapping[item_id].remove(user_id)
+
+    def unlike(self, user_id, item_id):
+        self.remove(user_id, item_id)
+
+    def get(self, user_id):
+        try:
+            return self._user_uuids_mapping[user_id]
+        except KeyError:
+            return []
+
+    def get_items_for_user(self, user_id):
+        return self.get(user_id)
+
+    def lookup(self, item_id):
+        try:
+            return self._uuid_userids_mapping[item_id]
+        except KeyError:
+            return []
+
+    def get_users_for_item(self, item_id):
+        return self.lookup(item_id)
+
+    def is_item_liked_by_user(self, user_id, item_id):
+        try:
+            return user_id in self.lookup(item_id)
+        except KeyError:
+            return False
 
     def items(self):
         return []
--- plonesocial/network/tests/test_likes.py
+++ plonesocial/network/tests/test_likes.py
@@ -5,16 +5,65 @@ from plonesocial.network.likes import LikesContainer
 
 class TestLikes(unittest.TestCase):
 
-    #def setUp(self):
+    def setUp(self):
         #self.portal = self.layer['portal']
+        self.userid = 'testperson@test.org'
+        self.object_uuid = '827e65bd826a89790eba679e0c9ff864'
+        self.container = LikesContainer()
 
-    def test_empty(self):
-        container = LikesContainer()
-        self.assertEqual(0, len(list(container.items())))
+    def _add(self):
+        self.container.add(
+            self.userid, self.object_uuid)
 
-    def test_add_like(self):
-        container = LikesContainer()
-        container.add_like(
-            'testperson@test.org', '827e65bd826a89790eba679e0c9ff864')
-        likes = container._user_uuids_mapping['testperson@test.org']
-        self.assertEqual(likes, ['827e65bd826a89790eba679e0c9ff864'])
+    def test_items_empty(self):
+        self.container = LikesContainer()
+        self.assertEqual(0, len(list(self.container.items())))
+
+    def test_add(self):
+        self._add()
+
+        liked_items = self.container._user_uuids_mapping[self.userid]
+        self.assertEqual(liked_items, [self.object_uuid])
+
+        liking_users = self.container._uuid_userids_mapping[self.object_uuid]
+        self.assertEqual(liking_users, [self.userid])
+
+    def test_remove(self):
+        self._add()
+
+        self.container.remove(self.userid, self.object_uuid)
+
+        liked_items = self.container._user_uuids_mapping[self.userid]
+        self.assertEqual(liked_items, [])
+
+        liking_users = self.container._uuid_userids_mapping[self.object_uuid]
+        self.assertEqual(liking_users, [])
+
+    def test_get(self):
+        self._add()
+        self.assertEqual(self.container.get(self.userid), [self.object_uuid])
+
+    def test_get_empty(self):
+        self.assertEqual(self.container.get(self.userid), [])
+
+    def test_lookup(self):
+        self._add()
+        self.assertEqual(
+            self.container.lookup(self.object_uuid), [self.userid])
+
+    def test_lookup_empty(self):
+        self.assertEqual(
+            self.container.lookup(self.object_uuid), [])
+
+    def test_is_item_liked_by_user(self):
+        self.assertFalse(
+            self.container.is_item_liked_by_user(
+                self.userid,
+                self.object_uuid))
+
+        self._add()
+
+        self.assertTrue(
+            self.container.is_item_liked_by_user(
+                self.userid,
+                self.object_uuid))
