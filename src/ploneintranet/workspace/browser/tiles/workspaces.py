7da7fb2ea11d19d8f82af4fb25c52a95225ff22b
Alexander Pilz <pilz@syslab.com>
2015-01-08 11:46:31 +0100

    Added workspaces overview

--- src/ploneintranet/workspace/browser/tiles/workspaces.py
+++ src/ploneintranet/workspace/browser/tiles/workspaces.py
@@ -3,53 +3,20 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone import api
 from plone.memoize.view import memoize
 from plone.tiles import Tile
-
+from ploneintranet.workspace.utils import my_workspaces
 
 class WorkspacesTile(Tile):
 
     index = ViewPageTemplateFile("templates/workspaces.pt")
 
-    def get_workspace_activities(self, brain, limit=1):
-        ''' Return the workspace activities sorted by reverse chronological
-        order
 
-        Regarding the time value:
-         - the datetime value contains the time in international format
-           (machine readable)
-         - the title value contains the absolute date and time of the post
-        '''
-        # BBB: this is a mock!!!!
-        return [
-            {
-                'subject': 'Charlotte Holzer',
-                'verb': 'published',
-                'object': 'Proposal draft V1.0 # This is a mock!!!',
-                'time': {
-                    'datetime': '2008-02-14',
-                    'title': '5 October 2015, 18:43',
-                }
-            }
-        ][:limit]
 
     @memoize
     def workspaces(self):
         ''' The list of my workspaces
         '''
-        pc = api.portal.get_tool('portal_catalog')
-        brains = pc(
-            portal_type="ploneintranet.workspace.workspacefolder",
-            sort_on="modified",
-            sort_order="reversed",
-        )
-        workspaces = [
-            {
-                'title': brain.Title,
-                'description': brain.Description,
-                'url': brain.getURL(),
-                'activities': self.get_workspace_activities(brain),
-            } for brain in brains
-        ]
-        return workspaces
+        return my_workspaces(self.context)
+
 
     def render(self):
         return self.index()
--- src/ploneintranet/workspace/utils.py
+++ src/ploneintranet/workspace/utils.py
@@ -53,3 +53,57 @@ def parent_workspace(context):
 
 def in_workspace(context):
     return IWorkspaceState.providedBy(parent_workspace(context))
+
+def escape_id_to_class(cid):
+    """ We use workspace ids as classes to style them.
+        if a workspace has dots in its name, this is not usable as a class
+        name. We have to escape that. We might need to do more to them, so this
+        became a utility function.
+    """
+    return cid.replace('.', '-')
+
+
+def get_workspace_activities(brain, limit=1):
+    ''' Return the workspace activities sorted by reverse chronological
+    order
+
+    Regarding the time value:
+     - the datetime value contains the time in international format
+       (machine readable)
+     - the title value contains the absolute date and time of the post
+    '''
+    # BBB: this is a mock!!!!
+    return [
+        {
+            'subject': 'Charlotte Holzer',
+            'verb': 'published',
+            'object': 'Proposal draft V1.0 # This is a mock!!!',
+            'time': {
+                'datetime': '2008-02-14',
+                'title': '5 October 2015, 18:43',
+            }
+        }
+    ][:limit]
+
+
+def my_workspaces(context):
+    ''' The list of my workspaces
+    '''
+    pc = api.portal.get_tool('portal_catalog')
+    brains = pc(
+        portal_type="ploneintranet.workspace.workspacefolder",
+        sort_on="modified",
+        sort_order="reversed",
+    )
+    workspaces = [
+        {
+            'id': brain.getId,
+            'title': brain.Title,
+            'description': brain.Description,
+            'url': brain.getURL(),
+            'activities': get_workspace_activities(brain),
+            'class': escape_id_to_class(brain.getId),
+        } for brain in brains
+    ]
+    return workspaces
+
