d13428ae37b62568b3f3b8f4636bd5024e0d443d
Alexander Pilz <pilz@syslab.com>
2015-01-08 12:26:10 +0100

    outcommenting robot tests temporarily, they will be moved to suite instead

--- src/ploneintranet/workspace/tests/test_robot.py
+++ src/ploneintranet/workspace/tests/test_robot.py
@@ -4,11 +4,16 @@ from plone.testing import layered
 import robotsuite
 import unittest
 
+# I outcomment these tests as they were written for barceloneta. 
+# We need to 
+#  a) Rewrite them for ploneintranet.theme and
+#  b) Move them to ploneintranet.suite so that we can test for all integrated packages.
+# We don't delete them yet so that we have a reference
 
-def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTests([
-        layered(robotsuite.RobotTestSuite("workspace.robot"),
-                layer=PLONEINTRANET_WORKSPACE_ROBOT_TESTING)
-    ])
-    return suite
+# def test_suite():
+#     suite = unittest.TestSuite()
+#     suite.addTests([
+#         layered(robotsuite.RobotTestSuite("workspace.robot"),
+#                 layer=PLONEINTRANET_WORKSPACE_ROBOT_TESTING)
+#     ])
+#     return suite
