ab9c0d4da9751f02d38051626a038a6cd98986ce
Ben Cole <benc@netsight.co.uk>
2015-01-09 11:32:23 +0000

    Added WorkspaceContainer
    A Container that allows only Workspaces to be added to it

--- src/ploneintranet/workspace/workspacecontainer.py
+++ src/ploneintranet/workspace/workspacecontainer.py
@@ -1,1 +1,17 @@
-class IWorkspaceContainer(form.Schema, IImageScaleTraversable)
+from five import grok
+from plone.dexterity.content import Container
+from plone.directives import form
+from plone.namedfile.interfaces import IImageScaleTraversable
+
+
+class IWorkspaceContainer(form.Schema, IImageScaleTraversable):
+    """
+    Marker interface for WorkspaceContainer
+    """
+
+
+class WorkspaceContainer(Container):
+    """
+    A folder to contain WorkspaceFolders
+    """
+    grok.implements(IWorkspaceContainer)
