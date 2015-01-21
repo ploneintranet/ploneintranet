4ee6962189eb1da82b15f5a77b628e639a452e3b
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-01-21 15:07:27 +0100

    fix unicode error when user fullname contains non ascii characters

--- src/plonesocial/core/browser/utils.py
+++ src/plonesocial/core/browser/utils.py
@@ -14,13 +14,15 @@ def link_tags(text, url=''):
 
 
 def link_users(text, url=''):
-    user_tmpl = '<a href="{0}/@@author/{1}" class="user user-{1}">@{2}</a>'
+    user_tmpl = u'<a href="{0}/@@author/{1}" class="user user-{1}">@{2}</a>'
     user_marks = USERRE.findall(text)
     for user_mark in user_marks:
         user_id = user_mark[1:]
         user = api.user.get(username=user_id)
         if user:
             user_fullname = user.getProperty('fullname', '') or user_id
+            if not isinstance(user_fullname, unicode):
+                user_fullname = user_fullname.decode('utf8')
             text = re.sub(
                 user_mark,
                 user_tmpl.format(
