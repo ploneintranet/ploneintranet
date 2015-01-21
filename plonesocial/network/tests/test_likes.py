82c99cda8f2c9e7c113ceb82f921c42f4aa573d4
Manuel Reinhardt <reinhardt@syslab.com>
2015-01-21 07:52:52 +0000

    moved TestLikesTool to test_likes.py

--- plonesocial/network/tests/test_likes.py
+++ plonesocial/network/tests/test_likes.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
+from plone import api
+from plonesocial.network.interfaces import ILikesContainer
+from plonesocial.network.interfaces import ILikesTool
 from plonesocial.network.likes import LikesContainer
 from plonesocial.network.testing import IntegrationTestCase
+from zope.component import queryUtility
+
+
+class TestLikesTool(IntegrationTestCase):
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_likes_tool_available(self):
+        tool = queryUtility(ILikesTool)
+        self.assertTrue(ILikesContainer.providedBy(tool))
+
+    def test_likes_tool_uninstalled(self):
+        qi = self.portal['portal_quickinstaller']
+        with api.env.adopt_roles(['Manager']):
+            qi.uninstallProducts(products=['plonesocial.network'])
+        self.assertNotIn('plonesocial_likes', self.portal)
+        tool = queryUtility(ILikesTool, None)
+        self.assertIsNone(tool)
 
 
 class TestLikes(IntegrationTestCase):
