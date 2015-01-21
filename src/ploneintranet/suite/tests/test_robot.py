b3b824a3b8e06a5920dedc8deecbff93055b926d
Alexander Pilz <pilz@syslab.com>
2015-01-21 11:07:24 +0100

    make jenkins happy so that he can read robot output again

--- src/ploneintranet/suite/tests/test_robot.py
+++ src/ploneintranet/suite/tests/test_robot.py
@@ -1,14 +1,14 @@
 import unittest
 
-# import robotsuite
-# from ploneintranet.suite.testing import PLONEINTRANET_SUITE_ROBOT
-# from plone.testing import layered
+import robotsuite
+from ploneintranet.suite.testing import PLONEINTRANET_SUITE_ROBOT
+from plone.testing import layered
 
 
 def test_suite():
     suite = unittest.TestSuite()
-    # suite.addTests([
-    #     layered(robotsuite.RobotTestSuite('test_hello.robot'),
-    #             layer=PLONEINTRANET_SUITE_ROBOT),
-    # ])
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite('test_hello.robot'),
+                layer=PLONEINTRANET_SUITE_ROBOT),
+    ])
     return suite
