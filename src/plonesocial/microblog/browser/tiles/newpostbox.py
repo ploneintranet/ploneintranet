f3f1d64a787ca7c56f739e37fe72e7a7d6ab0ba7
Wolfgang Thomas <thomas@syslab.com>
2015-02-04 17:36:57 +0100

    WIP: started to copy the exact markup from Proto. Defined some properties in the view with dummy values

--- src/plonesocial/microblog/browser/tiles/newpostbox.py
+++ src/plonesocial/microblog/browser/tiles/newpostbox.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.memoize.view import memoize
 from plone.tiles import Tile
 from zope.component import getMultiAdapter
 
@@ -24,3 +25,33 @@ class NewPostBoxTile(Tile):
         token = provider.form.attachment_form_token()
         self.form = {'attachment_form_token': token}
         return super(NewPostBoxTile, self).__call__(*args, **kwargs)
+
+    ###
+    # The following properties are currently set to dummy values.
+    # Reason: We want the template to already mirror the Prototype;
+    # this includes various conditions based on the following properties
+
+    @property
+    @memoize
+    def user(self):
+        return u"test_user"
+
+    @property
+    @memoize
+    def direct(self):
+        return False
+
+    @property
+    @memoize
+    def hideuser(self):
+        return False
+
+    @property
+    @memoize
+    def fixeduser(self):
+        return False
+
+    @property
+    @memoize
+    def placeholder(self):
+        return u"What are you doing?"
