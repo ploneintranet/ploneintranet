75ca3591de588673c73329b1488fa4f165056c5f
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-02-17 12:35:15 +0100

    Testing the tile

--- src/plonesocial/microblog/tests/test_tiles.py
+++ src/plonesocial/microblog/tests/test_tiles.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from plone import api
 from plonesocial.microblog.browser.interfaces import IPlonesocialMicroblogLayer
+from plonesocial.microblog.tool import MicroblogTool
 from plonesocial.microblog.testing import (
     PLONESOCIAL_MICROBLOG_INTEGRATION_TESTING
 )
+from plonesocial.microblog.statusupdate import StatusUpdate
 from zope.interface import alsoProvides
 import unittest2 as unittest
 
@@ -17,7 +19,7 @@ class TestSetup(unittest.TestCase):
         self.request = self.layer['request']
         alsoProvides(self.request, IPlonesocialMicroblogLayer)
 
-    def test_newpostbox_tile(self):
+    def test_newpostbox_tile_on_portal(self):
         ''' This will test the existence of the newpostbox.tile
         and its functionality
         '''
@@ -26,4 +28,46 @@ class TestSetup(unittest.TestCase):
             self.portal,
             self.request
         )
-        tile()
+        # we have a post container which is the microblog tool
+        self.assertIsInstance(tile.post_container, MicroblogTool)
+        # we dont' have a post context
+        self.assertEqual(tile.post_context, None)
+        # we have an attachment form token
+        self.assertRegexpMatches(
+            tile.attachment_form_token,
+            'test-user-([0-9]*)'
+        )
+        # we are not posting
+        self.assertEqual(tile.is_posting, False)
+        self.assertEqual(tile.post_text, u'')
+        self.assertEqual(tile.post_attachment, None)
+        # calling update does not create a post
+        tile.update()
+        self.assertEqual(tile.post, None)
+        # check if we render correctly
+        self.assertIn('div id="microblog"', tile())
+
+    def test_newpostbox_tile_submission_on_portal(self):
+        ''' This will test the existence of the newpostbox.tile
+        and its functionality
+        '''
+        request = self.request.clone()
+        request.form.update({
+            'form.widgets.text': u'Testing post',
+            # 'form.widgets.attachments': u'No attachments',
+            'form.buttons.statusupdate': '1'
+        })
+        request.other['ACTUAL_URL'] = 'http://nohost'
+
+        tile = api.content.get_view(
+            'newpostbox.tile',
+            self.portal,
+            request
+        )
+        # we are not posting
+        self.assertEqual(tile.is_posting, True)
+        self.assertEqual(tile.post_text, u'Testing post')
+        # self.assertEqual(tile.post_attachment, u'No attachments')
+        # calling update does not create a post
+        tile.update()
+        self.assertIsInstance(tile.post, StatusUpdate)
