d8af7adf3cbe1025995c0e625aeac4f07a7cf4f8
Claudio Kirchhoff <claudio.kirchhoff@gmail.com>
2015-02-27 18:25:27 +0100

    Put documents, info, tasks and events on a single tile and use navigation to switch between them

--- src/ploneintranet/workspace/browser/tiles/sidebar.py
+++ src/ploneintranet/workspace/browser/tiles/sidebar.py
@@ -19,35 +19,7 @@ class Sidebar(BrowserView):
     """ A view to serve as a sidebar navigation for workspaces
     """
 
-class EventsTile(BrowserView):
-
-    """ A view for workspace events
-    """
-
-    index = ViewPageTemplateFile("templates/sidebar-events.pt")
-
-    def render(self):
-        return self.index()
-
-    def __call__(self):
-        return self.render()
-
-class TasksTile(BrowserView):
-
-    """ A view for workspace tasks
-    """
-
-    index = ViewPageTemplateFile("templates/sidebar-tasks.pt")
-
-    def render(self):
-        return self.index()
-
-    def __call__(self):
-        return self.render()
-
-class InfoTile(Tile):
-
-    index = ViewPageTemplateFile("templates/sidebar-info.pt")
+    index = ViewPageTemplateFile("templates/sidebar.pt")
 
     def render(self):
         return self.index()
@@ -81,6 +53,7 @@ class InfoTile(Tile):
 
     @memoize
     def existing_users(self):
+
         members = IWorkspace(self.context).members
         info = []
         for userid, details in members.items():
@@ -106,16 +79,7 @@ class InfoTile(Tile):
             self.context,
         )
 
-
-class ContentItemsTile(Tile):
-
-    index = ViewPageTemplateFile("templates/sidebar-contentitems.pt")
-
-    def render(self):
-        return self.index()
-
-    def __call__(self):
-        return self.render()
+    # ContentItems
 
     def parent(self):
         if self.context.portal_type == \
@@ -184,3 +148,129 @@ class ContentItemsTile(Tile):
                 'mime-type': mime_type,
                 'dpi': dpi})
         return items
+
+    ### Events
+
+    # Nothing yet
+
+    ### Tasks
+
+    # Nothing yet
+
+
+
+
+
+
+
+    
+
+
+# class EventsTile(BrowserView):
+
+#     """ A view for workspace events
+#     """
+
+#     index = ViewPageTemplateFile("templates/sidebar-events.pt")
+
+#     def render(self):
+#         return self.index()
+
+#     def __call__(self):
+#         return self.render()
+
+# class TasksTile(BrowserView):
+
+#     """ A view for workspace tasks
+#     """
+
+#     index = ViewPageTemplateFile("templates/sidebar-tasks.pt")
+
+#     def render(self):
+#         return self.index()
+
+#     def __call__(self):
+#         return self.render()
+
+
+# class InfoTile(Tile):
+
+
+
+# class ContentItemsTile(Tile):
+
+#     index = ViewPageTemplateFile("templates/sidebar-contentitems.pt")
+
+#     def render(self):
+#         return self.index()
+
+#     def __call__(self):
+#         return self.render()
+
+#     def parent(self):
+#         if self.context.portal_type == \
+#                 "ploneintranet.workspace.workspacefolder":
+#             return None
+#         parent = self.context.aq_parent
+#         return {'id': parent.getId(),
+#                 'title': parent.Title(),
+#                 'url': parent.absolute_url() + '/@@sidebar.default'}
+
+#     def children(self):
+#         """ returns a list of dicts of items in the current context
+#         """
+#         items = []
+#         catalog = self.context.portal_catalog
+#         current_path = '/'.join(self.context.getPhysicalPath())
+
+#         sidebar_search = self.request.get('sidebar-search', None)
+#         if sidebar_search:
+#             st = '%s*' % sidebar_search  # XXX plone only allows * as postfix.
+#             # With solr we might want to do real substr
+#             results = catalog.searchResults(SearchableText=st,
+#                                             path=current_path)
+#         else:
+#             results = self.context.getFolderContents()
+
+#         for item in results:
+#             # Do some checks to set the right classes for icons and candy
+#             desc = (
+#                 item['Description']
+#                 and 'has-description'
+#                 or 'has-no-description'
+#             )
+
+#             content_type = TYPE_MAP.get(item['portal_type'], 'none')
+
+#             mime_type = ''  # XXX: will be needed later for grouping by mimetyp
+#             # typ can be user, folder, date and mime typish
+#             typ = 'folder'  # XXX: This needs to get dynamic later
+
+#             if content_type in FOLDERISH_TYPES:
+#                 dpi = (
+#                     "source: #items; target: #items && "
+#                     "source: #selector-contextual-functions; "
+#                     "target: #selector-contextual-functions && "
+#                     "source: #context-title; target: #context-title && "
+#                     "source: #sidebar-search-form; "
+#                     "target: #sidebar-search-form"
+#                 )
+#                 url = item.getURL() + '/@@sidebar.default#items'
+#                 content_type = 'group'
+#             else:
+#                 dpi = "target: #document-body"
+#                 url = item.getURL() + "#document-body"
+#                 content_type = 'document'
+
+#             cls = 'item %s type-%s %s' % (content_type, typ, desc)
+
+#             items.append({
+#                 'id': item['getId'],
+#                 'cls': cls,
+#                 'title': item['Title'],
+#                 'description': item['Description'],
+#                 'url': url,
+#                 'type': TYPE_MAP.get(item['portal_type'], 'none'),
+#                 'mime-type': mime_type,
+#                 'dpi': dpi})
+#         return items
