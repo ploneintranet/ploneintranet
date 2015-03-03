0710f8720d758444c8c967c80e2219b992aba4f8
Alexander Pilz <pilz@syslab.com>
2015-03-03 16:24:29 +0100

    don't do virtualenv if already there

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -1,4 +1,7 @@
-virtualenv --no-site-packages --clear .
+if [ ! -d lib ] 
+then
+    virtualenv --no-site-packages  .
+fi
 mkdir -p buildout-cache/downloads || exit 1
 #./bin/python bootstrap.py || exit 1
 ./bin/easy_install pip || exit 1
