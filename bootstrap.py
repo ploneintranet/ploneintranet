341f816839d3f5c4475f8efd07399fc59b42cec9
Cillian de Róiste <goibhniu@fsfe.org>
2015-03-09 19:09:24 +0100

    bootstrap, remove broken buildout, if it exists

--- bootstrap.py
+++ bootstrap.py
@@ -8,14 +8,17 @@ import sys
 python_path = sys.executable
 bin_dir = os.path.dirname(python_path)
 pip_path = os.path.join(bin_dir, "pip")
-command = "{0} install -r requirements.txt".format(pip_path)
-
+bootstrap_clean = "{0} uninstall zc.buildout".format(pip_path)
+bootstrap = "{0} install -r requirements.txt".format(pip_path)
 
 try:
-    print "Running bootstrap: {0}".format(command)
-    check_call(command.split(" "))
+    print "Cleaning up from previous bootstrap: {0}".format(bootstrap_clean)
+    check_call(bootstrap_clean.split(" "))
+except CalledProcessError:
+    print "Ready for bootstrap"
+try:
+    print "Running bootstrap command: {0}".format(bootstrap)
+    check_call(bootstrap.split(" "))
     print "Bootstrap complete"
 except CalledProcessError:
-    print "Please try to bootstrap manually using: {0}".format(command)
-
-
+    print "Please try to bootstrap manually using: {0}".format(bootstrap)
