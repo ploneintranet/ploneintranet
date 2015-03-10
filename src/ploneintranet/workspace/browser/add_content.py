2100c8afa72eee8c85739f1ca06e4861e5d8cad6
Philip Bauer <bauer@starzel.de>
2015-03-10 19:32:36 +0100

    add image-upload-field when selecting image

--- src/ploneintranet/workspace/browser/add_content.py
+++ src/ploneintranet/workspace/browser/add_content.py
@@ -3,7 +3,7 @@ from Products.CMFPlone.utils import safe_unicode
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone import api
-from plone.protect import CheckAuthenticator, PostOnly
+from plone.namedfile.file import NamedBlobImage
 
 
 class AddContent(BrowserView):
@@ -12,21 +12,25 @@ class AddContent(BrowserView):
 
     template = ViewPageTemplateFile('templates/add_content.pt')
 
-    def __call__(self, portal_type=None, title=None, description=None):
+    def __call__(
+            self,
+            portal_type=None,
+            title=None,
+            description=None,
+            image=None):
         """Evaluate form and redirect"""
         if title is not None:
-            CheckAuthenticator(self.request)
-            PostOnly(self.request)
             self.portal_type = portal_type
             self.title = title.strip()
             self.description = description.strip()
+            self.image = image
             if self.portal_type in api.portal.get_tool('portal_types'):
-                url = self.create()
+                url = self.create(image)
                 return self.redirect(url)
         return self.template()
 
-    def create(self):
-        """Create content and return url"""
+    def create(self, image=None):
+        """Create content and return url. In case of images add the image."""
         container = self.context
         new = api.content.create(
             container=container,
@@ -34,6 +38,12 @@ class AddContent(BrowserView):
             title=self.title,
             safe_id=True,
         )
+        if image:
+            namedblobimage = NamedBlobImage(
+                data=image.read(),
+                filename=safe_unicode(image.filename))
+            new.image = namedblobimage
+
         if new:
             new.description = safe_unicode(self.description)
             return new.absolute_url()
@@ -41,6 +51,12 @@ class AddContent(BrowserView):
     def redirect(self, url):
         """Has its own method to allow overriding"""
         url = '{}/view'.format(url)
+        # portal_type = new.portal_type.lower()
+        # if portal_type in ['image', 'file']:
+        #     url = '{}/{}_view'.format(new_url, portal_type)
+        # else:
+        #     url = '{}'.format(new_url)
+        # import pdb; pdb.set_trace()
         return self.request.response.redirect(url)
 
 
