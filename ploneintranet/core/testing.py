898f6614a78163bfa488d169a209653f9018bf27
Guido A.J. Stevens <guido.stevens@cosent.nl>
2015-03-12 15:35:10 +0100

    package rename to prepare ploneintranet merge

---
+++ ploneintranet/core/testing.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.tiles.testing import PLONE_APP_TILES_FIXTURE
+from zope.configuration import xmlconfig
+
+
+class PlonesocialCore(PloneSandboxLayer):
+
+    defaultBases = (
+        PLONE_APP_TILES_FIXTURE,
+    )
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML for this package
+        import ploneintranet.core
+        xmlconfig.file(
+            'configure.zcml',
+            ploneintranet.core,
+            context=configurationContext
+        )
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'ploneintranet.core:minimal')
+
+PLONESOCIAL_CORE_FIXTURE = PlonesocialCore()
+PLONESOCIAL_CORE_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONESOCIAL_CORE_FIXTURE, ),
+    name='PlonesocialCore:Integration'
+)
--- src/plonesocial/core/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from zope.i18nmessageid import MessageFactory
-
-
-plonesocialCoreMessageFactory = MessageFactory('plonesocial.core')
