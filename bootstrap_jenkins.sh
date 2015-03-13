017182a22250dbe2524952d8faab2b5fd4eb581d
Alexander Pilz <pilz@syslab.com>
2015-03-13 15:58:22 +0100

    attempt to reintroduce docsplit

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -6,5 +6,6 @@ mkdir -p buildout-cache/downloads || exit 1
 ./bin/python bootstrap.py || exit 1
 ./bin/buildout -c travis.cfg buildout:develop= install download install || exit 1
 ./bin/buildout -N -t 3 -c jenkins.cfg || exit 1
+bundle install --path vendor/bundle --binstubs
 ./bin/develop up -f || exit 1
 make clean-stamps all || exit 1
