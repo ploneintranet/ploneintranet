f702a0aebc4bee022abf1ecb074f50c602317a62
Patrick Gerken <do3cc@patrick-gerken.de>
2015-03-13 18:11:39 +0100

    no travis

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -4,7 +4,6 @@ then
 fi
 mkdir -p buildout-cache/downloads || exit 1
 ./bin/python bootstrap.py || exit 1
-./bin/buildout -c travis.cfg buildout:develop= install download install || exit 1
 ./bin/buildout -N -t 3 -c jenkins.cfg || exit 1
 bundle install --path vendor/bundle --binstubs
 ./bin/develop up -f || exit 1
