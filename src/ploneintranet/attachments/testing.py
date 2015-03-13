a307e2b18d5e60b015d3a2a0329680b025bf1ce1
Philip Bauer <bauer@starzel.de>
2015-03-13 12:39:42 +0100

    remove unused commit

--- src/ploneintranet/attachments/testing.py
+++ src/ploneintranet/attachments/testing.py
@@ -39,16 +39,6 @@ class PloneintranetAttachmentsLayer(PloneSandboxLayer):
         applyProfile(portal, 'plone.app.discussion:default')
         applyProfile(portal, 'ploneintranet.attachments:default')
 
-        # Login and create some test content
-        setRoles(portal, TEST_USER_ID, ['Manager'])
-        login(portal, TEST_USER_NAME)
-        portal.invokeFactory('Folder', 'folder')
-
-        # Commit so that the test browser sees these objects
-        portal.portal_catalog.clearFindAndRebuild()
-        import transaction
-        transaction.commit()
-
     def tearDownZope(self, app):
         """Tear down Zope."""
         z2.uninstallProduct(app, 'ploneintranet.attachments')
