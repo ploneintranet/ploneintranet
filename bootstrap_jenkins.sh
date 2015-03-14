9f9d9117aa124515426619516515a5e739168117
Cillian de Róiste <cillian.deroiste@gmail.com>
2015-03-14 15:20:37 +0100

    bootstrap_jenkins: use virtualenv --relocatable to reduce shabang length
    When jenkins jobs have long names the shabang path created by virtualenv
    exceeds the maximum length allowed of 127 characters. We can work around
    this by using `virtualenv --relocatable .` after any console scripts are
    added to ./bin.

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -1,10 +1,17 @@
 if [ ! -f bin/activate ] 
 then
-    virtualenv  .
+    virtualenv .
 fi
 mkdir -p buildout-cache/downloads || exit 1
+
+# NOTE: we need to use virtualenv --relocatable every time new
+# console_scripts have been created in ./bin to avoid running into the
+# problem where the shabang is > 127 characters long.
+virtualenv --relocatable .
 ./bin/python bootstrap.py || exit 1
+virtualenv --relocatable .
 ./bin/buildout -N -t 3 -c jenkins.cfg || exit 1
+virtualenv --relocatable .
 bundle install --path vendor/bundle --binstubs
 ./bin/develop up -f || exit 1
 make clean-stamps all || exit 1
