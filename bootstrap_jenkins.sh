a6338f6015e9469bb12b7e444c1be28fa3e50a27
Cillian de Róiste <cillian.deroiste@gmail.com>
2015-03-14 15:29:03 +0100

    fix a typo in the comment: shabang -> shebang

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -6,7 +6,7 @@ mkdir -p buildout-cache/downloads || exit 1
 
 # NOTE: we need to use virtualenv --relocatable every time new
 # console_scripts have been created in ./bin to avoid running into the
-# problem where the shabang is > 127 characters long.
+# problem where the shebang is > 127 characters long.
 virtualenv --relocatable .
 . bin/activate
 ./bin/python bootstrap.py || exit 1
