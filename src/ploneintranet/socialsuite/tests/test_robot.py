6ccd1d90b83017d161806a15db689a8354979213
Guido A.J. Stevens <guido.stevens@cosent.nl>
2015-03-14 10:27:23 +0100

    remove outdated plonesocial demo script

--- src/ploneintranet/socialsuite/tests/test_robot.py
+++ src/ploneintranet/socialsuite/tests/test_robot.py
@@ -6,11 +6,6 @@ from plone.testing import layered
 
 def test_suite():
     suite = unittest.TestSuite()
-    demo_suite = robotsuite.RobotTestSuite("demo")
-    # demo disabled by default, to run use: bin/test -a 2
-    demo_suite.level = 2
-    suite.addTests([layered(demo_suite,
-                            layer=PLONEINTRANET_SOCIAL_ROBOT), ])
     # functional tests ARE run by default - we have no unit tests
     suite.addTests([layered(robotsuite.RobotTestSuite('functional'),
                             layer=PLONEINTRANET_SOCIAL_ROBOT), ])
