d88eaee428506869a8e25062b321cb844bb8a028
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-03-24 15:47:46 +0100

    Initial work

--- src/ploneintranet/attachments/browser/upload.py
+++ src/ploneintranet/attachments/browser/upload.py
@@ -2,10 +2,10 @@ from Products.Five.browser import BrowserView
 
 from ploneintranet.attachments import utils
 from ploneintranet.attachments.attachments import IAttachmentStorage
+from ploneintranet.docconv.client.interfaces import IDocconv
 import logging
 
 log = logging.getLogger(__name__)
-
 try:
     from ploneintranet.docconv.client.interfaces import IPreviewFetcher
 except ImportError:
@@ -13,6 +13,17 @@ except ImportError:
 
 
 class UploadAttachments(BrowserView):
+
+    def get_thumbs_urls(self, attachment):
+        ''' This will return the URL for the thumbs of the attachment
+        '''
+        base_url = '%s/docconv_image_thumb.jpg?page=' % (
+            attachment.absolute_url()
+        )
+        thumbs = IDocconv(attachment).get_thumbs()
+        return [
+            (base_url + str(i+1)) for i, thumb in enumerate(thumbs)
+        ]
 
     def __call__(self):
         token = self.request.get('attachment-form-token')
@@ -33,4 +44,5 @@ class UploadAttachments(BrowserView):
                                  '{1}: {2}'.format(
                                      obj.id, e.__class__.__name__, e))
                 self.attachments.append(obj)
+
         return self.index()
