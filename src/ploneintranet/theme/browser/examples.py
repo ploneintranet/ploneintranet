cdcbefb1fe1305fbacaa55bf23ef9f2cef023c8c
Guido A.J. Stevens <guido.stevens@cosent.nl>
2015-03-25 15:51:44 +0100

    fix E703 statement ends with a semicolon

--- src/ploneintranet/theme/browser/examples.py
+++ src/ploneintranet/theme/browser/examples.py
@@ -41,7 +41,7 @@ class ExampleFilePreview(BrowserPage):
     """
 
     def __call__(self, *args, **kw):
-        seconds = 10;
+        seconds = 10
         context = aq_inner(self.context)
         if not hasattr(context, '_v_wait'):
             context._v_wait = datetime.now() + timedelta(seconds=seconds)
