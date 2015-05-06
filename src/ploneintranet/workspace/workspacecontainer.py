662c9c102a8f5f1f03d88095cd3b637436c02e84
Roel Bruggink <roel@jaroel.nl>
2015-05-06 14:16:32 +0200

    Replace grok

--- src/ploneintranet/workspace/workspacecontainer.py
+++ src/ploneintranet/workspace/workspacecontainer.py
@@ -1,8 +1,8 @@
-from five import grok
 from plone.dexterity.content import Container
 from plone.directives import form
 from plone.namedfile.interfaces import IImageScaleTraversable
 from ploneintranet.attachments.attachments import IAttachmentStoragable
+from zope.interface import implementer
 
 
 class IWorkspaceContainer(form.Schema, IImageScaleTraversable):
@@ -11,11 +11,8 @@ class IWorkspaceContainer(form.Schema, IImageScaleTraversable):
     """
 
 
+@implementer(IWorkspaceContainer, IAttachmentStoragable)
 class WorkspaceContainer(Container):
     """
     A folder to contain WorkspaceFolders
     """
-    grok.implements(
-        IWorkspaceContainer,
-        IAttachmentStoragable,
-    )
