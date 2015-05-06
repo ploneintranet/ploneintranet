17cca16ace26ee16ca8b5b6613cb0a6f720d2cb5
Ben Cole <wengole@gmail.com>
2015-05-06 16:30:12 +0100

    Fix tests

--- src/ploneintranet/search/tests/test_results.py
+++ src/ploneintranet/search/tests/test_results.py
@@ -10,19 +10,27 @@ class TestResults(IntegrationTestCase):
     """Test the results structures"""
 
     def test_search_result_url(self):
-        result = SearchResult()
-        result.path = '/plone/path/to/item'
+        result = SearchResult(
+            'item',
+            '/plone/path/to/item',
+        )
         self.assertEqual(result.url,
                          'http://nohost/plone/path/to/item')
 
     def test_search_result_preview_image_url(self):
-        result = SearchResult()
-        result.preview_image_path = '/plone/path/to/item/preview'
+        result = SearchResult(
+            'item',
+            '/plone/path/to/item',
+            preview_path='/plone/path/to/item/preview',
+        )
         self.assertEqual(result.preview_image_url,
                          'http://nohost/plone/path/to/item/preview')
 
     def test_search_response(self):
-        res1 = SearchResult()
+        res1 = SearchResult(
+            'foo',
+            '/path/to/foo',
+        )
         result_list = [res1]
         response = SearchResponse(result_list)
         self.assertEqual(response.results, result_list)
