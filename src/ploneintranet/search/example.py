4cc0ed5e1d899bb1bff80ea97e55b7de801611b6
Ben Cole <wengole@gmail.com>
2015-05-06 16:25:32 +0100

    results should be a list of SearchResult objects

--- src/ploneintranet/search/example.py
+++ src/ploneintranet/search/example.py
@@ -3,7 +3,7 @@ from plone.batching import Batch
 from zope.interface import implements
 
 from .interfaces import ISiteSearch
-from .results import SearchResponse
+from .results import SearchResponse, SearchResult
 
 
 class ExampleSiteSearch(object):
@@ -41,14 +41,24 @@ class ExampleSiteSearch(object):
         facets.update({
             'SearchableText': keywords
         })
-        results = catalog.searchResults(facets)
-        batch = Batch(results, step, start)
+        brains = catalog.searchResults(facets)
+        batch = Batch(brains, step, start)
+        results = []
+        for brain in batch:
+            results.append(
+                SearchResult(
+                    brain['Title'],
+                    brain.getPath(),
+                    description=brain['Description'],
+                    document_type=brain['Type'],
+                )
+            )
         response_facets = {
             'content_type': {x['Type'] for x in results},
             'Subject': {y for x in results for y in x['Subject']},
         }
         response = SearchResponse(
-            batch,
+            results,
             total_results=len(results),
             facets=response_facets
         )
