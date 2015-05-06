2e3512f88674b0f611c917c2039fda3f91204aaa
JC Brand <jc@opkode.com>
2015-05-06 13:41:02 +0200

    Expand the files in ploneintranet.layout
    - Add configure.zcml and __init__.py where necessary
    - Add IDs to the messages
    - Include it in the suite's configure.zcml

--- src/ploneintranet/layout/viewlets/globalstatusmessage.py
+++ src/ploneintranet/layout/viewlets/globalstatusmessage.py
@@ -1,9 +1,19 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets import globalstatusmessage
+from Products.statusmessages.interfaces import IStatusMessage
+from plone.app.layout.viewlets.common import ViewletBase
+from plone.i18n.normalizer import idnormalizer
 
 
-class GlobalStatusMessage(globalstatusmessage.GlobalStatusMessage):
+class GlobalStatusMessage(ViewletBase):
     """ Displays messages to the current user.
         The markup is changed to rely on pat-notification.
     """
     index = ViewPageTemplateFile('globalstatusmessage.pt')
+
+    def update(self):
+        super(GlobalStatusMessage, self).update()
+        self.status = IStatusMessage(self.request)
+        messages = self.status.show()
+        for m in messages:
+            m.id = idnormalizer.normalize(m.message)
+        self.messages = messages
