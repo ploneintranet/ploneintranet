9f4ceef46987e04cd0093dcac49db76473ebb819
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-05-07 12:26:56 +0200

    Remove statusprovider

--- src/ploneintranet/microblog/browser/interfaces.py
+++ src/ploneintranet/microblog/browser/interfaces.py
@@ -1,17 +1,5 @@
 from zope.interface import Interface
-from zope.interface import Attribute
-from zope.contentprovider.interfaces import IContentProvider
-
-from zope.i18nmessageid import MessageFactory
-_ = MessageFactory('ploneintranet.microblog')
 
 
 class IPloneIntranetMicroblogLayer(Interface):
     """Marker interface to define ZTK browser layer"""
-
-
-class IStatusProvider(IContentProvider):
-    """Microblog input form as a content provider"""
-
-    portlet_data = Attribute(
-        "Optional slot for portlet data access")
