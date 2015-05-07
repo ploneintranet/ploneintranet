b9e4c3568d38c691c0db1b6d6336472e02a7c3e4
Matthew Sital-Singh <mattss@netsight.co.uk>
2015-05-07 17:22:50 +0100

    Refactor the SearchResults/SearchResponse implementation using adapters

--- src/ploneintranet/search/example.py
+++ src/ploneintranet/search/example.py
@@ -1,9 +1,83 @@
 from plone import api
 from plone.batching import Batch
 from zope.interface import implements
-
+from zope.globalrequest import getRequest
+from zope.component import adapts
+from Products.ZCatalog.interfaces import ICatalogBrain
 from .interfaces import ISiteSearch
-from .results import SearchResponse, SearchResult
+from .interfaces import ISearchResult, ISearchResponse
+
+
+class CatalogSearchResult(object):
+    """
+    Adapts a catalog brain to a search result
+    """
+    implements(ISearchResult)
+    adapts(ICatalogBrain)
+
+    title = None
+    description = None
+    path = None
+    preview_image_path = None
+    document_type = None
+    highlighted_summary = None
+
+    def __init__(self, brain):
+        self.title = brain['Title']
+        self.path = brain.getPath()
+        self.description = brain['Description']
+        #self.document_type = brain['friendly_type_name']
+        self.document_type = brain['Type']
+
+    def _path_to_url(self, physical_path):
+        """
+        Generate an absolute url from a physical path
+        """
+        if physical_path is not None:
+            request = getRequest()
+            return request.physicalPathToURL(physical_path)
+        else:
+            return None
+
+    @property
+    def url(self):
+        """
+        Generate the absolute URL for the indexed document
+        :return: The absolute URL to the document in Plone
+        :rtype: str
+        """
+        return self._path_to_url(self.path)
+
+    @property
+    def preview_image_url(self):
+        """
+        Generate the absolute URL for the preview image of the indexed document
+        :return: The absolute URL to the preview image
+        :rtype: str
+        """
+        return self._path_to_url(self.preview_image_path)
+
+
+class CatalogSearchResponse(object):
+    """
+    Adapts a batched catalog search to a search response
+    """
+    adapts(Batch)
+    implements(ISearchResponse)
+
+    results = None
+    spell_corrected_search = None
+    facets = None
+    total_results = None
+
+    def __init__(self, batched_results):
+        self.results = (ISearchResult(result) for result in batched_results)
+        self.total_results = batched_results.sequence_length
+        self.facets = {
+            #'friendly_type_name': {x['friendly_type_name'] for x in brains},
+            'friendly_type_name': {x['Type'] for x in batched_results},
+            'Subject': {y for x in batched_results for y in x['Subject']},
+        }
 
 
 class ExampleSiteSearch(object):
@@ -43,29 +117,5 @@ class ExampleSiteSearch(object):
         })
         brains = catalog.searchResults(facets)
         batch = Batch(brains, step, start)
-        results = []
-        for brain in batch:
-            if brain['has_thumbs']:
-                preview_path = brain.getPath() + '/docconv_image_thumb.jpg'
-            else:
-                preview_path = None
 
-            results.append(
-                SearchResult(
-                    brain['Title'],
-                    brain.getPath(),
-                    description=brain['Description'],
-                    document_type=brain['friendly_type_name'],
-                    preview_path=preview_path,
-                )
-            )
-        response_facets = {
-            'friendly_type_name': {x['friendly_type_name'] for x in brains},
-            'Subject': {y for x in brains for y in x['Subject']},
-        }
-        response = SearchResponse(
-            results,
-            total_results=len(results),
-            facets=response_facets
-        )
-        return response
+        return ISearchResponse(batch)
