2195cd8e361abfdb7218f18a71723522d8b54459
Wolfgang Thomas <thomas@syslab.com>
2015-05-07 14:04:15 +0200

    Revert "Merge pull request #166 from ploneintranet/integrate-raptor"
    This reverts commit f44088e760d37e3b37cd366aa196eb6a6a6d3050, reversing
    changes made to bc692413317f6965a2a02a70473230af584dfae4.

--- src/ploneintranet/theme/content/content.py
+++ src/ploneintranet/theme/content/content.py
@@ -4,7 +4,6 @@ from Products.CMFPlone.utils import safe_unicode
 from Products.Five import BrowserView
 from plone import api
 from plone.app.blocks.interfaces import IBlocksTransformEnabled
-from plone.app.textfield.value import RichTextValue
 from ploneintranet.docconv.client.interfaces import IDocconv
 from ploneintranet.workspace.utils import parent_workspace
 from zope.event import notify
@@ -16,22 +15,17 @@ from zope.lifecycleevent import ObjectModifiedEvent
 class ContentView(BrowserView):
     """View and edit class/form for all default DX content-types."""
 
-    def __call__(self, title=None, description=None, tags=[], text=None):
+    def __call__(self, title=None, description=None, tags=[]):
         """Render the default template and evaluate the form when editing."""
         context = aq_inner(self.context)
         self.workspace = parent_workspace(context)
         self.can_edit = api.user.has_permission(
             'Modify portal content',
             obj=context)
-        if self.can_edit and title or description or tags or text:
+        if self.can_edit and title or description or tags:
             modified = False
             if title and safe_unicode(title) != context.title:
                 context.title = safe_unicode(title)
-                modified = True
-            if text:
-                richtext = RichTextValue(raw=text, mimeType='text/html',
-                                         outputMimeType='text/x-html-safe')
-                context.text = richtext
                 modified = True
             if description:
                 if safe_unicode(description) != context.description:
