73045e9188d13acd89565246db95dd4b8e320ff2
Cillian de Róiste <deroiste@syslab.com>
2015-05-15 11:42:29 +0200

    Todo: tidy up the code to get the milestone
    Note: I removed the custom __call__ method on todo_view.BaseView because
    I have no idea what purpose it serves and it makes things more
    complicated to keep it.

--- src/ploneintranet/todo/browser/todo_view.py
+++ src/ploneintranet/todo/browser/todo_view.py
@@ -1,15 +1,13 @@
 from ..interfaces import ITodoUtility
 from Acquisition import aq_inner
-from Products.Five import BrowserView
 from plone import api
-from ploneintranet.workspace.interfaces import IMetroMap
-from ploneintranet.workspace.utils import parent_workspace
+from ploneintranet.workspace.browser.workspace import BaseWorkspaceView
 from zope.component import getUtility
 from zope.event import notify
 from zope.lifecycleevent import ObjectModifiedEvent
 
 
-class BaseView(BrowserView):
+class BaseView(BaseWorkspaceView):
 
     def __init__(self, context, request):
         super(BaseView, self).__init__(context, request)
@@ -17,25 +15,17 @@ class BaseView(BrowserView):
         self.current_user_id = api.user.get_current().getId()
         self.content_uid = api.content.get_uuid(self.context)
 
-    def __call__(self):
-        referer = self.request.get('HTTP_REFERER', '').strip()
-        if not referer:
-            referer = self.context.absolute_url()
-        return self.request.response.redirect(referer)
-
 
 class TodoView(BaseView):
+
     def __call__(self, milestone=None):
         context = aq_inner(self.context)
         self.can_edit = api.user.has_permission('Modify portal content', obj=context)
 
-        self.workspace = parent_workspace(context)
-        if self.workspace.is_case:
-            if milestone is not None:
+        if self.workspace().is_case:
+            if self.can_edit and milestone is not None and milestone != context.milestone:
                 context.milestone = milestone
                 context.reindexObject()
                 notify(ObjectModifiedEvent(context))
 
-            self.milestones = IMetroMap(self.workspace).metromap_sequence.keys()
-
-        return super(BaseView, self).__call__()
+        return super(TodoView, self).__call__()
