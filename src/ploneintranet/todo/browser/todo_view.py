bbb046916be116cab8576fbc10913eccc134b315
Cillian de Róiste <deroiste@syslab.com>
2015-05-19 14:13:38 +0200

    Todo item: reindex when modified, show notifications

--- src/ploneintranet/todo/browser/todo_view.py
+++ src/ploneintranet/todo/browser/todo_view.py
@@ -1,10 +1,12 @@
 from ..interfaces import ITodoUtility
 from Acquisition import aq_inner
 from plone import api
+from ploneintranet.theme.utils import dexterity_update
 from ploneintranet.workspace.browser.workspace import BaseWorkspaceView
 from zope.component import getUtility
 from zope.event import notify
 from zope.lifecycleevent import ObjectModifiedEvent
+from ploneintranet.theme import _
 
 
 class BaseView(BaseWorkspaceView):
@@ -22,12 +24,25 @@ class TodoView(BaseView):
         context = aq_inner(self.context)
         self.can_edit = api.user.has_permission(
             'Modify portal content', obj=context)
+        modified, errors = dexterity_update(context)
 
         if self.workspace().is_case:
             if self.can_edit and milestone is not None \
                and milestone != context.milestone:
                 context.milestone = milestone
-                context.reindexObject()
-                notify(ObjectModifiedEvent(context))
+                modified = True
+
+        if modified:
+            api.portal.show_message(
+                _("Item updated."), request=self.request, type="success")
+            context.reindexObject()
+            notify(ObjectModifiedEvent(context))
+
+        if errors:
+                api.portal.show_message(
+                    _("There was an error."),
+                    request=self.request,
+                    type="error",
+                )
 
         return super(TodoView, self).__call__()
