221b28e522359bb8550c41a654455d4129f354c4
Ben Cole <wengole@gmail.com>
2015-05-20 15:38:12 +0100

    Removed test view

--- src/ploneintranet/async/__init__.py
+++ src/ploneintranet/async/__init__.py
@@ -1,7 +1,6 @@
 __author__ = 'benc'
 
 from Products.Five import BrowserView
-from ploneintranet.async.celerytasks import message
 from plone import api
 
 
@@ -11,14 +10,3 @@ class CheckAuth(BrowserView):
             print "User is anonymous"
             self.request.response.setStatus(403, lock=True)
         return api.user.get_current().id
-
-
-class SendMessage(BrowserView):
-    def __call__(self):
-        if api.user.is_anonymous():
-            print "User is anonymous"
-            self.request.response.setStatus(403, lock=True)
-        # message.delay(api.user.get_current().id, "Here is a message")
-        # Hardcode for admin for now.
-        message.delay('admin', u'Here is a message for admin.')
-        return 'Message scheduled'
