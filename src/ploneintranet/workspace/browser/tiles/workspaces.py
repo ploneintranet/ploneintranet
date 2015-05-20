df8ad09ec472c60f69390484f0ab239c547a3572
Cillian de Róiste <deroiste@syslab.com>
2015-05-20 16:08:17 +0200

    workspaces: set the appropriate class for Case objects

--- src/ploneintranet/workspace/browser/tiles/workspaces.py
+++ src/ploneintranet/workspace/browser/tiles/workspaces.py
@@ -34,16 +34,20 @@ def my_workspaces(context):
         sort_on="modified",
         sort_order="reversed",
     )
-    workspaces = [
-        {
+    workspaces = []
+    for brain in brains:
+        css_class = escape_id_to_class(brain.getId)
+        if brain.portal_type == 'ploneintranet.workspace.case':
+            css_class = 'type-case ' + css_class
+        workspaces.append({
             'id': brain.getId,
             'title': brain.Title,
             'description': brain.Description,
             'url': brain.getURL(),
             'activities': get_workspace_activities(brain),
-            'class': escape_id_to_class(brain.getId),
-        } for brain in brains
-    ]
+            'class': css_class,
+        })
+
     return workspaces
 
 
