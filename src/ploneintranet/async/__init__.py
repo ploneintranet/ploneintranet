bd28f8d863adc2c61a88b51d5f9aaca55986fc6d
Maurits van Rees <maurits@vanrees.org>
2015-05-20 15:18:23 +0100

    Add convert-document view to call from Celery task.

--- src/ploneintranet/async/__init__.py
+++ src/ploneintranet/async/__init__.py
@@ -2,6 +2,11 @@ __author__ = 'benc'
 
 from Products.Five import BrowserView
 from plone import api
+from plone.protect import PostOnly
+from plone.protect.interfaces import IDisableCSRFProtection
+from ploneintranet.async.celerytasks import message
+from ploneintranet.docconv.client import handlers
+from zope.interface import alsoProvides
 
 
 class CheckAuth(BrowserView):
@@ -10,3 +15,22 @@ class CheckAuth(BrowserView):
             print "User is anonymous"
             self.request.response.setStatus(403, lock=True)
         return api.user.get_current().id
+
+
+class ConvertDocument(BrowserView):
+    """Convert a document to pdf and preview images.
+
+    Meant to be called from a Celery task.
+
+    Note that we explicitly disable CSRF protection.  There is no form
+    that we can fill in, so there is no authenticator token that we
+    could check.  Alternative: create such a form anyway, GET it from
+    a Celery task, parse the html to get the authenticator token, and
+    POST to the form again.  Seems overkill.
+    """
+
+    def __call__(self):
+        PostOnly(self.request)
+        alsoProvides(self.request, IDisableCSRFProtection)
+        handlers._update_preview_images(self.context, event=None)
+        return 'OK'
