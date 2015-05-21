ffe0b1cecbfe9472d3a34de5ad08329161f1fd58
Matthew Sital-Singh <mattss@netsight.co.uk>
2015-05-21 15:47:43 +0100

    Override the author view for user profiles

--- src/ploneintranet/userprofile/browser/userprofile.py
+++ src/ploneintranet/userprofile/browser/userprofile.py
@@ -1,4 +1,8 @@
 from Products.Five import BrowserView
+from Products.CMFPlone.browser.author import AuthorView as BaseAuthorView
+
+from ploneintranet import api as pi_api
+from zExceptions import NotFound
 
 
 class UserProfileView(BrowserView):
@@ -6,3 +10,15 @@ class UserProfileView(BrowserView):
     """View for user profile."""
 
     pass
+
+
+class AuthorView(BaseAuthorView):
+    """Overrides default author view to link to PI profiles"""
+    def __call__(self):
+        profile = pi_api.userprofile.get(self.username)
+
+        if profile is not None:
+            return self.request.response.redirect(
+                profile.absolute_url()
+            )
+        raise NotFound
