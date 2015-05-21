3e1b1a1d441710efa4afc425a84462abdcf8ba56
Matthew Sital-Singh <mattss@netsight.co.uk>
2015-05-21 16:00:45 +0100

    Initial avatar image added to profile page

--- src/ploneintranet/userprofile/browser/userprofile.py
+++ src/ploneintranet/userprofile/browser/userprofile.py
@@ -1,6 +1,7 @@
 from Products.Five import BrowserView
 from Products.CMFPlone.browser.author import AuthorView as BaseAuthorView
 
+from plone import api
 from ploneintranet import api as pi_api
 from zExceptions import NotFound
 
@@ -9,7 +10,17 @@ class UserProfileView(BrowserView):
 
     """View for user profile."""
 
-    pass
+    def is_me(self):
+        """Does this user profile belong to the current user"""
+        return self.context.username == api.user.get_current().getUserName()
+
+    def avatar_url(self):
+        """Avatar url for this profile"""
+        portal_url = api.portal.get().absolute_url()
+        return u'%s/portal_memberdata/portraits/%s' % (
+            portal_url,
+            self.context.username,
+        )
 
 
 class AuthorView(BaseAuthorView):
