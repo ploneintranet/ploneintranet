1628b5886cb9d8f0878c80862a1d7661566b15fc
Cillian de Róiste <deroiste@syslab.com>
2015-05-27 12:57:42 +0200

    Dashboard: list open Todo items, and allow them to be closed

--- src/ploneintranet/theme/browser/dashboard.py
+++ src/ploneintranet/theme/browser/dashboard.py
@@ -1,6 +1,9 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from collections import defaultdict
+from plone import api
 from plone.app.blocks.interfaces import IBlocksTransformEnabled
 from plone.tiles import Tile
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ploneintranet.workspace.utils import parent_workspace
 from zope.interface import implements
 from zope.publisher.browser import BrowserView
 
@@ -35,4 +38,25 @@ class TasksTile(Tile):
         return self.index()
 
     def __call__(self):
+        """Display a list of Todo items in the Open workflow state, grouped by
+        Workspace and ordered by Due date.
+        {'workspace1': {'title': 'WS1', 'url': '...', 'tasks':[<brain>, ...]}}
+        """
+        pc = api.portal.get_tool('portal_catalog')
+        tasks = pc(portal_type='todo', review_state='open', sort_order='due')
+        self.grouped_tasks = {}
+        for task in tasks:
+            obj = task.getObject()
+            workspace = parent_workspace(obj)
+            workspace_title = workspace.title
+            if workspace.id not in self.grouped_tasks:
+                self.grouped_tasks[workspace.id] = {
+                    'title': workspace.title,
+                    'url': workspace.absolute_url(),
+                    'tasks': [task],
+                }
+            else:
+                self.grouped_tasks[workspace.id]['tasks'].append(task)
         return self.render()
+
+
