403b29dd8e1c5af13eac0a7caa9a71319d78719e
Maurits van Rees <maurits@vanrees.org>
2015-06-01 14:27:42 +0200

    Run our entry point only when releasing ploneintranet.

--- src/ploneintranet/core/release.py
+++ src/ploneintranet/core/release.py
@@ -6,6 +6,10 @@ logger = logging.getLogger('ploneintranet.core.release')
 
 def add_files_to_release(data):
     """zest.releaser entry point for adding files to the release."""
+    if data.get('name') != 'ploneintranet':
+        # This entry point should only do something when releasing the
+        # ploneintranet package.
+        return
     # cd to the directory that has the fresh tag checkout and use the
     # Makefile to fetch the release bundles.
     logger.info('Making fetchrelease call in tag checkout.')
