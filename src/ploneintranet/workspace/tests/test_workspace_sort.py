011737c5e0a7256ad233ca08db1ae0e2e8dbd3eb
Claudio Kirchhoff <claudio.kirchhoff@gmail.com>
2015-06-05 19:46:39 +0200

    add test for my_workspaces

--- src/ploneintranet/workspace/tests/test_workspace_sort.py
+++ src/ploneintranet/workspace/tests/test_workspace_sort.py
@@ -23,20 +23,27 @@ from zope.publisher.browser import TestRequest
 # from zope.annotation.interfaces import IAttributeAnnotatable
 #from ploneintranet.workspace.testing import \
 #    PLONEINTRANET_WORKSPACE_FUNCTIONAL_TESTING
-
+from ploneintranet.workspace.browser.tiles.workspaces import my_workspaces
 
 class TestWorkspaceSort(BaseTestCase):
 
     def test_workspace_sort(self):
         self.login_as_portal_owner()
-        request = TestRequest()
+        request = self.layer['request']
         
         for sort_by in ('alphabet', 'newest', 'activity'):
             request.sort = sort_by
             ws_container = api.content.get_view(
                 name='workspaces.html',
-                context=self.portal,
+                context=self.portal.workspaces,
                 request = request)
-            assertEqual(len(ws_container.sort_options()),3)
-            assertEqual(ws_container.sort_options()[0]['value'],'alphabet')
+            self.assertEqual(len(ws_container.sort_options()),3)
+            self.assertEqual(ws_container.sort_options()[0]['value'], sort_by)
 
+    def test_my_workspaces(self):
+        api.content.create(
+            self.portal.workspaces,
+            'ploneintranet.workspace.workspacefolder',
+            'example-workspace')
+        ws = my_workspaces(self.portal.workspaces)
+        self.assertEqual(len(ws) > 0, True)
