935b0c9b86e6b3ee73f114acaa9ea6ce58a0ea4d
Ben Cole <benc@netsight.co.uk>
2015-06-25 15:04:16 +0100

    Simplified attachments upload view

--- src/ploneintranet/attachments/browser/upload.py
+++ src/ploneintranet/attachments/browser/upload.py
@@ -1,10 +1,7 @@
 import logging
 
 from Products.Five.browser import BrowserView
-from plone import api
 from plone.memoize.view import memoize
-from plone.app.contenttypes.content import File
-from plone.app.contenttypes.content import Image
 
 from ploneintranet import api as pi_api
 
@@ -12,76 +9,32 @@ log = logging.getLogger(__name__)
 
 
 class UploadAttachments(BrowserView):
+    attachments = []
 
     @property
     @memoize
     def fallback_thumbs_urls(self):
         """ Return a dummy image thumbnails
-
-        BBB: Ask for a better image :)
-        See #122
         """
-        url = '/'.join((
-            api.portal.get().absolute_url(),
-            '++theme++ploneintranet.theme/generated/media/logo.svg'
-        ))
-        return [url]
-
-    def get_docconv_thumbs_urls(self, attachment):
-        """Returns the URLs of all the rendered pages of the document.
-        """
-        # TODO: actually implement this.
-        # The previous implementation relied heavily
-        # on the IDocconv abstraction layer
-        # which is now gone after async refactoring.
-        return []
-
-    def get_image_thumbs_urls(self, image):
-        """
-        If we have an Image or a File object,
-        we ask the Plone scale machinery for a URL
-        """
-        images = api.content.get_view(
-            'images',
-            image,
-            self.request,
-        )
-        try:
-            urls = [images.scale(scale='preview').absolute_url()]
-        except:
-            log.error('Preview url generation failed')
-            urls = []
-        return urls
-
-    def get_thumbs_urls(self, attachment):
-        """ This will return the URL for the thumbs of the attachment
-
-        """
-        # We first ask docconv for a URL
-        urls = self.get_docconv_thumbs_urls(attachment)
-        if urls:
-            return urls
-
-        # If we have an image we return the usual preview url
-        if isinstance(attachment, (Image, File)):
-            urls = self.get_image_thumbs_urls(attachment)
-        if urls:
-            return urls
-
-        # If every other method fails return a fallback
-        return self.fallback_thumbs_urls
+        return [pi_api.previews.fallback_image_url()]
 
     def __call__(self):
-        uploaded_attachments = self.request.get('form.widgets.attachments')
-        if not uploaded_attachments:
-            return self.index()
-
+        """Attach any new attachments to the context and render the preview
+        thumbnails
+        """
+        uploaded_attachments = self.request.get('form.widgets.attachments', [])
+        if not isinstance(uploaded_attachments, list):
+            uploaded_attachments = [uploaded_attachments]
         for file_field in uploaded_attachments:
             pi_api.attachments.add(
                 self.context,
                 file_field.filename,
                 file_field.read())
-
+        current_attachments = pi_api.attachments.get_attachments(self.context)
+        for att in current_attachments:
+            self.attachments.append(
+                pi_api.previews.get_thumbnail_url(att)
+            )
         return self.index()
 
 
