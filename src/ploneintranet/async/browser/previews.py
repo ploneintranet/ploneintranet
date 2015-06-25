1287063787bca3147fb255878e9234d321ee7848
Ben Cole <benc@netsight.co.uk>
2015-06-25 14:55:40 +0100

    Preview and Thumbnail views

--- src/ploneintranet/async/browser/previews.py
+++ src/ploneintranet/async/browser/previews.py
@@ -1,71 +1,37 @@
 from Products.Five import BrowserView
+from plone.namedfile.utils import stream_data
 
 from ploneintranet import api as pi_api
 
 
-class ImageView(BrowserView):
-    """ Base class for views that render preview related images """
-
-    def pages_count(self):
-        return len(self._get_data() or [])
-
-    def available(self):
-        """ check if we have a preview image """
-        raise NotImplementedError
-
-    def _get_data(self):
-        raise NotImplementedError
-
-    def __call__(self):
-        page = int(self.request.get('page', 1))
-
-        previews = self._get_data()
-        if previews:
-            if page - 1 >= len(previews):
-                page = 0
-            elif page < 1:
-                page = 1
-            imgdata = previews[page - 1]
-            R = self.request.RESPONSE
-            R.setHeader('content-type', 'image/jpeg')
-            R.setHeader(
-                'content-disposition',
-                'inline; filename="{0}_preview.jpg"'.format(
-                    self.context.getId().encode('utf8'))
-            )
-            if isinstance(imgdata, basestring):
-                length = len(imgdata)
-                R.setHeader('content-length', length)
-                return imgdata
-            else:
-                length = imgdata.get_size(self.context)
-                R.setHeader('content-length', length)
-                blob = imgdata.get(self.context, raw=True)
-                charset = 'utf-8'
-                return blob.index_html(
-                    REQUEST=self.request, RESPONSE=R,
-                    charset=charset
-                )
-
-        self.request.RESPONSE.setStatus(404)
-        return None
-
-
-class PreviewView(ImageView):
+class PreviewView(BrowserView):
+    """View providing thumbnail image of context
     """
+    def __call__(self, page=1, scale='preview'):
+        try:
+            self.page = int(page)
+        except ValueError:
+            self.page = 1
+        self.scale = scale
+        img = self.get_data()
+        imgdata = img.data
+        R = self.request.RESPONSE
+        R.setHeader('content-type', 'image/png')
+        R.setHeader(
+            'content-disposition',
+            'inline; filename="{0}_preview.png"'.format(
+                self.context.getId().encode('utf8'))
+        )
+        R.setHeader('content-length', imgdata.size)
+        return stream_data(imgdata)
 
-    """
-
-    def _get_data(self):
-        return pi_api.previews.get(self.context)
-
-    def available(self):
-        return pi_api.previews.has_previews(self.context)
+    def get_data(self):
+        prev = pi_api.previews.get_previews(self.context, self.scale)
+        return prev[self.page - 1]
 
 
-class ThumbnailView(ImageView):
-    def _get_data(self):
-        return pi_api.previews.get_thumb(self.context)
+class ThumbnailView(PreviewView):
 
-    def available(self):
-        return pi_api.previews.has_thumb(self.context)
+    def get_data(self):
+        thumb = pi_api.previews.get_thumbnail(self.context)
+        return thumb
