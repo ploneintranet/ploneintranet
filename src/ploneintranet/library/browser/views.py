9cd49ca81f2542adf0e831cc1df7ce379d84cfc7
Guido A.J. Stevens <guido.stevens@cosent.nl>
2015-06-27 11:23:35 +0000

    library home/app views WIP

---
+++ src/ploneintranet/library/browser/views.py
@@ -0,0 +1,34 @@
+from logging import getLogger
+from Products.Five import BrowserView
+from plone import api
+
+log = getLogger(__name__)
+
+
+class LibraryHomeView(BrowserView):
+    """
+    The '/library' link in the portal tabs is coded as a CMF action
+    and not derived from the actual library app URL.
+    This view redirects to the, or a, actual LibraryApp/view.
+    """
+    def __call__(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        results = catalog(portal_type='ploneintranet.library.app')
+        if not results:
+            msg = "Somebody removed the last library app and broke the site."
+            log.error(msg)
+            return msg
+        target = results[0].getURL()
+
+        if len(results) > 1:
+            # pick the first one, unless there is one actually called 'libary'
+            for brain in results:
+                if brain.id == 'library':
+                    target = brain.getURL()
+
+        # get us to an actual LibraryAppView
+        self.request.response.redirect(target)
+
+
+class LibraryAppView(BrowserView):
+    pass
