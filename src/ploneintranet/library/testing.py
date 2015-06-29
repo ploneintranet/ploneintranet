3dada603201be0e5561baf870b30a906fa1fc6a1
Guido A.J. Stevens <guido.stevens@cosent.nl>
2015-06-29 10:31:03 +0000

    fix library testing fixture

--- src/ploneintranet/library/testing.py
+++ src/ploneintranet/library/testing.py
@@ -1,39 +1,10 @@
 # -*- coding: utf-8 -*-
-"""Base module for unittesting ploneintranet.search."""
-from contextlib import contextmanager
+import unittest2 as unittest
 
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app import testing
-from plone import api
-import unittest2 as unittest
-import ploneintranet.library
-import ploneintranet.docconv.client
+
 from ploneintranet.testing import PLONEINTRANET_FIXTURE
-
-
-TEST_USER_1_NAME = 'icarus'
-
-TEST_USER_1_EMAIL = 'icarus@ploneintranet.org'
-
-
-@contextmanager
-def login_session(username):
-    """Temporarily login as another user.
-
-    Re-logging-in the previous user after exiting the context.
-    """
-    portal = api.portal.get()
-    prev_login = None
-    try:
-        if not api.user.is_anonymous():
-            prev_login = api.user.get_current().getUserName()
-        testing.logout()
-        testing.login(portal, username)
-        yield username
-    finally:
-        testing.logout()
-        if prev_login:
-            testing.login(portal, prev_login)
 
 
 class PloneintranetLibraryLayer(testing.PloneSandboxLayer):
@@ -42,8 +13,14 @@ class PloneintranetLibraryLayer(testing.PloneSandboxLayer):
                     PLONEINTRANET_FIXTURE)
 
     def setUpZope(self, app, configurationContext):
+        import ploneintranet.library
         self.loadZCML(package=ploneintranet.library)
+
+        import ploneintranet.docconv.client
         self.loadZCML(package=ploneintranet.docconv.client)
+
+        import ploneintranet.layout
+        self.loadZCML(package=ploneintranet.layout)
 
     def setUpPloneSite(self, portal):
         self.applyProfile(portal, 'ploneintranet.docconv.client:default')
