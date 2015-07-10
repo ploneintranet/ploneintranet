89ed0fd259d58039669f054a563293baa0396d57
Matthew Sital-Singh <mattss@netsight.co.uk>
2015-07-10 13:21:55 +0100

    Render status update correctly re-using the activity stream view

--- src/ploneintranet/notifications/adapters/message_factory.py
+++ src/ploneintranet/notifications/adapters/message_factory.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from plone import api
 from ploneintranet.notifications.message import Message
+from ploneintranet.activitystream.browser.statusupdate \
+    import StatusUpdateView
 
 
 class Base(object):
@@ -52,8 +54,15 @@ class StatusUpdate(Base):
         ''' return a dict representation of the object
         '''
         portal_relative_url = api.portal.get().absolute_url(relative=True)
+        portal = api.portal.get()
+        view = StatusUpdateView(self.context, portal.REQUEST)
+        text = view.decorated_text
+
         return {
             'id': self.context.id,
-            'url': portal_relative_url + '/@@stream/network',
-            'title': self.context.text
+            'url': '{0}/@@stream/network#post-{1}'.format(
+                portal_relative_url,
+                self.context.id,
+            ),
+            'title': text
         }
