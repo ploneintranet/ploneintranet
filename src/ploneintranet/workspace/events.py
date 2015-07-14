ef836e39cb3915ce1e960f4b6d5e58f3d0f815e0
Matthew Sital-Singh <mattss@netsight.co.uk>
2015-07-14 16:29:31 +0100

    Revert policy change event
    This must have been a red herring

--- src/ploneintranet/workspace/events.py
+++ src/ploneintranet/workspace/events.py
@@ -1,15 +1,16 @@
 from zope.interface import implements
+from zope.component.interfaces import ObjectEvent
 
 from ploneintranet.workspace.interfaces import IParticipationPolicyChangedEvent
 
 
-class ParticipationPolicyChangedEvent(object):
+class ParticipationPolicyChangedEvent(ObjectEvent):
     """ Event class, which is fired once the participation policy
     of the workspace has changed
     """
     implements(IParticipationPolicyChangedEvent)
 
     def __init__(self, ob, old_policy, new_policy):
-        self.workspace = ob
+        super(ParticipationPolicyChangedEvent, self).__init__(ob)
         self.old_policy = old_policy
         self.new_policy = new_policy
