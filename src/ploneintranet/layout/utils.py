f0f0e8bd0c0b3c21dae692c125147a773ecf7648
Wolfgang Thomas <thomas@syslab.com>
2015-08-08 14:25:50 +0200

    replace misleading variable name

--- src/ploneintranet/layout/utils.py
+++ src/ploneintranet/layout/utils.py
@@ -6,10 +6,10 @@ and have no dependencies of other packages
 from Products.CMFPlone.utils import safe_unicode
 
 
-def shorten(text, length=20, ellipse=u'\u2026'):
+def shorten(text, length=20, ellipsis=u'\u2026'):
     text = text.replace('_', ' ').replace('-', ' ')
     if len(text) <= length:
         return text
     if ' ' not in text:
-        return u'%s %s' % (safe_unicode(text[:length - 4]), ellipse)
-    return u" ".join(safe_unicode(text)[:length].split(" ")[:-1] + [ellipse])
+        return u'%s %s' % (safe_unicode(text[:length - 4]), ellipsis)
+    return u" ".join(safe_unicode(text)[:length].split(" ")[:-1] + [ellipsis])
