007497cdac3aa30428a40e48fc272e251c2e2833
Cillian de Róiste <deroiste@syslab.com>
2015-08-19 17:37:29 +0200

    Remove the following.tile, not needed

--- src/ploneintranet/userprofile/browser/tiles/following.py
+++
@@ -1,7 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.tiles import Tile
-from ploneintranet.userprofile.browser.userprofile import UserProfileView
-
-
-class FollowingTile(Tile, UserProfileView):
-    pass
