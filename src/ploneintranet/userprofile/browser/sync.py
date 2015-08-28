19b7853c240c61488c8e34688986a9663dcfbb9b
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2015-08-28 15:20:12 +0100

    Add testing profile which sets up mutable property plugin

--- src/ploneintranet/userprofile/browser/sync.py
+++ src/ploneintranet/userprofile/browser/sync.py
@@ -7,7 +7,8 @@ class UserPropertySync(BrowserView):
     def sync_properties(self):
         """Sync properties from PAS into the membrane object.
         """
-        read_only = plone_api.portal.get_registry_record(
+        read_only = api.portal.get_registry_record(
             'ploneintranet.userprofile.read_only_fields')
         membrane = api.portal.get_tool('membrane_tool')
         user_brains = membrane.searchResults()
+        return len(user_brains)
--- src/ploneintranet/userprofile/tests/test_sync.py
+++ src/ploneintranet/userprofile/tests/test_sync.py
@@ -1,11 +1,14 @@
 from plone import api
 from ploneintranet.userprofile.tests.base import BaseTestCase
+from ploneintranet.userprofile.browser.sync import UserPropertySync
 
 
 class TestUserPropertySync(BaseTestCase):
+    """The following tests use ZODBMutablePropertyProvider ('mutable_properties)
+    as a example foreign property provider.
+    """
 
     def _create_userprofiles(self):
-        self.login_as_portal_owner()
         api.content.create(
             container=self.profiles,
             type='ploneintranet.userprofile.userprofile',
@@ -17,7 +20,10 @@ class TestUserPropertySync(BaseTestCase):
             password='secret',
             confirm_password='secret',
         )
-        self.logout()
 
     def test_sync_properties(self):
+        self.login_as_portal_owner()
         self._create_userprofiles()
+        sync_view = UserPropertySync(self.portal, self.request)
+        self.assertEqual(
+            sync_view.sync_properties(), 1)
