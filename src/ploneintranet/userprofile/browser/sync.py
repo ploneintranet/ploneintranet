aa1083a923b45f41599611c40cbec80b1d024cc5
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2015-09-01 17:18:44 +0100

    Implementation for user attribute sync process.

--- src/ploneintranet/userprofile/browser/sync.py
+++ src/ploneintranet/userprofile/browser/sync.py
@@ -1,19 +1,69 @@
+import logging
+import time
+
 from Products.Five import BrowserView
 from plone import api
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import Interface
+
+from ..content.userprofile import IUserProfile
 
 
-class UserPropertySync(BrowserView):
+class IUserProfileManager(Interface):
+    """Sync properties for a user profile
+    """
 
-    def sync_properties(self):
-        """Sync properties from PAS into the membrane object.
+_no_value = object()
+logger = logging.getLogger(__name__)
+
+
+@adapter(IUserProfile)
+@implementer(IUserProfileManager)
+class UserPropertyManager(object):
+
+    def __init__(self, context):
+        self.context = context
+        self.property_mapping = api.portal.get_registry_record(
+            'ploneintranet.userprofile.property_sheet_mapping')
+
+    def sync(self):
+        logger.info('Updating info for {0.username}'.format(self.context))
+        member = api.user.get(username=self.context.username)
+        property_sheet_mapping = {
+            sheet.getId(): sheet
+            for sheet in member.getOrderedPropertySheets()
+        }
+        membrane_properties = property_sheet_mapping.pop('membrane_properties')
+        changed = False
+        for (property_name, pas_plugin_id) in self.property_mapping.items():
+            sheet = property_sheet_mapping[pas_plugin_id]
+            value = sheet.getProperty(property_name, default=_no_value)
+            if value is not _no_value:
+                membrane_properties.setProperty(member, property_name, value)
+                changed = True
+
+        if changed:
+            self.context.reindexObject()
+
+
+class AllUsersPropertySync(BrowserView):
+
+    def _get_users_to_sync(self):
+        """Override this to specify a subset of users to
+        be updated.
         """
-        read_only = api.portal.get_registry_record(
-            'ploneintranet.userprofile.read_only_fields')
         membrane = api.portal.get_tool('membrane_tool')
+        for brain in membrane.searchResults():
+            yield brain.getObject()
 
-        user_brains = membrane.searchResults()
-        for brain in user_brains:
-            member = api.user.get(username=brain['getId'])
-            sheets = member.getOrderedPropertySheets()
-
-        return len(user_brains)
+    def __call__(self):
+        """Sync properties from PAS into the membrane object for
+        all users across the application.
+        """
+        start = time.time()
+        for (count, user) in enumerate(self._get_users_to_sync(), start=1):
+            IUserProfileManager(user).sync()
+        duration = time.time() - start
+        logger.info('Updated {} in {:0.2f} seconds'.format(
+            count, duration))
--- src/ploneintranet/userprofile/tests/test_sync.py
+++ src/ploneintranet/userprofile/tests/test_sync.py
@@ -1,29 +1,49 @@
+"""
+The following tests use ZODBMutablePropertyProvider ('mutable_properties)
+as a example foreign property provider.
+"""
+
 from plone import api
+from ploneintranet import api as pi_api
 from ploneintranet.userprofile.tests.base import BaseTestCase
-from ploneintranet.userprofile.browser.sync import UserPropertySync
+from ploneintranet.userprofile.browser.sync import IUserProfileManager
 
 
-class TestUserPropertySync(BaseTestCase):
-    """The following tests use ZODBMutablePropertyProvider ('mutable_properties)
-    as a example foreign property provider.
-    """
+def _create_userprofiles(container):
+    user = api.content.create(
+        container=container,
+        type='ploneintranet.userprofile.userprofile',
+        id='johndoe',
+        username='johndoe',
+        first_name=u'John',
+        last_name=u'Doe',
+        email='johndoe@example.com',
+        password='secret',
+        confirm_password='secret',
+    )
+    return user
 
-    def _create_userprofiles(self):
-        api.content.create(
-            container=self.profiles,
-            type='ploneintranet.userprofile.userprofile',
-            id='johndoe',
-            username='johndoe',
-            first_name=u'John',
-            last_name=u'Doe',
-            email='johndoe@example.com',
-            password='secret',
-            confirm_password='secret',
-        )
 
-    def test_sync_properties(self):
+def set_foreign_property(member, key, value):
+    acl_users = api.portal.get_tool(name='acl_users')
+    plugin = acl_users['mutable_properties']
+    sheet = plugin.getPropertiesForUser(member)
+    sheet.setProperty(member, key, value)
+
+
+class TestUserPropertyManager(BaseTestCase):
+
+    def test_sync(self):
         self.login_as_portal_owner()
-        self._create_userprofiles()
-        sync_view = UserPropertySync(self.portal, self.request)
-        self.assertEqual(
-            sync_view.sync_properties(), 1)
+        user = _create_userprofiles(self.profiles)
+        member = api.user.get(username=user.username)
+        email = 'test@foo.com'
+        set_foreign_property(member, 'email', email)
+
+        user = pi_api.userprofile.get(user.username)
+        IUserProfileManager(user).sync()
+        self.assertEqual(user.email, email)
+        catalog = api.portal.get_tool(name='portal_catalog')
+        brains = catalog.searchResults(portal_type=user.portal_type)
+        self.assertEqual(len(brains), 1)
+        self.assertEqual(brains[0]['email'], email)
