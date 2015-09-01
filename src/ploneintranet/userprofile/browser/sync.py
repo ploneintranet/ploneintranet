66ef999e125e2ba1cdd6fbe76c05e79e4c5de791
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2015-09-01 11:48:12 +0100

    look up member and property sheet for each user brain

--- src/ploneintranet/userprofile/browser/sync.py
+++ src/ploneintranet/userprofile/browser/sync.py
@@ -10,5 +10,10 @@ class UserPropertySync(BrowserView):
         read_only = api.portal.get_registry_record(
             'ploneintranet.userprofile.read_only_fields')
         membrane = api.portal.get_tool('membrane_tool')
+
         user_brains = membrane.searchResults()
+        for brain in user_brains:
+            member = api.user.get(username=brain['getId'])
+            sheets = member.getOrderedPropertySheets()
+
         return len(user_brains)
