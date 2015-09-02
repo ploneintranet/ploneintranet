47307cd826ae01a5b1186aed697d885fe4a6457d
Matthew Sital-Singh <mattss@netsight.co.uk>
2015-09-02 16:36:39 +0100

    More comments helps

--- src/ploneintranet/userprofile/ldap.py
+++ src/ploneintranet/userprofile/ldap.py
@@ -1,3 +1,6 @@
+"""
+LDAP/AD-specific support for user property syncing
+"""
 import logging
 
 from plone.app.ldap.ploneldap.util import getLDAPPlugin
@@ -7,6 +10,9 @@ from .sync import get_last_sync
 
 logger = logging.getLogger(__name__)
 
+# TODO: Make this configurable
+LDAP_CHANGED_ATTRIBUTE = 'whenChanged'
+
 
 class LDAPChangedUsersPropertySync(AllUsersPropertySync):
 
@@ -14,15 +20,20 @@ class LDAPChangedUsersPropertySync(AllUsersPropertySync):
         """Perform a custom LDAP query to limit the users
         who should be updated.
 
-        Looks for users with a 'whenChanged' value
+        Looks for users that have changed since the last sync,
+        according to the LDAP_CHANGED_ATTRIBUTE
 
-        Requires plone.app.ldap
+        (Requires plone.app.ldap)
         """
         last_sync = get_last_sync(self.context)
         if not last_sync:
-            # Never been synced - so sync all current membrane profiles
+            # First sync is always a full sync
             return super(LDAPChangedUsersPropertySync,
                          self)._get_users_to_sync()
+
+        logger.info('Looking for LDAP users changed since {0}'.format(
+            last_sync,
+        ))
 
         try:
             ldap_plugin = getLDAPPlugin()
@@ -31,16 +42,20 @@ class LDAPChangedUsersPropertySync(AllUsersPropertySync):
             return []
 
         ldap_folder = ldap_plugin['acl_users']
-        login_attr = ldap_folder._login_attr
+
+        # Convert to AD/LDAP date string
         datestring = last_sync.strftime('%Y%m%d%H%M%S.0Z')
-        logger.info('Looking for LDAP users changed since {0}'.format(
-            last_sync,
-        ))
-
-        changed_filter = '(whenChanged>={0})'.format(datestring)
+        changed_filter = '({0}>={1})'.format(
+            LDAP_CHANGED_ATTRIBUTE,
+            datestring,
+        )
+        # Combine date check with the default LDAP filters
         search_str = ldap_folder._getUserFilterString(
-            filters=(changed_filter,))
+            filters=(changed_filter,)
+        )
 
+        # Perform a search using the LDAPUserFolder
+        login_attr = ldap_folder._login_attr
         results = ldap_folder._delegate.search(
             base=ldap_folder.users_base,
             scope=ldap_folder.users_scope,
@@ -54,6 +69,7 @@ class LDAPChangedUsersPropertySync(AllUsersPropertySync):
             logger.info('No users to update')
             return []
 
+        # Translate to membrane profiles
         userids = [x[login_attr][0] for x in results['results']]
         mtool = api.portal.get_tool('membrane_tool')
         users = mtool.searchResults(exact_getUserId=userids)
