96a5cb5cca2770eb4f5c31ccd2b56c2b06275756
Matthew Sital-Singh <mattss@netsight.co.uk>
2015-09-02 17:15:17 +0100

    Auto-create user as manager

--- src/ploneintranet/userprofile/subscribers.py
+++ src/ploneintranet/userprofile/subscribers.py
@@ -1,5 +1,9 @@
+from plone import api
 from ploneintranet import api as pi_api
 from ploneintranet.userprofile.sync import IUserProfileManager
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def on_user_login(event):
@@ -9,5 +13,9 @@ def on_user_login(event):
     userid = member.getId()
     membrane_user = pi_api.userprofile.get(userid)
     if membrane_user is None:
-        profile = pi_api.userprofile.create(userid, approve=True)
-        IUserProfileManager(profile).sync()
+        logger.info('Auto-creating membrane profile for {0}'.format(
+            userid,
+        ))
+        with api.env.adopt_roles(roles=('Manager', )):
+            profile = pi_api.userprofile.create(userid, approve=True)
+            IUserProfileManager(profile).sync()
