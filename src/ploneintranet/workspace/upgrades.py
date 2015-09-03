b9f4183b541b9f3bdf36038ed6a5a72f4fe7d6bb
Cillian de Róiste <deroiste@syslab.com>
2015-09-03 12:35:31 +0200

    Save an empty tag field as () not (u'',)
    Includes an upgrade step to fix existing items.
    
    To see the problem:
    * Create a new Document in workspace
    * Save the Document (without tags)
    * Expand the sidebar
    
    You will see an empty tag in the listing for the object.

--- src/ploneintranet/workspace/upgrades.py
+++ src/ploneintranet/workspace/upgrades.py
@@ -1,3 +1,5 @@
+from plone import api
+
 import logging
 
 default_profile = 'profile-ploneintranet.workspace:default'
@@ -7,3 +9,14 @@ logger = logging.getLogger(__file__)
 def import_portal_types(context):
     logger.info('Import Types Tool')
     context.runImportStepFromProfile(default_profile, 'typeinfo')
+
+
+def reset_empty_tags(context):
+    logger.info('Resetting empty tags')
+    pc = api.portal.get_tool('portal_catalog')
+    empties = pc.searchResults({'Subject': ''})
+    for empty in empties:
+        obj = empty.getObject()
+        obj.subject = ()
+        obj.reindexObject()
+        logger.info('Reset tags for {}'.format(obj.absolute_url()))
