95ff92ff7e510e51bbe85e593b0b2511658f3bac
Alexander Pilz <pilz@syslab.com>
2015-09-14 22:33:55 +0200

    cleanup

--- src/ploneintranet/docconv/client/config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# PDF_VERSION_KEY = 'ploneintranet.docconv.pdfversion'
-# PREVIEW_IMAGES_KEY = 'ploneintranet.docconv.previewimages'
-# THUMBNAIL_KEY = 'ploneintranet.docconv.thumbnails'
-# PREVIEW_MESSAGE_KEY = 'ploneintranet.docconv.previewmessage'
-# DOCCONV_EXCLUDE_TYPES = ['video', 'image', ]
