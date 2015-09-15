400cd8b6ad5bddd552a2e1acbcce3c59423798ae
Matt Russell <mattr@netsight.co.uk>
2015-09-15 13:02:29 +0100

    Implementation and tests for user sync.

--- src/ploneintranet/userprofile/sync.py
+++ src/ploneintranet/userprofile/sync.py
@@ -10,15 +10,15 @@ from zope.interface import Interface
 from zope.interface import alsoProvides
 from plone.protect.interfaces import IDisableCSRFProtection
 
+from ploneintranet import api as pi_api
 from ploneintranet.core import ploneintranetCoreMessageFactory as _  # noqa
 from .content.userprofile import IUserProfile
 
 
-class IUserProfileManager(Interface):
-    """Sync properties for a user profile
-    """
-
 NO_VALUE = object()
+PROP_SHEET_MAP_KEY = 'ploneintranet.userprofile.property_sheet_mapping'
+PRI_EXT_USERS_KEY = 'ploneintranet.userprofile.primary_external_user_source'
+
 logger = logging.getLogger(__name__)
 
 
@@ -30,6 +30,11 @@ def get_last_sync(context):
     return getattr(context, 'last_sync', None)
 
 
+class IUserProfileManager(Interface):
+    """Sync properties for a user profile
+    """
+
+
 @adapter(IUserProfile)
 @implementer(IUserProfileManager)
 class UserPropertyManager(object):
@@ -37,7 +42,7 @@ class UserPropertyManager(object):
     def __init__(self, context):
         self.context = context
         self.property_mapping = api.portal.get_registry_record(
-            'ploneintranet.userprofile.property_sheet_mapping')
+            PROP_SHEET_MAP_KEY)
 
     def sync(self):
         logger.info('Updating info for {0.username}'.format(self.context))
@@ -76,15 +81,20 @@ class UserPropertySync(BrowserView):
         return self.request.response.redirect(userprofile.absolute_url())
 
 
-class AllUsersPropertySync(BrowserView):
+def sync_many(profiles_container, users):
+    if users:
+        start = time.time()
+        record_last_sync(profiles_container)
+        for (count, user) in enumerate(users, start=1):
+            profile_manager = IUserProfileManager(user)
+            profile_manager.sync()
+        duration = time.time() - start
+        logger.info('Updated {} in {:0.2f} seconds'.format(count, duration))
+    else:
+        logger.info('No users to sync')
 
-    def _get_users_to_sync(self):
-        """Override this to specify a subset of users to
-        be updated.
-        """
-        membrane = api.portal.get_tool('membrane_tool')
-        for brain in membrane.searchResults():
-            yield brain.getObject()
+
+class AllUsersPropertySync(BrowserView):
 
     def __call__(self):
         """Sync properties from PAS into the membrane object for
@@ -93,14 +103,51 @@ class AllUsersPropertySync(BrowserView):
         alsoProvides(self.request, IDisableCSRFProtection)
 
         users = self._get_users_to_sync()
+        sync_many(self.context, users)
 
-        start = time.time()
-        count = 0
-        # set the last sync date on the userprofile container
-        record_last_sync(self.context)
-        for (count, user) in enumerate(users, start=1):
-            IUserProfileManager(user).sync()
+    def _get_users_to_sync(self):
+        """Override this to specify a subset of users to
+        be updated.
+        """
+        membrane = api.portal.get_tool(name='membrane_tool')
+        for brain in membrane.searchResults():
+            yield brain.getObject()
 
-        duration = time.time() - start
-        logger.info('Updated {} in {:0.2f} seconds'.format(
-            count, duration))
+
+def create_membrane_profile(member):
+    userid = member.getId()
+    membrane_user = pi_api.userprofile.get(userid)
+    if membrane_user is None:
+        logger.info('Auto-creating membrane profile for {0}'.format(
+            userid,
+        ))
+        with api.env.adopt_roles(roles=('Manager', )):
+            profile = pi_api.userprofile.create(userid, approve=True)
+            profile_manager = IUserProfileManager(profile)
+            profile_manager.sync()
+
+
+class AllUsersSync(BrowserView):
+    """Sync all users from the "canonical" source of external users."""
+
+    def __call__(self):
+        self._sync()
+
+    def _sync(self):
+        pas_plugin_id = api.portal.get_registry_record(PRI_EXT_USERS_KEY)
+        acl_users = api.portal.get_tool(name='acl_users')
+        external_userids = set(acl_users[pas_plugin_id].getUserIds())
+        local_userids = set(acl_users['membrane_users'].getUserIds())
+        self._delete_user_profiles(local_userids, external_userids)
+        to_sync = self._create_user_profiles(local_userids, external_userids)
+        sync_many(self.context, list(to_sync))
+
+    def _create_user_profiles(self, local_userids, external_userids):
+        to_sync = external_userids - local_userids
+        for userid in to_sync:
+            yield pi_api.userprofile.create(username=userid)
+
+    def _delete_user_profiles(self, local_userids, external_userids):
+        to_remove = local_userids - external_userids
+        for userid in to_remove:
+            api.content.delete(self.context[userid])
--- src/ploneintranet/userprofile/tests/test_sync.py
+++ src/ploneintranet/userprofile/tests/test_sync.py
@@ -13,21 +13,23 @@ from Products.PlonePAS.plugins.property import ZODBMutablePropertyProvider
 
 from plone import api
 from ploneintranet import api as pi_api
-from ploneintranet.userprofile.tests.base import BaseTestCase
-from ploneintranet.userprofile.sync import IUserProfileManager
 from ploneintranet.userprofile.sync import AllUsersPropertySync
+from ploneintranet.userprofile.sync import AllUsersSync
+from ploneintranet.userprofile.sync import IUserProfileManager
 from ploneintranet.userprofile.sync import NO_VALUE
+from ploneintranet.userprofile.sync import record_last_sync
+from ploneintranet.userprofile.tests.base import BaseTestCase
+
+TESTING_PLUGIN_ID = 'mock_ldap'
+TEST_USER_PREFIX = 'sync-user'
 
 
-TESTING_PLUGIN_ID = 'mock_ldap_properties'
-
-
-def install_mock_properties_plugin():
+def install_mock_pas_plugin():
     out = io.BytesIO()
     pp = ZODBMutablePropertyProvider(TESTING_PLUGIN_ID,
-                                     'Mock LDAP Properties',
+                                     'Mock LDAP',
                                      schema=(('email', 'string', NO_VALUE), ))
-    pp.meta_type = 'Mock PAS User Properties'
+    pp.meta_type = 'Mock External PAS Users'
     acl_users = api.portal.get_tool('acl_users')
     acl_users[TESTING_PLUGIN_ID] = pp
     activatePluginInterfaces(api.portal.get(), TESTING_PLUGIN_ID, out)
@@ -37,9 +39,18 @@ class SyncBaseTestCase(BaseTestCase):
 
     def setUp(self):
         super(SyncBaseTestCase, self).setUp()
-        install_mock_properties_plugin()
+        install_mock_pas_plugin()
 
-    def _create_userprofiles(self):
+    def _get_member_ids(self):
+        user_ids = {user.getId() for user in api.user.get_users()}
+        return {user_id
+                for user_id in user_ids
+                if user_id.startswith(TEST_USER_PREFIX)}
+
+    def _get_userprofiles(self):
+        return map(pi_api.userprofile.get, self._get_member_ids())
+
+    def _create_userprofile(self):
         user = api.content.create(
             container=self.profiles,
             type='ploneintranet.userprofile.userprofile',
@@ -53,7 +64,7 @@ class SyncBaseTestCase(BaseTestCase):
         )
         return user
 
-    def _check_set_last_sync(self, context):
+    def _check_last_sync_set(self, context):
         self.assertIsInstance(context.last_sync, datetime)
 
     def _set_foreign_property(self, member, key, value):
@@ -67,7 +78,7 @@ class TestUserPropertyManager(SyncBaseTestCase):
 
     def test_sync(self):
         self.login_as_portal_owner()
-        user = self._create_userprofiles()
+        user = self._create_userprofile()
         member = api.user.get(username=user.username)
         email = 'test@foo.com'
         self._set_foreign_property(member, 'email', email)
@@ -80,11 +91,11 @@ class TestUserPropertyManager(SyncBaseTestCase):
         brains = catalog.searchResults(portal_type=user.portal_type)
         self.assertEqual(len(brains), 1)
         self.assertEqual(brains[0]['email'], email)
-        self._check_set_last_sync(brains[0].getObject())
+        self._check_last_sync_set(brains[0].getObject())
 
     def test_sync_local_user(self):
         self.login_as_portal_owner()
-        user = self._create_userprofiles()
+        user = self._create_userprofile()
         ad = IUserProfileManager(user)
         ad.sync()
         self.assertFalse(
@@ -97,7 +108,91 @@ class TestAllUsersPropertySync(SyncBaseTestCase):
 
     def test__call__(self):
         self.login_as_portal_owner()
-        self._create_userprofiles()
+        self._create_userprofile()
         view = AllUsersPropertySync(self.profiles, self.request)
         view()
-        self._check_set_last_sync(self.profiles)
+        self._check_last_sync_set(self.profiles)
+
+
+class TestAllUsersSync(SyncBaseTestCase):
+
+    def _create_external_user(self, test_user_suffix):
+        """Return a userid for tests."""
+        username = '{}-{}'.format(TEST_USER_PREFIX, test_user_suffix)
+        email = '{}@ploneintranet.org'.format(username)
+        member = api.user.create(username=username, email=email)
+        self._set_foreign_property(member, 'email', email)
+        return username
+
+    def _create_external_users(self, n):
+        for i in range(n):
+            self._create_external_user(i)
+
+    def _check_profiles_synced(self, expected_userids, since=datetime.min):
+        for userid in expected_userids:
+            self.assertIn(userid, self.profiles)
+            profile = self.profiles[userid]
+            self._check_last_sync_set(profile)
+            self.assertGreater(profile.last_sync, since)
+
+    def _check_sync_dates(self):
+        profiles = self._get_userprofiles()
+        sync_start = self.profiles.last_sync
+        self.assertTrue(all(sync_start < profile.last_sync
+                            for profile in profiles))
+
+    def _call_view_under_test(self):
+        view = AllUsersSync(self.profiles, self.request)
+        view()
+
+    def setUp(self):
+        super(SyncBaseTestCase, self).setUp()
+        self.login_as_portal_owner()
+        install_mock_pas_plugin()
+
+    def test_no_profiles_synced(self):
+        self._create_external_users(10)
+        self._check_profiles_synced([])
+        sync_dt = datetime.utcnow()
+        self._call_view_under_test()
+        member_ids = self._get_member_ids()
+        self._check_profiles_synced(member_ids, sync_dt)
+        self._check_sync_dates()
+
+    def test_some_profiles_synced(self):
+        self._create_external_users(9)
+
+        # Simulate 1 profile already synced
+        username = '{}-9'.format(TEST_USER_PREFIX)
+        profile = pi_api.userprofile.create(username=username)
+        record_last_sync(profile)
+        self._check_profiles_synced({profile.getId()})
+
+        self._call_view_under_test()
+        self._check_profiles_synced(self._get_member_ids())
+        self._check_sync_dates()
+
+    def test_all_profiles_synced(self):
+        for n in range(3):
+            username = '{}-{}'.format(TEST_USER_PREFIX, n)
+            profile = pi_api.userprofile.create(username=username)
+            record_last_sync(profile)
+
+        sync_dt = datetime.utcnow()
+        self._call_view_under_test()
+        self._check_profiles_synced([], sync_dt)
+
+        # Simulate a new remote user being added
+        sync_dt = datetime.utcnow()
+        userid = self._create_external_user(3)
+        self._call_view_under_test()
+        self._check_profiles_synced({userid}, since=sync_dt)
+        self._check_sync_dates()
+
+    def test_external_profile_deleted(self):
+        userid = self._create_external_user(0)
+        self._call_view_under_test()
+        api.user.delete(username=userid)
+        self._call_view_under_test()
+        self.assertNotIn(userid, self.profiles)
+        self._check_sync_dates()
