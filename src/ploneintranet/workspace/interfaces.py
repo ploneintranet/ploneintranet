f73d09bb0f41b8aedbc954a290661446dbee608f
Cillian de Róiste <deroiste@syslab.com>
2015-09-22 15:08:25 +0200

    Fire an event when the workspace roster is changed
    This can be useful for clearing caches, related to #780.

--- src/ploneintranet/workspace/interfaces.py
+++ src/ploneintranet/workspace/interfaces.py
@@ -26,6 +26,12 @@ class IParticipationPolicyChangedEvent(Interface):
     new_policy = Attribute(u"Policy we are moving to")
 
 
+class IWorkspaceRosterChangedEvent(Interface):
+    """
+    Event, which is fired once the roster of a workspace had changed
+    """
+
+
 class IWorkspaceState(Interface):
     """A view that gives access to the containing workspace
     """
