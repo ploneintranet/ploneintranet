3d10f14f23b801cb7d395d9c2b3df98fbc3ef6cb
Adrian White <adrian@netsight.co.uk>
2015-10-19 10:24:05 +0100

    added localised date format to document listing tile

--- src/ploneintranet/core/browser/tiles/my_documents.py
+++ src/ploneintranet/core/browser/tiles/my_documents.py
@@ -1,3 +1,4 @@
+from DateTime import DateTime
 from plone import api
 from plone.app.contenttypes.interfaces import IDocument
 from plone.app.contenttypes.interfaces import IFile
@@ -25,3 +26,25 @@ class MyDocumentsTile(Tile):
             sort_order='descending',
         )
         return recently_modified_items
+
+    def date(self, date):
+        ''' The date of our context object
+        '''
+        # We have to transform Python datetime into Zope DateTime
+        # before we can call toLocalizedTime.
+        time = date
+        if hasattr(time, 'isoformat'):
+            time = DateTime(self.context.raw_date.isoformat())
+
+        if DateTime().Date() == time.Date():
+            time_only = True
+        else:
+            # time_only=False still returns time only
+            time_only = None
+
+        toLocalizedTime = api.portal.get_tool('translation_service').toLocalizedTime
+        return toLocalizedTime(
+            time,
+            long_format=True,
+            time_only=time_only
+        )
