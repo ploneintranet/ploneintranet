06fd7d3961e2ee00e9b6a983fd4f03e084ef1f82
Alexander Pilz <pilz@syslab.com>
2015-11-05 23:56:31 +0100

    Change workspace overview to use solr

--- src/ploneintranet/search/base.py
+++ src/ploneintranet/search/base.py
@@ -90,6 +90,7 @@ class SearchResult(object):
         self.portal_type = context['portal_type']
         self.contact_email = context.get('email')
         self.contact_telephone = context.get('telephone')
+        self.modified = context['modified']
         if context['has_thumbs']:  # indexer in docconv
             # can occur in workspaces AND library
             if self.portal_type in ('Image', 'Document', 'News Item'):
