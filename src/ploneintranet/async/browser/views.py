a02066c8f3863f24fdf814c6e54264ba4350422d
Alexander Pilz <pilz@syslab.com>
2015-11-13 00:52:31 +0100

    remove unnecessary extra reindex

--- src/ploneintranet/async/browser/views.py
+++ src/ploneintranet/async/browser/views.py
@@ -57,5 +57,4 @@ class GeneratePreviewsView(AbstractAsyncView):
             logger.info("Generating previews for %s",
                         self.context.absolute_url(1))
             pi_api.previews.generate_previews(self.context)
-            self.context.reindexObject()
         return self.template()
