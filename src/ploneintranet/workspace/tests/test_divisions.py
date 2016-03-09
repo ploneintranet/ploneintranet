7504964a4af011b0da67ac207319178239697aef
Guido A.J. Stevens <guido.stevens@cosent.nl>
2016-03-09 22:11:31 +0000

    Refactor division test to avoid solr dependency. (And since it does transaction.commit() its a functional testcase)

--- src/ploneintranet/workspace/tests/test_divisions.py
+++ src/ploneintranet/workspace/tests/test_divisions.py
@@ -3,38 +3,32 @@ import transaction
 from plone import api
 from plone.app.testing import setRoles, login
 
-from ploneintranet.search.solr.testing import IntegrationTestCase
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
 
-from ploneintranet.suite.testing import PLONEINTRANET_SUITE_SOLR_INTEGRATION
+from ploneintranet.workspace.tests.base import FunctionalBaseTestCase
 
 vocab = 'ploneintranet.workspace.vocabularies.Divisions'
 
 
-class TestDivisions(IntegrationTestCase):
+class TestDivisions(FunctionalBaseTestCase):
     """
     Test the DivisionsVocabulary
     """
 
-    layer = PLONEINTRANET_SUITE_SOLR_INTEGRATION
-
     def setUp(self):
         super(TestDivisions, self).setUp()
-        self.portal = self.layer['portal']
-        self.catalog = api.portal.get_tool('portal_catalog')
-        self.request = self.layer['request']
 
+        self.catalog = api.portal.get_tool('portal_catalog')
         api.user.create(username='john_doe', email='john@doe.org')
         setRoles(self.portal, 'john_doe', ['Manager', ])
-
         login(self.portal, 'john_doe')
+
         self.workspaces = api.content.create(
             self.portal,
             'ploneintranet.workspace.workspacecontainer',
             'example-workspace-container'
         )
-
         self.workspace_folder = api.content.create(
             self.workspaces,
             'ploneintranet.workspace.workspacefolder',
