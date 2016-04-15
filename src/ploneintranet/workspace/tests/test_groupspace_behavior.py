99fda3e67948a4c4afcaa3391f4ddd9fb70a0c8d
Wolfgang Thomas <thomas@syslab.com>
2016-04-15 18:31:45 +0200

    Add a failing test that checks if permisison via group membership in a groupspace gets properly propagated

--- src/ploneintranet/workspace/tests/test_groupspace_behavior.py
+++ src/ploneintranet/workspace/tests/test_groupspace_behavior.py
@@ -4,6 +4,7 @@ from plone.dexterity.interfaces import IDexterityFTI
 from ploneintranet.workspace.behaviors.group import IMembraneGroup
 from Products.membrane.interfaces import IGroup
 from zope.component import queryUtility
+from AccessControl import Unauthorized
 
 
 class TestGroupspaceBehavior(BaseTestCase):
@@ -29,6 +30,7 @@ class TestGroupspaceBehavior(BaseTestCase):
             'ploneintranet.workspace.workspacefolder',
             'example-workspace'
         )
+        # add participants to workspace
         self.add_user_to_workspace(
             'wsadmin',
             self.workspace,
@@ -38,7 +40,19 @@ class TestGroupspaceBehavior(BaseTestCase):
             'user1',
             self.workspace,
         )
+
+        # Add a second workspace, without participants
+        self.workspace_b = api.content.create(
+            self.workspace_container,
+            'ploneintranet.workspace.workspacefolder',
+            'workspace-b'
+        )
+
         self.logout()
+
+    def traverse_to_item(self, item):
+        """ helper method to travers to an item by path """
+        return api.content.get(path='/'.join(item.getPhysicalPath()))
 
     def test_groups_behavior_present(self):
         self.assertTrue(IMembraneGroup.providedBy(self.workspace))
@@ -56,3 +70,25 @@ class TestGroupspaceBehavior(BaseTestCase):
             set(obj.getGroupMembers()),
             set(['wsadmin', 'user1', 'admin'])
         )
+
+    def test_group_permissions_from_workspace(self):
+        self.login('user1')
+        # user1 cannot access workspace-b
+        with self.assertRaises(Unauthorized):
+            self.traverse_to_item(self.workspace_b)
+        self.logout()
+        self.login_as_portal_owner()
+        # the group example-workspace gets added as member to workspace-b
+        self.add_user_to_workspace(
+            'example-workspace',
+            self.workspace_b,
+        )
+        obj = IGroup(self.workspace_b)
+        self.assertEqual(
+            set(obj.getGroupMembers()),
+            set(['example-workspace', 'admin'])
+        )
+        self.logout()
+        self.login('user1')
+        # user1 can now access workspace-b
+        self.traverse_to_item(self.workspace_b)
