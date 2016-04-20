0a4e1102a89cc37787cc63329e897dfcbbbda315
Guido A.J. Stevens <guido.stevens@cosent.nl>
2016-04-20 08:58:07 +0000

    Move my_documents.tile from core to layout

--- src/ploneintranet/layout/browser/dashboard.py
+++ src/ploneintranet/layout/browser/dashboard.py
@@ -3,6 +3,9 @@ from logging import getLogger
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone import api
 from plone.app.blocks.interfaces import IBlocksTransformEnabled
+from plone.app.contenttypes.interfaces import IDocument
+from plone.app.contenttypes.interfaces import IFile
+from plone.app.contenttypes.interfaces import IImage
 from plone.tiles import Tile
 from ploneintranet.workspace.utils import parent_workspace
 from ploneintranet.todo.utils import update_task_status
@@ -128,3 +131,25 @@ class TasksTile(Tile):
             else:
                 self.grouped_tasks[workspace.id]['tasks'].append(task)
         return self.render()
+
+
+class MyDocumentsTile(Tile):
+
+    def my_documents(self):
+        """
+        Return the 10 most recently modified documents which I have the
+        permission to view.
+        """
+        catalog = api.portal.get_tool('portal_catalog')
+
+        recently_modified_items = catalog.searchResults(
+            object_provides=[
+                IDocument.__identifier__,
+                IFile.__identifier__,
+                IImage.__identifier__,
+            ],
+            sort_on='modified',
+            sort_limit=10,
+            sort_order='descending',
+        )
+        return recently_modified_items
