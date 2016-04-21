b6e2a972aebf051b6bcfeb21e550a79cbe69fbf3
Wolfgang Thomas <thomas@syslab.com>
2016-04-21 16:18:23 +0200

    Make the roster view (members of a group in a workspace) play nicely with groups

--- src/ploneintranet/userprofile/browser/groups.py
+++ src/ploneintranet/userprofile/browser/groups.py
@@ -1,23 +1,37 @@
+# -*- coding: utf-8 -*-
 from plone import api
 from Products.Five import BrowserView
+from Products.PlonePAS.interfaces.group import IGroupData
 
 
 class WorkspaceGroupView(BrowserView):
     """Helper view to show the members of a group when clicked in workspace"""
 
-    def group(self):
+    def group_details(self):
         """ Get group data """
         gid = self.request.get('id')
         if not gid:
-            return dict(id='',
-                        title="No Group",
-                        description="",
-                        members=[])
+            return dict(
+                id='', title=u"No Group", description=u"", members=[])
 
-        g = api.group.get(groupname=gid)
-        m = api.user.get_users(groupname=gid)
+        group = api.group.get(groupname=gid)
+        g_title = group.getProperty('title') or group.getId() or gid
+        g_description = group.getProperty('description')
 
-        return dict(id=gid,
-                    title=g.title or gid,
-                    description=g.description,
-                    members=m)
+        users = api.user.get_users(groupname=gid)
+        members = []
+        for principal in users:
+            id = principal.getId()
+            if IGroupData.providedBy(principal):
+                typ = 'group'
+                title = principal.getProperty('title') or id
+                path = principal.getProperty('workspace_path') or ''
+            else:
+                typ = 'user'
+                title = principal.getProperty('fullname') or id
+                path = '/profiles/{0}'.format(id)
+            members.append(dict(id=id, title=title, typ=typ, path=path))
+
+        return dict(
+            id=gid, title=g_title, description=g_description, members=members
+        )
