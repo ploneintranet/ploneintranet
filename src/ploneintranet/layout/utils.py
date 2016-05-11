c9ea56ad983fc0afb705c26de7aa27530f84de8e
Alexander Pilz <pilz@syslab.com>
2016-05-11 16:04:32 +0200

    move the setting for the default dashboard view to registry so that integrators can pick their default.

--- src/ploneintranet/layout/utils.py
+++ src/ploneintranet/layout/utils.py
@@ -1,9 +1,15 @@
+# coding=utf-8
 """
 Utilities for general purposes. Meant to be imported from other packages
 and have no dependencies of other packages
 """
 
 from Products.CMFPlone.utils import safe_unicode
+from plone import api
+from logging import getLogger
+
+
+logger = getLogger(__name__)
 
 
 def shorten(text, length=20, ellipsis=u'\u2026'):
@@ -13,3 +19,21 @@ def shorten(text, length=20, ellipsis=u'\u2026'):
     if ' ' not in text:
         return u'%s %s' % (safe_unicode(text[:length - 4]), ellipsis)
     return u" ".join(safe_unicode(text)[:length].split(" ")[:-1] + [ellipsis])
+
+
+def get_record_from_registry(record, fallback=[]):
+        ''' Returns a record from the registry
+
+        If we do not have the record in the registry return the fallback tiles
+        '''
+        try:
+            return api.portal.get_registry_record(record)
+        except api.exc.InvalidParameterError:
+            logger.warning(
+                (
+                    'Cannot find registry record: %s. '
+                    'Check that ploneintranet.layout has been upgraded'
+                ),
+                record,
+            )
+            return fallback
