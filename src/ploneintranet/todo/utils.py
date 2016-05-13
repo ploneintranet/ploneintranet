1c9bfc44daf6f14eaf5c31f8fce02bd2f6ffee9b
ale-rt <alessandro.pisa@gmail.com>
2016-05-13 14:09:06 +0200

    Do not notify ObjectModifiedEvent just to trigger a catalog reindex

--- src/ploneintranet/todo/utils.py
+++ src/ploneintranet/todo/utils.py
@@ -1,8 +1,6 @@
 from plone import api
 from ploneintranet.core import ploneintranetCoreMessageFactory as _  # noqa
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.event import notify
-from zope.lifecycleevent import ObjectModifiedEvent
 
 
 def update_task_status(self, return_status_message=False):
@@ -23,11 +21,11 @@ def update_task_status(self, return_status_message=False):
         if brain.UID in active_tasks:
             if state in ["open", "planned"]:
                 api.content.transition(obj, "finish")
-                notify(ObjectModifiedEvent(obj))
+                obj.reindexObject()
         else:
             if state == "done":
                 obj.reopen()
-                notify(ObjectModifiedEvent(obj))
+                obj.reindexObject()
 
     if return_status_message:
         api.portal.show_message(
