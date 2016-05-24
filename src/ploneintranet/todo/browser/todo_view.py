0c0307f1174496112a2ce47b08eea2e160cd00ef
Cillian de Róiste <deroiste@syslab.com>
2016-05-24 17:13:53 +0200

    Tidy up the Todo view
    Remove the __call__ method since baseviews.ContentView already handles
    updating the fields and it was being called twice, and stripping event
    descriptions for event handlers.
    
    Fix the injection for "Your changes have been saved".

--- src/ploneintranet/todo/browser/todo_view.py
+++ src/ploneintranet/todo/browser/todo_view.py
@@ -1,16 +1,11 @@
 from ..interfaces import ITodoUtility
-from Acquisition import aq_inner
 from logging import getLogger
 from plone import api
 from plone.app.blocks.interfaces import IBlocksTransformEnabled
 from ploneintranet.core import ploneintranetCoreMessageFactory as _  # noqa
 from ploneintranet.workspace.basecontent.baseviews import ContentView
-from ploneintranet.workspace.basecontent.utils import dexterity_update
-from ploneintranet.workspace.utils import parent_workspace
 from zope.component import getUtility
-from zope.event import notify
 from zope.interface import implementer
-from zope.lifecycleevent import ObjectModifiedEvent
 
 log = getLogger(__name__)
 
@@ -26,33 +21,6 @@ class BaseView(ContentView):
 
 @implementer(IBlocksTransformEnabled)
 class TodoView(BaseView):
-
-    def __call__(self, milestone=None):
-        context = aq_inner(self.context)
-        self.can_edit = api.user.has_permission(
-            'Modify portal content', obj=context)
-        modified, errors = dexterity_update(context)
-        self.workspace = parent_workspace(context)
-
-        if self.workspace.is_case:
-            if self.can_edit and milestone is not None \
-               and milestone != context.milestone:
-                context.milestone = milestone
-                modified = True
-
-        if modified:
-            api.portal.show_message(
-                _("Your changes have been saved."),
-                request=self.request,
-                type="success",
-            )
-            notify(ObjectModifiedEvent(context))
-
-        if errors:
-            api.portal.show_message(
-                _("There was an error."), request=self.request, type="error")
-
-        return super(TodoView, self).__call__()
 
     def update(self):
         """ """
