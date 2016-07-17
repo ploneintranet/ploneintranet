df89786574a4dca504e77f92704aa6b157fea313
Wolfgang Thomas <thomas@syslab.com>
2016-07-17 21:38:35 +0200

    Also make sure the change-password form (for logged in users) can't be used if the option for resetting passwords is not enabled

--- src/ploneintranet/layout/browser/passwordpanel.py
+++ src/ploneintranet/layout/browser/passwordpanel.py
@@ -1,5 +1,8 @@
 # coding=utf-8
+from plone import api
+from plone.api.exc import InvalidParameterError
 from plone.app.users.browser.passwordpanel import PasswordPanel
+from ploneintranet.core import ploneintranetCoreMessageFactory as _
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
@@ -7,6 +10,21 @@ class PloneIntranetPasswordPanel(PasswordPanel):
     """Override the account panel template, which uses z3c.form"""
 
     template = ViewPageTemplateFile('templates/change-password.pt')
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        try:
+            enable_password_reset = api.portal.get_registry_record(
+                'ploneintranet.userprofile.enable_password_reset')
+        except InvalidParameterError:
+            enable_password_reset = False
+        if not enable_password_reset:
+            message = _(u'Resetting your own password is not supported.')
+            api.portal.show_message(message, self.request, 'error')
+            self.request.response.redirect(self.context.absolute_url())
+        else:
+            super(PloneIntranetPasswordPanel, self).__init__(context, request)
 
     def validate_password(self, action, data):
         super(PloneIntranetPasswordPanel, self).validate_password(action, data)
