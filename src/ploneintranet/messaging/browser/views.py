5048d80fb6dfa34cf1ef3c2a429ee0d0b327c424
Guido A.J. Stevens <guido.stevens@cosent.nl>
2016-08-01 12:24:30 +0000

    Hook up proto templates into messaging views

---
+++ src/ploneintranet/messaging/browser/views.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+import logging
+from Products.Five import BrowserView
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
+
+logger = logging.getLogger(__name__)
+
+
+class AppMessagingIndex(BrowserView):
+    """
+    'Home' view for the messaging app, with the chatlist in the sidebar.
+    """
+    # /prototype/_site/apps/messages/index.html
+
+
+class AppMessagingList(BrowserView):
+    """
+    Helper view, renders the chatlist used in the sidebar.
+    """
+    # extracted from ~/prototype/_site/apps/messages/index.html
+
+
+@implementer(IPublishTraverse)
+class AppMessagingChat(BrowserView):
+    """
+    Render a single conversation, with the chatlist in the sidebar.
+    Uses traversal: @@app-messaging-chat/userid to extract the userid.
+    """
+
+    # ~/prototype/_site/chat-guido-stevens.html
+
+    def publishTraverse(self, request, name):
+        if isinstance(name, unicode):
+            self.userid = name
+        else:
+            self.userid = name.decode('utf8')
+        # stop traversing, we have arrived
+        request['TraversalRequestNameStack'] = []
+        # return self so the publisher calls this view
+        return self
+
+
+class AppMessagingNewChat(AppMessagingChat):
+    """
+    Panel helper to create a new conversation.
+    """
+
+    # /prototype/_site/apps/messages/panel-new-chat.html
+
+    def update(self):
+        pass
+
+    def __call__(self):
+        if False:
+            self.update()
+            return self.request.response.redirect('FIXME CHAT URL')
+        else:
+            return self.index()
+
+
+class AppMessagingNewMessage(BrowserView):
+    """
+    Injection helper to add a message to a conversation.
+    """
+
+    # ~/prototype/_site/apps/messages/feedback-liz-guido.html
+
+    def update(self):
+        pass
+
+    def __call__(self):
+        self.update()
+        return self.index()
