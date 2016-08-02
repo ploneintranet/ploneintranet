5f9f36ae6eb61388001a00d814c8f995a13395bf
Guido A.J. Stevens <guido.stevens@cosent.nl>
2016-08-02 14:00:07 +0000

    Implement sidebar and main panel with mocked data

--- src/ploneintranet/messaging/browser/views.py
+++ src/ploneintranet/messaging/browser/views.py
@@ -1,5 +1,9 @@
 # coding=utf-8
 import logging
+import urllib
+from plone import api
+from ploneintranet import api as pi_api
+from Products.CMFPlone.utils import safe_unicode
 from Products.Five import BrowserView
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
@@ -16,15 +20,84 @@ class AppMessagingView(BrowserView):
 
     # ~/prototype/_site/chat-guido-stevens.html
 
+    def __init__(self, context, request):
+        super(AppMessagingView, self).__init__(context, request)
+        self.userid = None
+
     def publishTraverse(self, request, name):
+        userid = urllib.unquote(name)
         if isinstance(name, unicode):
-            self.userid = name
+            self.userid = userid
         else:
-            self.userid = name.decode('utf8')
+            self.userid = userid.decode('utf8')
         # stop traversing, we have arrived
         request['TraversalRequestNameStack'] = []
         # return self so the publisher calls this view
         return self
+
+    def conversations(self):
+        fake = [
+            dict(userid='allan_neece',
+                 status='unread',
+                 byline='Foo the bar'),
+            dict(userid='alice_lindstrom',
+                 status='',
+                 byline='Nix the nought'),
+            dict(userid='guy_hackey',
+                 status='',
+                 byline='...')
+        ]
+        for x in fake:
+            if x['userid'] == self.userid:
+                x['status'] = 'current'
+            x['fullname'] = self._fullname(x['userid'])
+            x['chat_url'] = self._chat_url(x['userid'])
+            x['avatar_url'] = self._avatar_url(x['userid'])
+        return fake
+
+    def _fullname(self, userid):
+        return userid
+
+    def _chat_url(self, userid):
+        portal_url = api.portal.get().absolute_url()
+        return '{}/@@app-messaging/{}'.format(
+            portal_url,
+            urllib.quote(safe_unicode(userid))
+        )
+
+    def _avatar_url(self, userid):
+        return pi_api.userprofile.avatar_url(userid)
+
+    def messages(self):
+        fake = [
+            dict(type='date',
+                 status='',
+                 timestamp='20 June 2016, 14:32'),
+            dict(type='text',
+                 status='self',
+                 userid='guido_stevens',
+                 message='Whatever. Wherever.'),
+            dict(type='text',
+                 status='',
+                 userid='alice_lindstrom',
+                 message='Seriously? Today?'),
+            dict(type='date',
+                 status='',
+                 timestamp='21 June 2016, 11:23'),
+            dict(type='text',
+                 status='self',
+                 userid='guido_stevens',
+                 message='Ping?'),
+            dict(type='text',
+                 status='',
+                 userid='alice_lindstrom',
+                 message='Pong!'),
+        ]
+        for x in fake:
+            if x['type'] == 'text':
+                x['fullname'] = self._fullname(x['userid'])
+                x['avatar_url'] = self._avatar_url(x['userid'])
+        return fake
 
 
 class AppMessagingNewChat(BrowserView):
