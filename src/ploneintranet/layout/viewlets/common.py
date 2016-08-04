9553b90eac1418837b7bc39dc15c8b85dec49d15
Wolfgang Thomas <thomas@syslab.com>
2016-08-04 19:00:58 +0200

    adapt logo viewlet according to new proto

--- src/ploneintranet/layout/viewlets/common.py
+++ src/ploneintranet/layout/viewlets/common.py
@@ -4,6 +4,7 @@ from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.interfaces import ISiteSchema
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getUtility
+from ploneintranet.workspace.utils import in_workspace
 
 
 class GlobalSectionsViewlet(common.GlobalSectionsViewlet):
@@ -22,4 +23,14 @@ class LogoViewlet(common.LogoViewlet):
     index = ViewPageTemplateFile('logo.pt')
 
     def update(self):
+        # This implements the following directive from proto:
+        # The href of the link below is variable. When in a workspaces it
+        # points to the workspaces overview, when in an app it points to the
+        # apps overview and for other situations to the dashboard.
+
+        # XXX to be done: add `in_app` util check and respective rule
         super(LogoViewlet, self).update()
+        if in_workspace(self.context):
+            self.logo_url = "{0}/workspaces".format(self.navigation_root_url)
+        else:
+            self.logo_url = self.navigation_root_url
