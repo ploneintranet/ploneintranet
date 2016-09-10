2e8878d534d0299eea9199c15ce03144a88cfba4
Alexander Pilz <pilz@syslab.com>
2016-09-10 20:14:41 +0200

    prepare PI bookmark workspace on workspace view

--- src/ploneintranet/bookmarks/browser/tiles.py
+++ src/ploneintranet/bookmarks/browser/tiles.py
@@ -3,6 +3,10 @@ from plone import api
 from plone.memoize.view import memoize_contextless
 from plone.tiles import Tile
 from ploneintranet.layout.app import apps_container_id
+from ploneintranet.bookmarks.browser.base import BookmarkActionView
+from ploneintranet.bookmarks.browser.bookmark_link import View
+from ploneintranet.core import ploneintranetCoreMessageFactory as _  # noqa
+from plone.protect.utils import addTokenToUrl
 
 
 class BookmarksTile(Tile):
@@ -38,3 +42,35 @@ class BookmarksTile(Tile):
             return self.app_bookmarks.most_popular_bookmarks()
         else:
             return self.app_bookmarks.my_recent_bookmarks()
+
+
+class WorkspaceBookmarkTile(Tile, BookmarkActionView, View):
+    """ Proto specific way to bookmark a workspace on the workspace view """
+    msg = ''
+
+    def __call__(self):
+
+        if self.request.get('bookmark'):
+            self.ploneintranet_network.bookmark('content', self.context.UID())
+            self.msg = _(u'success_bookmark',
+                         u'You have bookmarked this item.')
+        elif self.request.get('unbookmark'):
+            self.ploneintranet_network.unbookmark('content',
+                                                  self.context.UID())
+            self.msg = _(u'success_unbookmarked',
+                         u'You have unbookmarked this item.')
+
+        if self.is_bookmarked:
+            self.title = 'Bookmarked'
+            self.icon_class = 'icon-bookmark'
+            self.description = \
+                _(u'Remove this workspace from your bookmarks')
+            self.link = addTokenToUrl(self.url + '?unbookmark=1')
+        else:
+            self.title = 'Bookmark'
+            self.icon_class = 'icon-bookmark-empty'
+            self.description = \
+                _(u'Add this workspace to your bookmarks.')
+            self.link = addTokenToUrl(self.url + '?bookmark=1')
+            self.status = ''
+        return self.index()
