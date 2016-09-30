dba9d8b09b10ac15a1f3e6274d11cd0437ae1fdd
Guido A.J. Stevens <guido.stevens@cosent.nl>
2016-09-30 06:05:43 +0000

    Remove duplicated main_template and registry from theme. These are now provided by quaive.resources.ploneintranet. Keeping browser layer which is still used.

--- src/ploneintranet/theme/browser/main_template.py
+++
@@ -1,7 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.CMFPlone.browser.main_template import MainTemplate
-
-
-class PIMainTemplate(MainTemplate):
-    main_template = ViewPageTemplateFile('templates/main_template.pt')
-    ajax_template = main_template
