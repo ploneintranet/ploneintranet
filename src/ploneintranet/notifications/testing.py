8e72b2b00005869b8e419c1fafbfd9fa9b283032
Alexander Pilz <pilz@syslab.com>
2016-10-24 11:21:47 +0200

    include permission

--- src/ploneintranet/notifications/testing.py
+++ src/ploneintranet/notifications/testing.py
@@ -27,6 +27,11 @@ class PloneintranetNotificationsLayer(PloneSandboxLayer):
             ploneintranet.microblog,
             context=configurationContext
         )
+        xmlconfig.file(
+            'permissions.zcml',
+            ploneintranet.microblog,
+            context=configurationContext
+        )
 
     def tearDownZope(self, app):
         pass
