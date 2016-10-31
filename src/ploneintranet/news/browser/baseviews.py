1e9e7425814ee4574add5d994b98f2e0cf2de501
Guido A.J. Stevens <guido.stevens@cosent.nl>
2016-10-31 12:20:49 +0000

    Hook up basic metadata editing

--- src/ploneintranet/news/browser/baseviews.py
+++ src/ploneintranet/news/browser/baseviews.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
-from Products.Five.browser import BrowserView
+from Acquisition import aq_inner
+
 from plone import api
+from ploneintranet.workspace.basecontent import baseviews
 
 
-class NewsItemEdit(BrowserView):
+class NewsItemEdit(baseviews.ContentView):
 
     def sidebar(self):
         app = self.context.aq_parent
         publisher = api.content.get_view('publisher', app, self.request)
         return publisher.sidebar()
+
+    def __call__(self):
+        context = aq_inner(self.context)
+        self.can_edit = api.user.has_permission('Modify portal content',
+                                                obj=context)
+        if self.request.method == 'POST':
+            self.update()
+        return super(NewsItemEdit, self).__call__()
+
+    def update(self):
+        return super(NewsItemEdit, self).update()
