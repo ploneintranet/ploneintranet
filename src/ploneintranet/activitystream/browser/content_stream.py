0d9db12d007b2415de3737e79eea47bf293bebcc
ale-rt <alessandro.pisa@gmail.com>
2017-03-28 08:50:20 +0200

    Include first comment box

--- src/ploneintranet/activitystream/browser/content_stream.py
+++ src/ploneintranet/activitystream/browser/content_stream.py
@@ -29,21 +29,45 @@ class ContentStreamView(BrowserView):
             )
 
     @property
+    @memoize
     def has_shares(self):
         return bool(self.num_shares)
 
     @property
+    @memoize
     def num_shares(self):
         return len(self._statusupdates_threadparents())
 
     @property
+    @memoize
     def has_comments(self):
         return bool(self.num_comments)
 
     @property
+    @memoize
     def num_comments(self):
         return len([su for su in self._statusupdates_all()
                     if su.thread_id])
+
+    @property
+    @memoize
+    def is_whitelisted(self):
+        ''' Check if the context portal type is whitelisted
+        '''
+        whitelist = api.portal.get_registry_record(
+            'ploneintranet.microblog.whitelisted_types',
+        )
+        return self.context.portal_type in whitelist
+
+    @property
+    def can_init_comments(self):
+        ''' Check if we can initialize the comments here
+        '''
+        if self.has_shares:
+            return False
+        if not self.is_whitelisted:
+            return False
+        return True
 
     @memoize
     def _statusupdates_threadparents(self):
@@ -70,6 +94,11 @@ class ContentStreamView(BrowserView):
     def _statusupdates_all(self):
         container = pi_api.microblog.get_microblog()
         return list(container.content_values(self.context))
+
+    def show_stream(self):
+        ''' Check if we should show the stream
+        '''
+        return self.has_shares or self.is_whitelisted
 
     def __call__(self):
         ''' On the content stream we want all the comments,
