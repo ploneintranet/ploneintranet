54c930d25e96e3b8c83c86f977f43c473393e252
ale-rt <alessandro.pisa@gmail.com>
2017-05-25 15:32:08 +0200

    Create the todo app

---
+++ src/ploneintranet/todo/setuphandlers.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+from logging import getLogger
+from plone import api
+
+
+logger = getLogger()
+
+
+def create_app():
+    ''' Create the task application
+    '''
+    portal = api.portal.get()
+    apps = portal.apps
+    if 'todo' not in apps:
+        api.content.create(
+            container=apps,
+            type='ploneintranet.layout.app',
+            title=u'Todo',
+            id='todo',
+            safe_id=False,
+            app='@@app-todo',
+            devices='desktop tablet mobile'
+        )
+
+    app = apps['todo']
+    if api.content.get_state(app) == 'published':
+        return
+    try:
+        api.content.transition(app, to_state='published')
+    except:
+        logger.exception('Cannot publish the app: %r', app)
+
+
+def post_default(context):
+    ''' Actions needed after importing
+    the ploneintranet.todo:default profile
+    '''
+    create_app()
