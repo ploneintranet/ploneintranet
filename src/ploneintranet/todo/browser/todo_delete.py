7f24c8a79204a89e49026ab4e8858b402bbba305
ale-rt <alessandro.pisa@gmail.com>
2017-05-30 15:30:14 +0200

    Handle redirect after a todo is cancelled

---
+++ src/ploneintranet/todo/browser/todo_delete.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+from ploneintranet.core import ploneintranetCoreMessageFactory as _
+from ploneintranet.layout.browser.actions import PIDeleteConfirmationForm
+from ploneintranet.workspace.utils import parent_workspace
+from z3c.form import button
+
+
+class TodoDeleteConfirmationForm(PIDeleteConfirmationForm):
+    ''' Override the Delete confirmation view to redirect to the todo app
+    if the task is in the userprofile context.
+
+    Note: we need to override both the actions
+    '''
+    @button.buttonAndHandler(_(u'I am sure, delete now'), name='Delete')
+    def handle_delete(self, action):
+        base_handler = super(TodoDeleteConfirmationForm, self).handle_delete
+        base_handler(self, action)
+        if parent_workspace(self.context):
+            return
+        url = self.context.apps['todo'].app_url()
+        return self.request.response.redirect(url)
+
+    @button.buttonAndHandler(
+        _(u'label_cancel', default=u'Cancel'), name='Cancel')
+    def handle_cancel(self, action):
+        return self.request.response.redirect(self.view_url())
