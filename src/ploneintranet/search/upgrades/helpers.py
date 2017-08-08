ploneintranet/ploneintranet

commit 984aae9cb6c9d564bd64d8dcc4e9db5cf0d15a5d

Todo app improvements

src/ploneintranet/search/upgrades/helpers.py
@@ -1,6 +1,8 @@
 # coding=utf-8
-import logging
 from plone import api
+
+import logging
+
 
 default_profile = 'profile-ploneintranet.search:default'
 logger = logging.getLogger(__file__)
@@ -15,21 +17,35 @@ def update_registry(context):
     )
 
 
-def update_registry_filter_fields(context, new_fields):
+def update_registry_filter_fields(context, new_fields, obsolete_fields=[]):
     '''
     '''
     key = 'ploneintranet.search.filter_fields'
     record = api.portal.get_registry_record(key)
+    obsolete_fields = [
+        field for field in obsolete_fields
+        if field in record
+    ]
     missing_fields = [
         field for field in new_fields
         if field not in record
     ]
-    if not missing_fields:
+    if not missing_fields or not obsolete_fields:
         logger.info('All records up to date')
         return
-    logger.info(
-        'Adding records %r to the registry record %s',
-        missing_fields,
-        key
-    )
+    if missing_fields:
+        logger.info(
+            'Adding records %r to the registry record %s',
+            missing_fields,
+            key
+        )
+    if obsolete_fields:
+        logger.info(
+            'Removing records %r to the registry record %s',
+            obsolete_fields,
+            key
+        )
+        record = tuple(
+            field for field in record if field not in obsolete_fields
+        )
     api.portal.set_registry_record(key, record + tuple(missing_fields))

src/ploneintranet/search/profiles/default/metadata.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0"?>
 <metadata>
-  <version>0011</version>
+  <version>0012</version>
   <dependencies>
     <dependency>profile-ploneintranet.docconv.client:default</dependency>
   </dependencies>

src/ploneintranet/search/profiles/default/registry.xml
@@ -22,8 +22,8 @@
       <element>division</element>
       <element>UID</element>
       <element>invitees</element>
-      <element>due__ge</element>
-      <element>due__le</element>
+      <element>due__gt</element>
+      <element>due__lt</element>
       <element>due__range</element>
       <element>end__gt</element>
       <element>object_provides</element>

src/ploneintranet/search/upgrades/configure.zcml
@@ -104,8 +104,8 @@
       title="Configure the solr fields for the todo app"
       description=""
       source="*"
-      destination="0011"
-      handler=".to0011.add_todo_app_solr_fields"
+      destination="0012"
+      handler=".to0012.add_todo_app_solr_fields"
       profile="ploneintranet.search:default" />
 
 </configure>

src/ploneintranet/search/upgrades/to0012.py
@@ -5,9 +5,17 @@ from ploneintranet.search.upgrades.helpers import update_registry_filter_fields
 def add_todo_app_solr_fields(context):
     new_fields = (
         u'assignee',
-        u'due__ge',
-        u'due__le',
+        u'due__gt',
+        u'due__lt',
         u'due__range',
         u'initiator',
     )
-    update_registry_filter_fields(context, new_fields)
+    obsolete_fields = (
+        u'due__ge',
+        u'due__le',
+    )
+    update_registry_filter_fields(
+        context,
+        new_fields,
+        obsolete_fields,
+    )

src/ploneintranet/suite/tests/acceptance/todo.robot
@@ -37,6 +37,11 @@ Allan can use the todo apps filters
      Then I can see that the first task is  Budget
      When I filter the results by state  Closed
      Then I can see that the first task is  Identify the requirements
+     [Documentation]  The filters are persistent through a cookie
+     Then I can go to the application  Todo
+      And I can see that the first task is  Identify the requirements
+     When I reset the filters
+     Then I can see that the first task is  Populate Metadata
 
 
 *** Keywords ***
@@ -83,7 +88,9 @@ I sort the results
     [arguments]  ${title}
     Select From List  sort-mode  ${title}
 
-
 I filter the results by state
     [arguments]  ${title}
     Select From List  state-mode  ${title} tasks
+
+I reset the filters
+    Click Button  Reset search and filters

src/ploneintranet/todo/browser/app.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from cgi import urlparse
 from collections import defaultdict
 from collections import OrderedDict
 from itertools import imap
@@ -21,6 +22,11 @@ from zope.interface import implementer
 
 
 logger = getLogger(__name__)
+
+
+class InvalidFiltersException(ValueError):
+    ''' Raise this if the search filters are invalid
+    '''
 
 
 @implementer(IAppView)
@@ -77,10 +83,59 @@ class View(BrowserView):
     )
 
     _search_tasks_limit = 100
-    _ignored_keys = {'_', 'app'}
+    _cookie_name = 'app-todo'
+    _ignored__form_keys = {
+        '_',
+        '_authenticator',
+        'app',
+        'reset-filters',
+    }
 
     show_initiators = True
     show_assignes = True
+
+    def get_cookie_params(self):
+        ''' Get the params stored in the cookie unless explicitely requested
+        '''
+        if 'reset-filters' in self.request.form:
+            return {}
+        cookie = self.request.cookies.get(self._cookie_name)
+        try:
+            params = dict(urlparse.parse_qsl(cookie))
+        except AttributeError:
+            params = {}
+        return params
+
+    def set_cookie_params(self, params):
+        ''' Set the current search parameters into a cookie
+        '''
+        if params:
+            self.request.response.setCookie(
+                self._cookie_name,
+                urlencode([
+                    (key, params[key])
+                    for key in sorted(params)
+                    if params[key] and key not in self._ignored__form_keys
+                ]),
+                path='/',
+            )
+        else:
+            pass  # import ipdb; ipdb.set_trace()
+
+    @property
+    @memoize_contextless
+    def form_params(self):
+        ''' Persist in a cookie the search options
+        '''
+        params = self.get_cookie_params()
+        for key, value in self.request.form.iteritems():
+            if key not in self._ignored__form_keys:
+                if value:
+                    params[key] = value
+                else:
+                    params.pop(key, None)
+        self.set_cookie_params(params)
+        return params
 
     @property
     @memoize_contextless
@@ -214,7 +269,7 @@ class View(BrowserView):
     def options2items(self, options, fieldname, default=''):
         ''' Make items that will be rendered in a page template
         '''
-        selected = self.request.form.get(fieldname, default)
+        selected = self.form_params.get(fieldname, default)
         return [
             {
                 'value': value,
@@ -239,7 +294,7 @@ class View(BrowserView):
     @property
     @memoize
     def browse_mode(self):
-        return self.request.form.get(
+        return self.form_params.get(
             'browse-mode',
             self._browse_mode_default,
         )
@@ -277,7 +332,7 @@ class View(BrowserView):
     def set_filters(self, filters):
         ''' Customizable filters for the view
         '''
-        form = self.request.form
+        form = self.form_params
         filters['portal_type'] = 'todo'
         filters['path'] = self.searched_paths
         state_mode = form.get('state-mode', self._state_mode_default)
@@ -286,11 +341,18 @@ class View(BrowserView):
         start = form.get('start', None)
         end = form.get('end', None)
         if start and end:
+            if start > end:
+                raise InvalidFiltersException(
+                    'Invalid date range: %r, %r' % (
+                        start,
+                        end,
+                    )
+                )
             filters['due__range'] = (solr_date(start), solr_date(end))
         elif start:
-            filters['due__ge'] = solr_date(start)
+            filters['due__gt'] = solr_date(start)
         elif end:
-            filters['due__le'] = solr_date(end)
+            filters['due__lt'] = solr_date(end)
         for key in (
             'assignee',
             'initiator',
@@ -304,9 +366,12 @@ class View(BrowserView):
         Search for specific content types
         """
         filters = filters.copy()
-        form = self.request.form
+        form = self.form_params
         keywords = form.get('SearchableText')
-        self.set_filters(filters)
+        try:
+            self.set_filters(filters)
+        except InvalidFiltersException:
+            return []
         search_util = getUtility(ISiteSearch)
         _params = {
             'sort': form.get('sort-mode', self._sort_mode_default),
@@ -561,8 +626,8 @@ class View(BrowserView):
     def show_reset_button(self):
         ''' Show the reset button if we have something in the query string
         '''
-        keys = set(self.request.form)
-        return keys - self._ignored_keys
+        keys = set(self.form_params)
+        return keys - self._ignored__form_keys
 
 
 class MyTasksView(View):

src/ploneintranet/todo/browser/templates/app-results.pt
@@ -18,6 +18,7 @@
     <button class="icon-cancel-circle"
             formaction="${here/absolute_url}/@@${view/reset_url}#sidebar-content"
             type="submit"
+            name="reset-filters"
             i18n:translate=""
     >Reset search and filters</button>
   </p>

commit 8b4a9cc93feb68810fe56b71f1bc1f679ef737a1

Fix stupid bug introduced in 984aae9cb6c9d564bd64d8dcc4e9db5cf0d15a5d (= when cheking for 2 negative conditions, use AND not OR)

src/ploneintranet/search/upgrades/helpers.py
@@ -30,7 +30,7 @@ def update_registry_filter_fields(context, new_fields, obsolete_fields=[]):
         field for field in new_fields
         if field not in record
     ]
-    if not missing_fields or not obsolete_fields:
+    if not missing_fields and not obsolete_fields:
         logger.info('All records up to date')
         return
     if missing_fields: