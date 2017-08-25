9370fcb1010c5119efefcda8e6303b9cb50db072
Wolfgang Thomas <thomas@syslab.com>
2017-08-25 05:37:55 +0200

    use msgids for all on-screen help texts, so that changes to the English texts can be done without invalidating the translations

--- src/ploneintranet/layout/browser/on_screen_help.py
+++ src/ploneintranet/layout/browser/on_screen_help.py
@@ -35,7 +35,8 @@ class View(BaseView):
         'dashboard-view': {
             'title': _('Dashboard view select'),
             'description': _(
-                'Here you can select different dashboards. '
+                'help-dashboard-view',
+                default='Here you can select different dashboards. '
                 'There is an activity centric and a task '
                 'centric dashboard available. You can also '
                 'configure your own dashboard using My view. '
@@ -46,7 +47,8 @@ class View(BaseView):
         'hamburger': {
             'title': _('Menu toggle'),
             'description': _(
-                'The menu toggle lets you hide and show the '
+                'help-hamburger',
+                default='The menu toggle lets you hide and show the '
                 'global navigation which allows you to quickly '
                 'jump to another section of the intranet.'
             ),
@@ -55,7 +57,8 @@ class View(BaseView):
         'portal-suite': {
             'title': _('Welcome!'),
             'description': _(
-                'We would like to introduce you to the '
+                'help-portal-suite',
+                default='We would like to introduce you to the '
                 'Quaive Social Intranet Platform. Please try out!'
             ),
             'next': 'portal-dashboard',
@@ -63,9 +66,11 @@ class View(BaseView):
         'portal-dashboard': {
             'title': _('Dashboard'),
             'description': _(
-                'This is the Dashboard. '
-                'Here is the home for your personal shortcuts '
-                'to your most important places. You can '
+                'help-portal-dashboard',
+                default='This is your personal Dashboard. '
+                'It is the home for shortcuts '
+                'to your most important places and shows you an overview '
+                'of recent activities. You can '
                 'add workspaces, contacts and apps to '
                 'your own dashboard.'
             ),
@@ -74,33 +79,38 @@ class View(BaseView):
         'portal-news': {
             'title': _('News'),
             'description': _(
-                'Here is the news magazine. It gives you an overview '
-                "over your company's actual news."
+                'help-portal-news',
+                default='Here is the news magazine. It gives you an overview '
+                "over your organization's news."
             ),
             'next': 'portal-library',
         },
         'portal-library': {
             'title': _('Library'),
             'description': _(
-                'In the library you can find any documents '
-                'that are informational for all intranet users.'
+                'help-portal-library',
+                default='In the library you can find documents '
+                'that are valid for all intranet users, organized by '
+                'different topics.'
             ),
             'next': 'portal-workspaces',
         },
         'portal-workspaces': {
             'title': _('Workspaces'),
             'description': _(
-                'This is workspaces section. Workspaces are '
+                'help-portal-workspaces',
+                default='Workspaces are '
                 'areas for creating, organizing and sharing '
                 'content. Go here to get an overview over the '
-                'workspaces you can access.'
+                'workspaces you can access or create your own workspaces.'
             ),
             'next': 'portal-apps',
         },
         'portal-apps': {
             'title': _('Apps'),
             'description': _(
-                'Apps are extra bits of functionality for your '
+                'help-portal-apps',
+                default='Apps are extra bits of functionality for your '
                 'Quaive site. Visit this section to find out what '
                 'apps are available.'
             ),
@@ -109,7 +119,8 @@ class View(BaseView):
         'portlet-bookmarks': {
             'title': _('Bookmarks portlet'),
             'description': _(
-                'Bookmarks are what matters most to you. '
+                'help-portlet-bookmarks',
+                default='Bookmarks are what matters most to you. '
                 'While browsing through the intranet, '
                 'you can mark interesting content with a '
                 'bookmark. All that content will then '
@@ -120,7 +131,8 @@ class View(BaseView):
         'portlet-contacts': {
             'title': _('Contacts portlet'),
             'description': _(
-                'Quick access to all your contacts. Start '
+                'help-portlet-contacts',
+                default='Quick access to all your contacts. Start '
                 'typing a name, a job title or a street. '
                 'Matching contacts appear immediately.'
             ),
@@ -129,7 +141,8 @@ class View(BaseView):
         'portlet-events': {
             'title': _('Events portlet'),
             'description': _(
-                'All events you are invited to - at your '
+                'help-portlet-events',
+                default='All events you are invited to - at your '
                 'fingertips. They are ordered by upcoming '
                 'first.',
             ),
@@ -138,7 +151,8 @@ class View(BaseView):
         'portlet-library': {
             'title': _('Library portlet'),
             'description': _(
-                'See recent changes to the library. '
+                'help-portlet-library',
+                default='See recent changes to the library. '
                 'Content listed here may be relevant for '
                 'your next holiday application, show '
                 'newest updates to the company manual or '
@@ -151,19 +165,21 @@ class View(BaseView):
         'portlet-news': {
             'title': _('News portlet'),
             'description': _(
-                'Stay informed - see the latest news articles listed here. '
-                "If you click the 'mark read' button of a news item "
-                'it disappears from the portlet.'
+                'help-portlet-news',
+                default='Stay informed - see the latest news articles listed '
+                "here. If you click the 'mark read' button of a news item, "
+                'it will disappear from the portlet.'
             ),
             'next': '',
         },
         'portlet-tasks': {
             'title': _('Tasks portlet'),
             'description': _(
-                'Your Tasks. This is what you need to work '
+                'help-portlet-tasks',
+                default='Your Tasks. This is what you need to work '
                 'on, ordered by deadline. The topmost one is '
                 'usually the one due first. If you just '
-                "completed one, check it. If you don't yet "
+                "completed one, mark it as completed. If you don't yet "
                 'know what to do, click it and read the '
                 'description.'
             ),
@@ -172,7 +188,8 @@ class View(BaseView):
         'portlet-workspaces': {
             'title': _('Workspaces portlet'),
             'description': _(
-                'Your Workspaces! That are workspaces '
+                'help-portlet-workspaces',
+                default='Your Workspaces! These are workspaces '
                 'that you can see. You will never see '
                 "any workspaces here that you can't "
                 'actually access.'
@@ -182,8 +199,9 @@ class View(BaseView):
         'portlet-bookmarked-workspaces': {
             'title': _('Bookmarked workspaces portlet'),
             'description': _(
-                'Your bookmarked Workspaces! That are workspaces '
-                'you marked as important by bookmarking them. '
+                'help-portlet-bookmarked-workspaces',
+                default='Your bookmarked Workspaces! These are workspaces '
+                'you have marked as important by bookmarking them. '
                 "You will never see any workspaces here that you can't "
                 'actually access.'
             ),
@@ -192,15 +210,17 @@ class View(BaseView):
         'portlet-bookmarked-apps': {
             'title': _('Bookmarked apps portlet'),
             'description': _(
-                'Your bookmarked Apps! That are apps '
-                'you marked as important by bookmarking them.'
+                'help-portlet-bookmarked-apps',
+                default='Your bookmarked Apps! These are apps '
+                'you have marked as important by bookmarking them.'
             ),
             'next': '',
         },
         'portlet-stream': {
             'title': _('Activity stream portlet'),
             'description': _(
-                "Read about what's going on in your company "
+                'help-portlet-stream',
+                default="Read about what's going on in your company "
                 'in the activity stream portlet. You can filter '
                 'existing messages and also write a post '
                 'to your colleagues yourself.'
@@ -210,7 +230,8 @@ class View(BaseView):
         'post-message': {
             'title': _('Post a message'),
             'description': _(
-                'Post a message to the team, to your '
+                'help-post-message',
+                default='Post a message to the team, to your '
                 'colleagues, to the company. Share what is '
                 'important right now, let your colleagues '
                 'know. Upload attachments, mention specific '
@@ -221,22 +242,26 @@ class View(BaseView):
         'global-search': {
             'title': _('Portal search'),
             'description': _(
-                'Use the search to find information within the Quaive portal.'
+                'help-global-search',
+                default='Use the search to find information within '
+                'the Quaive portal.'
             ),
             'next': 'global-help-info',
         },
         'global-help-info': {
             'title': _('Help'),
             'description': _(
-                'Use this toggle to turn the help bubbles on or off.'
+                'help-global-help-info',
+                default='Use this toggle to turn the help bubbles on or off.'
             ),
             'next': 'global-chat-info',
         },
         'global-chat-info': {
             'title': _('Post a message'),
             'description': _(
-                'Wanna chat with your collegues? Click here to open the '
-                'chat app. A number within a red circle shows that you '
+                'help-global-chat-info',
+                default='Wanna chat with your collegues? Click here to open  '
+                'the chat app. A number within a red circle shows that you '
                 'have unread chat messages.'
             ),
             'next': 'global-messages-info',
@@ -244,7 +269,8 @@ class View(BaseView):
         'global-messages-info': {
             'title': _('Post a message'),
             'description': _(
-                'Notifications about news posts are listed here. '
+                'help-global-messages-info',
+                default='Notifications about news posts are listed here. '
                 'A number within a red circle shows that you '
                 'have unread notifications.'
             ),
@@ -253,8 +279,9 @@ class View(BaseView):
         'global-personal-menu': {
             'title': _('Personal menu'),
             'description': _(
-                'Your avatar image opens the personal menu with links to '
-                'your user profile, password change and logout actions.'
+                'help-global-personal-menu',
+                default='Your avatar image opens the personal menu with links '
+                'to your user profile, password change and logout actions.'
             ),
             'next': '',
         }
